272f3591ff2add4ed5ea006bf91611914f946039
squid bacon <owenbchanas@gmail.com>
2025-09-23 19:19:01 -0500

    changed tab length

--- Loadout.java
+++ Loadout.java
@@ -1,3 +1,7 @@
+/**
+ * Represents a pilot gear loadout. Stores information about what
+ *     gear items the pilot has chosen.
+ */
 public class Loadout {
-  // TODO: fill out
+    // TODO: fill out
 }
