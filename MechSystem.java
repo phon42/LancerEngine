7192846266546f67e31670f7acc9a5b1def61d2d
squid bacon <owenbchanas@gmail.com>
2025-09-23 19:19:20 -0500

    changed tab length

--- MechSystem.java
+++ MechSystem.java
@@ -1,36 +1,47 @@
+/**
+ * Contains a single system for a mech. Stores information about
+ *     that system such as its name and its tags.
+ */
 public class MechSystem {
-  // TODO: fill out
-  private String systemName;
+    // TODO: fill out
+    private String systemName;
 
-  private boolean limited;
-  private int limitedCharges;
+    private boolean limited;
+    private int limitedCharges;
 
-  public String getSystemName() {
-    return systemName;
-  }
-  public boolean isLimited() {
-    return limited;
-  }
-  public int getLimitedCharges() {
-    return limitedCharges;
-  }
-  public void setSystemName(String systemName) {
-    this.systemName = systemName;
-  }
-  public void setLimited(boolean limited) {
-    this.limited = limited;
-  }
-  public void setLimitedCharges(int limitedCharges) {
-    this.limitedCharges = limitedCharges;
-  }
-
-  public String outputSystem(String outputType) {
-    String outputString = "";
-
-    if (outputType == "mech build" || outputType == "full") {
-      outputString += systemName;
+    public String getSystemName() {
+        return systemName;
+    }
+    public boolean isLimited() {
+        return limited;
+    }
+    public int getLimitedCharges() {
+        return limitedCharges;
+    }
+    public void setSystemName(String systemName) {
+        this.systemName = systemName;
+    }
+    public void setLimited(boolean limited) {
+        this.limited = limited;
+    }
+    public void setLimitedCharges(int limitedCharges) {
+        this.limitedCharges = limitedCharges;
     }
 
-    return outputString;
-  }
+    /**
+     * Outputs a printout of this specific mech system.
+     * @param @param outputType a String which can be one of the following:
+     *     "mech build", "pilot", or "full", and determines how much information
+     *     is printed.
+     * @return a String containing the printed out information of the system.
+     */
+    public String outputSystem(String outputType) {
+        String outputString = "";
+
+        if (outputType == "mech build" || outputType == "full") {
+            outputString += systemName;
+        }
+
+        return outputString;
+    }
 }
