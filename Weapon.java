3300e0b9c5d18dd7bbe86ad4da1af2feccdb3eea
squid bacon <owenbchanas@gmail.com>
2025-09-23 19:19:56 -0500

    changed tab length

--- Weapon.java
+++ Weapon.java
@@ -1,11 +1,14 @@
+/**
+ * Represents a single weapon to be mounted on a mount.
+ */
 public class Weapon {
-  // TODO: fill out
-  private String weaponName;
+    // TODO: fill out
+    private String weaponName;
 
-  public String getWeaponName() {
-    return weaponName;
-  }
-  public void setWeaponName(String weaponName) {
-    this.weaponName = weaponName;
-  }
+    public String getWeaponName() {
+        return weaponName;
+    }
+    public void setWeaponName(String weaponName) {
+        this.weaponName = weaponName;
+    }
 }
