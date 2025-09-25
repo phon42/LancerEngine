174aa0c010bf367e38b00527c6a16072925b3aac
squid bacon <owenbchanas@gmail.com>
2025-09-24 19:23:13 -0500

    Update Mount.java

--- Mount.java
+++ Mount.java
@@ -1,42 +1,37 @@
 /**
- * Represents a single mount on a mech. Contains information such as what
- *     type this mount is, what weapon is mounted on it (if there is one),
- *     whether it has any modifications and whether there is a core bonus
- *     attached to it.
+ * Represents a single mount on a mech. Contains information such as what type
+ *     this mount is, what weapon is mounted on it (if there is one), whether it
+ *     has any modifications and whether there is a core bonus attached to it.
  */
 public class Mount {
-    private final String[] allowedMountTypes = {"aux", "aux/aux", "flex", "heavy", "integrated weapon", "main", "main/aux"};
-    // allowed values: "aux", "aux/aux", "flex", "heavy", "integrated weapon", "main", "main/aux"
+    private final String[] allowedMountTypes = {
+        "aux", "aux/aux", "flex", "heavy", "integrated weapon", "main", "main/aux"
+    };
+    /**
+     * String with allowed values:
+     *     "aux", "aux/aux", "flex", "heavy", "integrated weapon", "main", "main/aux"
+     * Cannot be null.
+     */
     private String mountType;
     private Weapon weapon;
     private boolean hasModification;
+    /**
+     * Can be any String, though "" is a placeholder. Cannot be null.
+     */
     private String modification;
     private boolean hasCoreBonus;
+    /**
+     * Can be any String, though "" is a placeholder. Cannot be null.
+     */
     private String coreBonus;
 
     public Mount() {
-        hasModification = false;
-        hasCoreBonus = false;
+        setModification("");
+        setCoreBonus("");
     }
 
     public String getMountType() {
         return mountType;
-    }
-    public void setMountType(String mountType) throws IllegalArgumentException {
-        boolean isValidType = false;
-        String exceptionMessage = "Given mount type is not one of the allowed mount types:\n\"Aux\", \"Aux/Aux\", \"Flex\", \"Heavy\", \"Integrated Weapon\", \"Main\", \"Main/Aux\"";
-
-        mountType = mountType.toLowerCase();
-        for (int i = 0; i < allowedMountTypes.length; i++) {
-            if (allowedMountTypes[i].equals(mountType)) {
-                isValidType = true;
-                break;
-            }
-        }
-        if (! isValidType) {
-            throw new IllegalArgumentException(exceptionMessage);
-        }
-        this.mountType = mountType;
     }
     public Weapon getWeapon() {
         return weapon;
@@ -53,12 +48,34 @@ public class Mount {
     public String getCoreBonus() {
         return coreBonus;
     }
+    public void setMountType(String mountType) throws IllegalArgumentException {
+        boolean isValidType = false;
+        String exceptionMessage = "Given mount type is not one of the allowed mount types:\n\"Aux\", \"Aux/Aux\", \"Flex\", \"Heavy\", \"Integrated Weapon\", \"Main\", \"Main/Aux\"";
+
+        if (mountType == null) {
+            throw new IllegalArgumentException("New mount type is null");
+        }
+        mountType = mountType.toLowerCase();
+        for (int i = 0; i < allowedMountTypes.length; i++) {
+            if (allowedMountTypes[i].equals(mountType)) {
+                isValidType = true;
+                break;
+            }
+        }
+        if (! isValidType) {
+            throw new IllegalArgumentException(exceptionMessage);
+        }
+        this.mountType = mountType;
+    }
     public void setWeapon(Weapon weapon) {
         this.weapon = weapon;
     }
-    // Setters for hasModification and hasCoreBonus removed
+    // Setters for hasModification and hasCoreBonus removed purposefully
     public void setModification(String modification) {
         if (modification == null) {
+            throw new IllegalArgumentException("New modification value is null");
+        }
+        if (modification == "") {
             hasModification = false;
         } else {
             hasModification = true;
@@ -66,7 +83,10 @@ public class Mount {
         this.modification = modification;
     }
     public void setCoreBonus(String coreBonus) {
-        if (coreBonus == null) {
+        if (modification == null) {
+            throw new IllegalArgumentException("New core bonus value is null");
+        }
+        if (coreBonus == "") {
             hasCoreBonus = false;
         } else {
             hasCoreBonus = true;
