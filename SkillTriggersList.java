8ea6f1f7a4d1a769cdd5315675f54c1e6546dfd6
squid bacon <owenbchanas@gmail.com>
2025-09-24 19:23:27 -0500

    Update SkillTriggersList.java

--- SkillTriggersList.java
+++ SkillTriggersList.java
@@ -7,16 +7,57 @@ public class SkillTriggersList {
     public SkillTrigger[] getSkillTriggers() {
         return skillTriggers;
     }
-    public void setSkillTriggers(SkillTrigger[] newSkillTriggers) {
-        skillTriggers = newSkillTriggers;
+    public void setSkillTriggers(SkillTrigger[] skillTriggers) {
+        if (skillTriggers == null) {
+            throw new IllegalArgumentException("New skill triggers value was null");
+        }
+        if (skillTriggers.length != 0) {
+            for (SkillTrigger skillTrigger : skillTriggers) {
+                if (skillTrigger == null) {
+                    throw new IllegalArgumentException("New skill triggers value included a null element");
+                }
+            }
+        }
+        this.skillTriggers = skillTriggers;
     }
 
+    public SkillTriggersList() {
+        setSkillTriggers(new SkillTrigger[0]);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        return true;
+    }
+    public boolean equals(SkillTriggersList skillTriggersList) {
+        if (skillTriggersList == null) {
+            return false;
+        }
+        if (skillTriggersList.getSkillTriggers().length == skillTriggers.length
+            && skillTriggers.length == 0) {
+            return true;
+        }
+        if (skillTriggersList.getSkillTriggers().length == skillTriggers.length) {
+            for (int i = 0; i < skillTriggers.length; i++) {
+                if (! skillTriggers[i].equals(skillTriggersList.getSkillTriggers()[i])) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+        return false;
+    }
     public String generateOutput() {
         String outputString = "";
 
-        if (skillTriggers == null) {
-            return outputString;
-        } else if (skillTriggers.length == 0) {
+        if (skillTriggers.length == 0) {
             return outputString;
         }
         for (int i = 0; i < skillTriggers.length; i += 2) {
@@ -25,14 +66,35 @@ public class SkillTriggersList {
                 if (ii != i) {
                     outputString += " ";
                 }
-                outputString += skillTriggers[ii].name + " (+" + skillTriggers[ii].value + ")";
+                outputString += skillTriggers[ii].getName() + " (+"
+                    + skillTriggers[ii].getValue() + ")";
                 if (ii + 1 < skillTriggers.length) {
                     outputString += ",";
                 }
             }
-            outputString += "\n";
+            if (i + 2 < skillTriggers.length) {
+                outputString += "\n";
+            }
         }
 
         return outputString;
+    }
+    public boolean isValid() {
+        if (skillTriggers == null) {
+            return false;
+        }
+        if (skillTriggers.length == 0) {
+            return true;
+        }
+        for (SkillTrigger skillTrigger : skillTriggers) {
+            if (skillTrigger.getName().equals("")) {
+                return false;
+            }
+            if (skillTrigger.getValue() == -1) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
