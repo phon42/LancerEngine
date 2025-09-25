ea7ba41c0b1898c3093dcb1d9fd482c13a4ace18
squid bacon <owenbchanas@gmail.com>
2025-09-24 19:23:04 -0500

    Update Mech.java

--- Mech.java
+++ Mech.java
@@ -1,20 +1,24 @@
 /**
- * Represents a single mech. Contains information about that mech's origin
- *     frame (the stat block after which it is patterned), its mounts, and
- *     its systems, among other statistics.
+ * Represents a single mech. Contains information about that mech's origin frame
+ *     (the stat block after which it is patterned), its mounts, and its
+ *     systems, among other statistics.
  */
 public class Mech {
     // frame that this mech is (i.e. Swallowtail)
-    Frame frame;
+    private Frame frame;
     
     // mounts/weapons
-    Mount[] mounts;
+    private Mount[] mounts;
 
     // systems
-    MechSystem[] systems;
+    private MechSystem[] systems;
 
     // frame attributes - size, structure, HP, etc.
-    /** Size is stored as 2 * its value (Size 1/2 would be stored as int 1) */
+    /**
+     * Represents the size of the mech.
+     * Size is stored as 2 * its value (Size 1/2 would be stored as int 1).
+     * Allowed values for size: -1 (placeholder), 1, 2, 4, 6, 8.
+     */
     private int size;
 
     // health and structure
@@ -51,166 +55,165 @@ public class Mech {
     private int limitedSystemsBonus;
 
     public Mech() {
-        currentStructure = 4;
-        maxStructure = 4;
-        currentStress = 4;
-        maxStress = 4;
+        setCurrentStructure(4);
+        setMaxStructure(4);
+        setCurrentStress(4);
+        setMaxStress(4);
     }
 
     public Frame getFrame() {
         return frame;
     }
-    public void setFrame(Frame newFrame) {
-        frame = newFrame;
+    public void setFrame(Frame frame) {
+        this.frame = frame;
     }
     public Mount[] getMounts() {
         return mounts;
     }
-    public void setMounts(Mount[] newMounts) {
-        mounts = newMounts;
+    public void setMounts(Mount[] mounts) {
+        this.mounts = mounts;
     }
     public MechSystem[] getSystems() {
         return systems;
     }
-    public void setSystems(MechSystem[] newSystems) {
-        systems = newSystems;
+    public void setSystems(MechSystem[] systems) {
+        this.systems = systems;
     }
     public int getSize() {
         return size;
     }
-    public void setSize(int newSize) {
-        size = newSize;
+    public void setSize(int size) {
+        this.size = size;
     }
     public int getCurrentStructure() {
         return currentStructure;
     }
-    public void setCurrentStructure(int newCurrentStructure) {
-        currentStructure = newCurrentStructure;
+    public void setCurrentStructure(int currentStructure) {
+        this.currentStructure = currentStructure;
     }
     public int getMaxStructure() {
         return maxStructure;
     }
-    public void setMaxStructure(int newMaxStructure) {
-        maxStructure = newMaxStructure;
+    public void setMaxStructure(int maxStructure) {
+        this.maxStructure = maxStructure;
     }
     public int getCurrentHP() {
         return currentHP;
     }
-    public void setCurrentHP(int newCurrentHP) {
+    public void setCurrentHP(int currentHP) {
         if (currentHP < 0) {
-            throw new IllegalArgumentException(
-                "Error: currentHP value provided: " + newCurrentHP + " is less than 0"
-            );
+            throw new IllegalArgumentException("Error: currentHP value provided: "
+                + currentHP + " is less than 0");
         } else if (currentHP > getMaxHP()) {
-            throw new IllegalArgumentException(
-                "Error: currentHP value provided: " + newCurrentHP + " is greater than maxHP value " + getMaxHP()
-            );
+            throw new IllegalArgumentException("Error: currentHP value provided: "
+                + currentHP + " is greater than maxHP value " + getMaxHP());
         } else {
-            currentHP = newCurrentHP;
+            this.currentHP = currentHP;
         }
     }
     public int getMaxHP() {
         return maxHP;
     }
-    public void setMaxHP(int newMaxHP) {
-        maxHP = newMaxHP;
+    public void setMaxHP(int maxHP) {
+        this.maxHP = maxHP;
     }
     public int getArmor() {
         return armor;
     }
-    public void setArmor(int newArmor) {
-        armor = newArmor;
+    public void setArmor(int armor) {
+        this.armor = armor;
     }
     public int getCurrentStress() {
         return currentStress;
     }
-    public void setCurrentStress(int newCurrentStress) {
-        currentStress = newCurrentStress;
+    public void setCurrentStress(int currentStress) {
+        this.currentStress = currentStress;
     }
     public int getMaxStress() {
         return maxStress;
     }
-    public void setMaxStress(int newMaxStress) {
-        maxStress = newMaxStress;
+    public void setMaxStress(int maxStress) {
+        this.maxStress = maxStress;
     }
     public int getCurrentHeatCapacity() {
         return currentHeatCapacity;
     }
-    public void setCurrentHeatCapacity(int newCurrentHeatCapacity) {
-        currentHeatCapacity = newCurrentHeatCapacity;
+    public void setCurrentHeatCapacity(int currentHeatCapacity) {
+        this.currentHeatCapacity = currentHeatCapacity;
     }
     public int getMaxHeatCapacity() {
         return maxHeatCapacity;
     }
-    public void setMaxHeatCapacity(int newMaxHeatCapacity) {
-        maxHeatCapacity = newMaxHeatCapacity;
+    public void setMaxHeatCapacity(int maxHeatCapacity) {
+        this.maxHeatCapacity = maxHeatCapacity;
     }
     public int getEvasion() {
         return evasion;
     }
-    public void setEvasion(int newEvasion) {
-        evasion = newEvasion;
+    public void setEvasion(int evasion) {
+        this.evasion = evasion;
     }
     public int getSpeed() {
         return speed;
     }
-    public void setSpeed(int newSpeed) {
-        speed = newSpeed;
+    public void setSpeed(int speed) {
+        this.speed = speed;
     }
     public int getEDefense() {
         return eDefense;
     }
-    public void setEDefense(int newEDefense) {
-        eDefense = newEDefense;
+    public void setEDefense(int eDefense) {
+        this.eDefense = eDefense;
     }
     public int getTechAttack() {
         return techAttack;
     }
-    public void setTechAttack(int newTechAttack) {
-        techAttack = newTechAttack;
+    public void setTechAttack(int techAttack) {
+        this.techAttack = techAttack;
     }
     public int getSensors() {
         return sensors;
     }
-    public void setSensors(int newSensors) {
-        sensors = newSensors;
+    public void setSensors(int sensors) {
+        this.sensors = sensors;
     }
     public int getCurrentRepairCapacity() {
         return currentRepairCapacity;
     }
-    public void setCurrentRepairCapacity(int newCurrentRepairCapacity) {
-        currentRepairCapacity = newCurrentRepairCapacity;
+    public void setCurrentRepairCapacity(int currentRepairCapacity) {
+        this.currentRepairCapacity = currentRepairCapacity;
     }
     public int getMaxRepairCapacity() {
         return maxRepairCapacity;
     }
-    public void setMaxRepairCapacity(int newMaxRepairCapacity) {
-        maxRepairCapacity = newMaxRepairCapacity;
+    public void setMaxRepairCapacity(int maxRepairCapacity) {
+        this.maxRepairCapacity = maxRepairCapacity;
     }
     public int getSaveTarget() {
         return saveTarget;
     }
-    public void setSaveTarget(int newSaveTarget) {
-        saveTarget = newSaveTarget;
+    public void setSaveTarget(int saveTarget) {
+        this.saveTarget = saveTarget;
     }
     public int getSystemPoints() {
         return systemPoints;
     }
-    public void setSystemPoints(int newSystemPoints) {
-        systemPoints = newSystemPoints;
+    public void setSystemPoints(int systemPoints) {
+        this.systemPoints = systemPoints;
     }
     public int getLimitedSystemsBonus() {
         return limitedSystemsBonus;
     }
-    public void setLimitedSystemsBonus(int newLimitedSystemsBonus) {
-        limitedSystemsBonus = newLimitedSystemsBonus;
+    public void setLimitedSystemsBonus(int limitedSystemsBonus) {
+        this.limitedSystemsBonus = limitedSystemsBonus;
     }
 
     public void calculateAttributes(int[] mechSkills) throws Exception {
         // TODO: fill out
         // Calculate this mech's attributes based off of the frame template provided
         if (frame == null) {
-            throw new Exception("calculateAttributes() was called while frame was set to null");
+            throw new Exception("calculateAttributes() was called while"
+                + " frame was set to null");
         }
         // Hull
         maxHP = frame.getHP() + (mechSkills[0] * 2);
@@ -252,7 +255,7 @@ public class Mech {
         return outputString;
     }
     public String outputWeapons(String outputType) {
-        // output something along the lines of
+        // output something along the lines of:
         // "  MAIN MOUNT: Vulture DMR // Overpower Caliber\n"
         // or "  INTEGRATED WEAPON: Nexus (Light)\n"
         String outputString = "";
@@ -279,7 +282,8 @@ public class Mech {
         }
         outputType = outputType.toLowerCase();
         if (outputType == "mech build") {
-            // output something like "  Pattern-A Smoke Charges x4, Seismic Ripper, High-Stress Mag Clamps, ATHENA-Class NHP"
+            // output something like:
+            //     "  Pattern-A Smoke Charges x4, Seismic Ripper, High-Stress Mag Clamps, ATHENA-Class NHP"
             for (int i = 0; i < systems.length; i++) {
                 if (i == 0) {
                     outputString += "  ";
@@ -290,7 +294,8 @@ public class Mech {
                 }
             }
         } else if (outputType == "full") {
-            // output something along the lines of "  Ammo Case II, Pattern-A Smoke Charges,\n"
+            // output something along the lines of:
+            //       "  Ammo Case II, Pattern-A Smoke Charges,\n"
             //     + "  Seismic Ripper, High-Stress Mag Clamps"
             for (int i = 0; i < systems.length; i += 2) {
                 outputString += "  ";
