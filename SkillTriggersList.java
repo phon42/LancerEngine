9a2eab5fd2629fb5b1ac55e3143306468ae18da0
squid bacon <owenbchanas@gmail.com>
2025-09-25 15:42:18 -0500

    All Pilot tests passing

--- SkillTriggersList.java
+++ SkillTriggersList.java
@@ -7,14 +7,21 @@ public class SkillTriggersList {
     public SkillTrigger[] getSkillTriggers() {
         return skillTriggers;
     }
+    /**
+     * Sets this.skillTriggers to the provided value.
+     * @param skillTriggers a SkillTrigger[] which cannot be null or contain an
+     *     element which is null.
+     */
     public void setSkillTriggers(SkillTrigger[] skillTriggers) {
         if (skillTriggers == null) {
-            throw new IllegalArgumentException("New skill triggers value was null");
+            throw new IllegalArgumentException("New skill triggers value is"
+                + " null");
         }
         if (skillTriggers.length != 0) {
             for (SkillTrigger skillTrigger : skillTriggers) {
                 if (skillTrigger == null) {
-                    throw new IllegalArgumentException("New skill triggers value included a null element");
+                    throw new IllegalArgumentException("New skill triggers"
+                        + " value includes a null element");
                 }
             }
         }
@@ -25,6 +32,12 @@ public class SkillTriggersList {
         setSkillTriggers(new SkillTrigger[0]);
     }
 
+    /**
+     * Compares this SkillTriggersList object and obj. If they are the same
+     *     class, returns true.
+     * @param obj an Object to be compared to.
+     * @return a boolean representing whether the two Objects are the same.
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
@@ -35,6 +48,13 @@ public class SkillTriggersList {
         }
         return true;
     }
+    /**
+     * Compares this SkillTriggersList object and skillTriggersList. If they
+     *     have the same property values, returns true.
+     * @param skillTriggersList a SkillTriggersList to be compared to.
+     * @return a boolean representing whether the two SkillTriggersLists are the
+     *     same.
+     */
     public boolean equals(SkillTriggersList skillTriggersList) {
         if (skillTriggersList == null) {
             return false;
@@ -43,9 +63,11 @@ public class SkillTriggersList {
             && skillTriggers.length == 0) {
             return true;
         }
-        if (skillTriggersList.getSkillTriggers().length == skillTriggers.length) {
+        if (skillTriggersList.getSkillTriggers().length == 
+            skillTriggers.length) {
             for (int i = 0; i < skillTriggers.length; i++) {
-                if (! skillTriggers[i].equals(skillTriggersList.getSkillTriggers()[i])) {
+                if (! skillTriggers[i].equals(
+                    skillTriggersList.getSkillTriggers()[i])) {
                     return false;
                 }
             }
@@ -54,10 +76,18 @@ public class SkillTriggersList {
         
         return false;
     }
+    /**
+     * Outputs the skill triggers contained in this object as a String.
+     * @return a String representing the output.
+     */
     public String generateOutput() {
+        // Output something like:
+        //     "  Apply Fists to Faces (+2), Assault (+2)\n"
+        //     "  Blow Something Up (+2)\n"
         String outputString = "";
 
         if (skillTriggers.length == 0) {
+            outputString += "  N/A\n";
             return outputString;
         }
         for (int i = 0; i < skillTriggers.length; i += 2) {
@@ -72,29 +102,30 @@ public class SkillTriggersList {
                     outputString += ",";
                 }
             }
-            if (i + 2 < skillTriggers.length) {
-                outputString += "\n";
-            }
+            outputString += "\n";
         }
 
         return outputString;
     }
-    public boolean isValid() {
-        if (skillTriggers == null) {
+    /**
+     * Tests whether this SkillTriggersList object's properties have any
+     *     placeholder values.
+     *     The SkillTriggersList doesn't have any placeholder values if the
+     *     object's SkillTriggers array does not contain any SkillTriggers with
+     *     name and value properties set to placeholder values.
+     * @return a boolean representing whether this SkillTriggersList object
+     *     has placeholders.
+     */
+    public boolean hasPlaceholders() {
+        if (skillTriggers.length == 0) {
             return false;
         }
-        if (skillTriggers.length == 0) {
-            return true;
-        }
         for (SkillTrigger skillTrigger : skillTriggers) {
-            if (skillTrigger.getName().equals("")) {
-                return false;
-            }
-            if (skillTrigger.getValue() == -1) {
-                return false;
+            if (skillTrigger.hasPlaceholders()) {
+                return true;
             }
         }
 
-        return true;
+        return false;
     }
 }
