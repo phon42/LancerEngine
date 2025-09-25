5ef6db4ab1597545c08e6577d59f1ada2f2e8c6c
squid bacon <owenbchanas@gmail.com>
2025-09-24 19:22:53 -0500

    Update License.java

--- License.java
+++ License.java
@@ -1,18 +1,51 @@
 /**
- * Represents a single license. Stores information about the license,
- *     such as the frame to which the license is held, and the level
- *     of the license.
+ * Represents a single license. Stores the frame to which the license is held,
+ *     and the level of the license.
  */
 public class License {
-    public String frame;
-    public int licenseLevel;
+    /**
+     * Can be any String, though "" is a placeholder. Cannot be null.
+     */
+    private String frame;
+    /**
+     * Must be between -1 (placeholder) and 3 (inclusive). Cannot be 0.
+     */
+    private int licenseLevel;
 
     public License() {
-        frame = "";
-        licenseLevel = 0;
+        setFrame("");
+        setLicenseLevel(-1);
     }
-    public License(String newFrame, int newLicenseLevel) {
-        frame = newFrame;
-        licenseLevel = newLicenseLevel;
+    public License(String frame, int licenseLevel) {
+        setFrame(frame);
+        setLicenseLevel(licenseLevel);
+    }
+
+    public String getFrame() {
+        return frame;
+    }
+    public int getLicenseLevel() {
+        return licenseLevel;
+    }
+    public void setLicenseLevel(int licenseLevel) {
+        if (licenseLevel < -1) {
+            throw new IllegalArgumentException("New value for license level "
+                + "was < -1");
+        }
+        if (licenseLevel == 0) {
+            throw new IllegalArgumentException("New value for license level "
+                + "was 0");
+        }
+        if (licenseLevel > 3) {
+            throw new IllegalArgumentException("New value for license level "
+                + "was > 3");
+        }
+        this.licenseLevel = licenseLevel;
+    }
+    public void setFrame(String frame) {
+        if (frame == null) {
+            throw new IllegalArgumentException("New value for frame was null");
+        }
+        this.frame = frame;
     }
 }
