cc9696391b4e829b0fe6e261d4edf39ea03026c5
squid bacon <owenbchanas@gmail.com>
2025-09-24 19:22:45 -0500

    Update HelperFunctions.java

--- HelperFunctions.java
+++ HelperFunctions.java
@@ -1,4 +1,6 @@
 public class HelperFunctions {
-    // TODO: fill out with a helper function that outputs a properly formatted String from an array
-    // there are bits of code that perform this function all over this project, i.e. Mech.outputSystems()
+    // TODO: fill out with a helper function that outputs a properly formatted
+    //     String from an array
+    // there are bits of code that perform this function all over this project,
+    //     i.e. Mech.outputSystems()
 }
