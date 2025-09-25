c3e4496fadaaf7644ba1a6abff8f26caf6a8ba15
squid bacon <owenbchanas@gmail.com>
2025-09-24 19:23:08 -0500

    Update MechSystem.java

--- MechSystem.java
+++ MechSystem.java
@@ -1,14 +1,26 @@
 /**
- * Contains a single system for a mech. Stores information about
- *     that system such as its name and its tags.
+ * Contains a single system for a mech. Stores information about that system
+ *     such as its name and its tags.
  */
 public class MechSystem {
     // TODO: fill out
+    /**
+     * Can be any String, though "" is a placeholder. Cannot be null.
+     */
     private String systemName;
 
     private boolean limited;
+    /**
+     * Must be > -1.
+     */
     private int limitedCharges;
 
+    public MechSystem() {
+        setSystemName("");
+        setLimited(false);
+        setLimitedCharges(0);
+    }
+    
     public String getSystemName() {
         return systemName;
     }
@@ -19,18 +31,24 @@ public class MechSystem {
         return limitedCharges;
     }
     public void setSystemName(String systemName) {
+        if (systemName == null) {
+            throw new IllegalArgumentException("New value for system name was null");
+        }
         this.systemName = systemName;
     }
     public void setLimited(boolean limited) {
         this.limited = limited;
     }
     public void setLimitedCharges(int limitedCharges) {
+        if (limitedCharges < 0) {
+            throw new IllegalArgumentException("New limited charges value is < 0");
+        }
         this.limitedCharges = limitedCharges;
     }
 
     /**
      * Outputs a printout of this specific mech system.
-     * @param @param outputType a String which can be one of the following:
+     * @param outputType a String which can be one of the following:
      *     "mech build", "pilot", or "full", and determines how much information
      *     is printed.
      * @return a String containing the printed out information of the system.
