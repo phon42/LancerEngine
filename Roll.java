139f2716f8d22aafe9c353b728a7e6217dc7f69b
phon42 <owenbchanas@gmail.com>
2025-10-14 11:49:53 -0500

    Added keepRolls() to Roll

--- Roll.java
+++ Roll.java
@@ -131,7 +131,11 @@ public final class Roll {
         for (int i = 0; i < rollNum; i++) {
             rolls[i] = roll(maxRoll);
         }
-        // TODO: implement keeping a certain number of rolls if keep != 0
+        if (keep != 0) {
+            keptRolls = keepRolls(rolls, keep, keepNum);
+        } else {
+            keptRolls = rolls;
+        }
         rolls = new int[keepNum + 1];
         for (int i = 0; i < keptRolls.length; i++) {
             total += keptRolls[i];
@@ -155,5 +159,87 @@ public final class Roll {
      */
     private static int[] rollComplex(int rollNum, int maxRoll) {
         return rollComplex(rollNum, maxRoll, 0, 0);
+    }
+    /**
+     * Take in some int[] and return an int[] of the (numToKeep) highest/lowest
+     *     ints, depending on (keep).
+     * @param rolls an int[] of rolls to be processed.
+     * @param keep an int which can be -1 or +1, designating "keep lowest" and
+     *     "keep highest", respectively.
+     * @param numToKeep an int containing the number of rolls to keep from the
+     *     array.
+     * @return an int[] containing the (numToKeep) highest/lowest ints,
+     *     depending on (keep).
+     */
+    private static int[] keepRolls(int[] rolls, int keep, int numToKeep) {
+        int[] keptRolls = new int[numToKeep];
+        int[] keptRoll = new int[2];
+        int[] tempRolls;
+        int numRolls;
+
+        if (keep == 0) {
+            throw new IllegalArgumentException("Roll.keepRolls() was called with a keep parameter of 0");
+        }
+        if (numToKeep >= rolls.length) {
+            throw new IllegalArgumentException("Roll.keepRolls() was called"
+                + " with a numToKeep parameter: " + numToKeep + " > the rolls"
+                + " parameter's length: " + rolls.length);
+        }
+        if (rolls.length > numToKeep / 2.0) {
+            // something like "8dYkh2", best strategy is to go through (2 times)
+            //     and grab the ("kh" -> maximum) each time
+            // For (numToKeep) times:
+            //     Go through a modified version of rolls (for (rolls.length))
+            //         Grab the maximum/minimum value
+            for (int i = 0; i < numToKeep; i++) {
+                // find the maximum/minimum value in rolls
+                for (int j = 0; j < rolls.length; j++) {
+                    if (j == 0 || (j != 0 && keep * rolls[j] > keptRoll[1])) {
+                        keptRoll[0] = j;
+                        keptRoll[1] = rolls[j];
+                    }
+                }
+                keptRolls[i] = keptRoll[1];
+                // remove the element at that index (keptRoll[0]) from rolls
+                tempRolls = new int[rolls.length - 1];
+                for (int j = 0; j < tempRolls.length; j++) {
+                    if (j < keptRoll[0]) {
+                        tempRolls[j] = rolls[j];
+                    } else if (j >= keptRoll[0]) {
+                        tempRolls[j] = rolls[j + 1];
+                    }
+                }
+                rolls = tempRolls;
+            }
+        } else {
+            // something like "8dYkh7", best strategy is to go through and grab
+            //     the ("kh" -> MINIMUM - grab the opposite of whatever it is)
+            //     each time
+            numRolls = rolls.length;
+            for (int i = 0; i < numRolls - numToKeep; i++) {
+                // find the maximum/minimum value in rolls (OPPOSITE of what it's supposed to be)
+                for (int j = 0; j < rolls.length; j++) {
+                    if (j == 0
+                        || (j != 0 && -1 * keep * rolls[j] > keptRoll[1])) {
+                        keptRoll[0] = j;
+                        keptRoll[1] = rolls[j];
+                    }
+                }
+                // remove the element at that index (keptRoll[0]) from rolls
+                tempRolls = new int[rolls.length - 1];
+                for (int j = 0; j < tempRolls.length; j++) {
+                    if (j < keptRoll[0]) {
+                        tempRolls[j] = rolls[j];
+                    } else if (j >= keptRoll[0]) {
+                        tempRolls[j] = rolls[j + 1];
+                    }
+                }
+                rolls = tempRolls;
+            }
+            keptRolls = rolls;
+        }
+
+
+        return keptRolls;
     }
 }
