09356ab6c7f2dc42c8cdacf12d6da124550f6ef3
phon42 <owenbchanas@gmail.com>
2025-10-20 16:40:35 -0500

    Added Preferences

---
+++ main/Preferences.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ * Contains a set of preferences/settings for the program.
+ */
+public class Preferences {
+    /**
+     * Whether all rolls should be given to the user to make with physical dice.
+     */
+    public static boolean makeRollsManually = false;
+    /**
+     * Whether the user should be notified whenever some actions occur that the
+     *     player can choose the order of.
+     */
+    public static boolean chooseOrderAutomatically = false;
+}
