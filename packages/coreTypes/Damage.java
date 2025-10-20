00c5840562ce29fd51d2fd50c3d1e840c90d0adb
phon42 <owenbchanas@gmail.com>
2025-10-20 16:37:09 -0500

    Updated Damage and Harm

--- packages/coreTypes/Damage.java
+++ packages/coreTypes/Damage.java
@@ -1,17 +1,18 @@
 package packages.coreTypes;
 
 import main.HelperMethods;
+import main.Roll;
 
 /**
  * See pgs. 67 and 104.
  */
 public class Damage {
     /**
-     * The Damage's type (i.e. "Blast").
+     * The Damage's type (i.e. "kinetic").
      * Must be an allowed type as defined by Damage.allowedTypes. Cannot be
      *     null.
      */
-    private String type;
+    protected String type;
     /**
      * Contains an array of allowed damage types.
      * Case-insensitive and stored in lowercase.
@@ -22,26 +23,31 @@ public class Damage {
     /**
      * The amount of dice damage dealt (i.e. "1d6", representing the "1d6" in
      *     "1d6+2").
-     * Can be any String that contains a valid dice expression. Can be "".
-     *     Cannot be null.
+     * Can be any String that contains a valid dice expression as defined by
+     *     Roll.isValidExpression(). Can be "" as long as this.flatValue is not
+     *     0. Cannot be null.
+     * Case-insensitive and stored in lowercase.
      */
-    private String diceValue;
+    protected String diceValue;
     /**
      * The amount of flat damage dealt (i.e. 2, representing the "2" in
      *     "1d6+2").
-     * Must be a minimum of -1.
+     * Must be a minimum of 1. Can be 0 as long as this.diceValue is a valid
+     *     dice expression as defined by Roll.isValidExpression().
      */
-    private int flatValue;
+    protected int flatValue;
 
     public Damage(String damageType, String damageDice, int damageFlatAmount) {
         setType(damageType);
         setDiceValue(damageDice);
         setFlatValue(damageFlatAmount);
+        checkValidity();
     }
     public Damage(Damage damage) {
         setType(damage.type);
         setDiceValue(damage.diceValue);
         setFlatValue(damage.flatValue);
+        checkValidity();
     }
     
     public String getType() {
@@ -57,40 +63,110 @@ public class Damage {
         HelperMethods.checkString("type", type);
         type = type.toLowerCase();
         // TODO: create methods like this for all the checkable final arrays
-        if (! Damage.isValid(type)) {
+        if (! Damage.isValidType(type)) {
             throw new IllegalArgumentException("type value: \"" + type + "\" is"
                 + " an invalid type");
         }
         this.type = type;
     }
+    /**
+     * Warning: this mutator DOES NOT protect against this.diceValue being set
+     *     to "" while this.flatValue is 0. It is assumed that this will be
+     *     taken care of in Damage() by calling Damage.checkValidity()
+     *     immediately after everything has been set.
+     * @param diceValue a String which must be "" or a valid dice expression as
+     *     defined by Roll.isValidExpression().
+     */
     protected void setDiceValue(String diceValue) {
+        boolean isBlank = false;
+        boolean isValidExpression = false;
+
         if (diceValue == null) {
             throw new IllegalArgumentException("diceValue is null");
         }
         diceValue = diceValue.toLowerCase();
-        // TODO: add some way of making sure diceValue is a valid dice
-        //     expression
-        if (diceValue.equals("") || true) {
-            this.diceValue = diceValue;
+        isBlank = diceValue.equals("");
+        isValidExpression = Roll.isValidExpression(diceValue);
+        if ((! isBlank) && (! isValidExpression)) {
+            // diceValue is neither "" nor a valid dice expression - not allowed
+            throw new IllegalArgumentException("Cannot set diceValue to a value"
+                + " of: \"" + diceValue + "\" (neither \"\" nor a valid dice"
+                + " expression)");
         }
+        this.diceValue = diceValue;
     }
     protected void setFlatValue(int flatValue) {
-        if (flatValue < -1) {
+        if (flatValue < 0) {
             throw new IllegalArgumentException("flat value value: " + flatValue
-                + " is < -1");
+                + " is < 0");
+        }
+        if (flatValue == 0) {
+            // diceValue must be a valid dice expression
+            if (! Roll.isValidExpression(this.diceValue)) {
+                throw new IllegalArgumentException("Cannot set flatValue to a"
+                    + " value of 0 while Damage.diceValue is: \""
+                    + this.diceValue + "\" (an invalid dice expression)");
+            }
+        } else {
+            // diceValue must be "" or a valid dice expression, which we have
+            //     already taken care of through Damage.setDiceValue()'s filters
+            // so no checking is necessary
         }
         this.flatValue = flatValue;
     }
 
-    public static boolean isValid(String type) {
-        boolean isValid = false;
-
+    /**
+     * Checks whether a provided String is a valid Damage.type value.
+     * @param type a String that cannot be null.
+     * @return a boolean containing the result of the check.
+     */
+    public static boolean isValidType(String type) {
+        if (type == null) {
+            throw new IllegalArgumentException("type is null");
+        }
         for (int i = 0; i < Damage.allowedTypes.length; i++) {
             if (type.equals(Damage.allowedTypes[i])) {
-                isValid = true;
+                return true;
             }
         }
+        return false;
+    }
+    /**
+     * Checks whether this.diceValue and this.flatValue are set to a valid
+     *     combination of values. Throws an IllegalArgumentException if not.
+     * @throws IllegalArgumentException if this.diceValue and this.flatValue are
+     *     set to an invalid combination of values.
+     */
+    private void checkValidity() {
+        if (! isValid()) {
+            throw new IllegalArgumentException("Cannot create a Damage object"
+                + " with a Damage.diceValue value of: \"" + this.diceValue
+                + "\" and a Damage.flatValue of: " + this.flatValue);
+        }
+    }
+    /**
+     * Checks whether this.diceValue and this.flatValue are set to a valid
+     *     combination of values.
+     * @return a boolean containing the result of the check.
+     */
+    private boolean isValid() {
+        boolean isValidExpression = false;
 
-        return isValid;
+        isValidExpression = Roll.isValidExpression(this.diceValue);
+        // this.diceValue must be a valid dice expression OR this.flatValue must
+        //     not be 0 (in which case this.diceValue can be "").
+        if (this.flatValue == 0) {
+            // this.diceValue must be a valid dice expression
+            if (! isValidExpression) {
+                return false;
+            }
+        } else {
+            // this.diceValue must be "" or a valid dice expression
+            // any in-between cases are taken care of by Damage.setDiceValue
+            // therefore we know that this.diceValue is EITHER "" or a valid
+            //     dice expression
+            // so no checking is necessary
+        }
+        return true;
     }
 }
--- packages/coreTypes/Harm.java
+++ packages/coreTypes/Harm.java
@@ -1,16 +1,18 @@
 package packages.coreTypes;
 
 import main.HelperMethods;
+import main.Roll;
 
 /**
  * See pgs. 67 and 104.
  */
 public class Harm extends Damage {
+    // TODO: figure out a way to override the documentation from Damage
     /**
-     * The Harm's type (i.e. "Blast").
+     * The Harm's type (i.e. "kinetic").
      * Must be an allowed type as defined by Harm.allowedTypes. Cannot be null.
      */
-    private String type;
+    // private String type;
     /**
      * Contains an array of allowed harm types.
      * Case-insensitive and stored in lowercase.
@@ -18,27 +20,39 @@ public class Harm extends Damage {
      */
     public static final String[] allowedTypes = new String[] {"kinetic",
         "explosive", "energy", "variable", "burn", "heat"};
+    // TODO: figure out a way to override the documentation from Damage
     /**
      * The amount of dice harm dealt (i.e. "1d6", representing the "1d6" in
      *     "1d6+2").
-     * Can be any String that contains a valid dice expression. Can be "".
-     *     Cannot be null.
+     * Can be any String that is "" or a valid dice expression as defined by
+     *     Roll.isValidExpression(). Cannot be null.
+     * Case-insensitive and stored in lowercase.
      */
-    private String diceValue;
+    // private String diceValue;
+    // TODO: figure out a way to override the documentation from Damage
     /**
      * The amount of flat harm dealt (i.e. 2, representing the "2" in
      *     "1d6+2").
-     * Must be a minimum of -1.
+     * Must be a minimum of 1. Can be -1 if this.diceValue is "". Can be 0 if
+     *     this.diceValue is a valid dice expression as defined by
+     *     Roll.isValidExpression().
      */
-    private int flatValue;
+    // private int flatValue;
 
     public Harm(String harmType, String harmDice, int harmFlatAmount) {
-        super("kinetic", harmDice, harmFlatAmount);
+        super("kinetic", "", 1);
         setType(harmType);
+        setDiceValue(harmDice);
+        setFlatValue(harmFlatAmount);
+        checkValidity();
     }
     public Harm(Harm harm) {
-        super(new Damage("kinetic", harm.diceValue, harm.flatValue));
+        super(new Damage("kinetic", "",
+            1));
         setType(harm.type);
+        setDiceValue(harm.diceValue);
+        setFlatValue(harm.flatValue);
+        checkValidity();
     }
 
     @Override
@@ -46,22 +60,117 @@ public class Harm extends Damage {
         HelperMethods.checkString("type", type);
         type = type.toLowerCase();
         // TODO: create methods like this for all the checkable final arrays
-        if (! Harm.isValid(type)) {
+        if (! Harm.isValidType(type)) {
             throw new IllegalArgumentException("type value: \"" + type + "\" is"
                 + " an invalid type");
         }
         this.type = type;
     }
+    /**
+     * Warning: this mutator DOES NOT protect any number of invalid cases. It is
+     *     assumed that this will be taken care of in Harm() by calling
+     *     Harm.checkValidity() immediately after everything has been set.
+     * @param diceValue a String which must be "" or a valid dice expression as
+     *     defined by Roll.isValidExpression(). Cannot be null.
+     */
+    @Override
+    protected void setDiceValue(String diceValue) {
+        boolean isBlank = false;
+        boolean isValidExpression = false;
 
-    public static boolean isValid(String type) {
-        boolean isValid = false;
+        if (diceValue == null) {
+            throw new IllegalArgumentException("diceValue is null");
+        }
+        diceValue = diceValue.toLowerCase();
+        isBlank = diceValue.equals("");
+        isValidExpression = Roll.isValidExpression(diceValue);
+        // (! isBlank) && (! isValidExpression) is equivalent to the below
+        //     expression
+        if (! (isBlank || isValidExpression)) {
+            // diceValue is neither "" nor a valid dice expression - not allowed
+            throw new IllegalArgumentException("Cannot set diceValue to a value"
+                + " of: \"" + diceValue + "\" (neither \"\" nor a valid dice"
+                + " expression)");
+        }
+        this.diceValue = diceValue;
+    }
+    /**
+     * Warning: this mutator DOES NOT protect any number of invalid cases. It is
+     *     assumed that this will be taken care of in Harm() by calling
+     *     Harm.checkValidity() immediately after everything has been set.
+     * @param flatValue an int which cannot be < -1.
+     */
+    @Override
+    protected void setFlatValue(int flatValue) {
+        if (flatValue < -1) {
+            throw new IllegalArgumentException("flat value value: " + flatValue
+                + " is < -1");
+        }
+        this.flatValue = flatValue;
+    }
 
+    /**
+     * Checks whether a provided String is a valid Harm.type value.
+     * @param type a String that cannot be null.
+     * @return a boolean containing the result of the check.
+     */
+    public static boolean isValidType(String type) {
+        if (type == null) {
+            throw new IllegalArgumentException("type is null");
+        }
         for (int i = 0; i < Harm.allowedTypes.length; i++) {
             if (type.equals(Harm.allowedTypes[i])) {
-                isValid = true;
+                return true;
             }
         }
+        return false;
+    }
+    /**
+     * Checks whether this.diceValue and this.flatValue are set to a valid
+     *     combination of values. Throws an IllegalArgumentException if not.
+     * @throws IllegalArgumentException if this.diceValue and this.flatValue are
+     *     set to an invalid combination of values.
+     */
+    private void checkValidity() {
+        if (! isValid()) {
+            throw new IllegalArgumentException("Cannot create a Harm object"
+                + " with a Harm.diceValue value of: \"" + this.diceValue + "\""
+                + " and a Harm.flatValue of: " + this.flatValue);
+        }
+    }
+    /**
+     * Checks whether this.diceValue and this.flatValue are set to a valid
+     *     combination of values.
+     * @return a boolean containing the result of the check.
+     */
+    private boolean isValid() {
+        boolean isValidExpression = false;
 
-        return isValid;
+        // isBlank must be (! isValidExpression) because (isBlank = false,
+        //     isValidExpression = false) is not possible because it is guarded
+        //     against in Harm.setDiceValue(), and (isBlank = true,
+        //     isValidExpression = true) is not possible because a blank String
+        //     is not a valid dice expression.
+        // Therefore we will only be using isValidExpression in this method to
+        //     determine whether this.diceValue and this.flatValue are set to a
+        //     valid combination of values.
+        isValidExpression = Roll.isValidExpression(this.diceValue);
+        if (! isValidExpression) {
+            // isValidExpression is false; therefore this.diceValue is ""
+            // this.flatValue is allowed to be -1 or any int > 0 in this case
+            if (this.flatValue == 0) {
+                // it is neither of those values
+                return false;
+            }
+        } else {
+            // isValidExpression is true; therefore this.diceValue is something
+            //     like "1d6"
+            // this.flatValue is allowed to be any int > 0
+            if (this.flatValue == -1) {
+                // it is not
+                return false;
+            }
+        }
+        return true;
     }
 }
