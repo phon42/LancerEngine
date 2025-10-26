50c363aa5aaad1fe5c4cda46f66d2a0d3ae1b50f
phon42 <owenbchanas@gmail.com>
2025-10-25 21:32:05 -0500

    Added BaseArea

---
+++ packages/coreTypes/battlefield/EntityArea.java
@@ -0,0 +1,31 @@
+package packages.coreTypes.battlefield;
+
+import main.HelperMethods;
+
+public class EntityArea extends BaseArea {
+    /**
+     * Contains an array of allowed values for EntityArea.type.
+     * Case-insensitive and stored in lowercase.
+     */
+    private static final String[] allowedTypes = {"size"};
+
+    public EntityArea() {
+        super();
+        setType("size");
+    }
+
+    @Override
+    protected void setType(String type) {
+        HelperMethods.checkString("type", type);
+        type = type.toLowerCase();
+        for (String allowedType : EntityArea.allowedTypes) {
+            if (type.equals(allowedType)) {
+                this.type = type;
+                this.isSpacesCurrent = false;
+                return;
+            }
+        }
+        throw new IllegalArgumentException("type value: \"" + type + "\" is"
+            + " not a valid value for EntityArea.type");
+    }
+}
