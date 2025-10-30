a1527e3a9f3cef63b10f6bfff288cac8f03f017a
phon42 <owenbchanas@gmail.com>
2025-10-30 16:32:10 -0500

    Updated Table

--- packages/coreTypes/Table.java
+++ packages/coreTypes/Table.java
@@ -1,5 +1,7 @@
 package packages.coreTypes;
 
+import main.HelperMethods;
+
 /**
  * Represents a single reference data table from an LCP. Contains information
  *     about the table's name and the table's data.
@@ -12,7 +14,55 @@ package packages.coreTypes;
  *     placeholder. None of its properties have allowed values of null.
  */
 public class Table {
-    // TODO: fill out
+    /**
+     * The table's name (i.e. "quirks").
+     * Can be any String except "". Cannot be null.
+     * Case-insensitive and stored in lowercase.
+     */
     private String name;
-    private Object[] data;
+    /**
+     * The table's data (too large to provide an example of).
+     * Can be any String[] that does not contain null elements or elements that
+     *     are "". Cannot be null.
+     * Elements are case-sensitive.
+     */
+    private String[] data;
+
+    public Table(String name, String[] data) {
+        setName(name);
+        setData(data);
+    }
+    public Table(Table table) {
+        setName(table.name);
+        setData(table.data);
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String[] getData() {
+        return HelperMethods.copyOf(data);
+    }
+    private void setName(String name) {
+        HelperMethods.checkString("name", name);
+        name = name.toLowerCase();
+        this.name = name;
+    }
+    private void setData(String[] data) {
+        if (data == null) {
+            throw new IllegalArgumentException("data is null");
+        }
+        for (String value : data) {
+            if (value == null) {
+                throw new IllegalArgumentException("data array contains a"
+                    + " null element");
+            }
+            if (value.equals("")) {
+                throw new IllegalArgumentException("data array contains a"
+                    + " element that is \"\"");
+            }
+        }
+        data = HelperMethods.copyOf(data);
+        this.data = data;
+    }
 }
