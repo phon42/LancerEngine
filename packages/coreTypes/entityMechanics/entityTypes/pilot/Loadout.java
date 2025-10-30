06872b72ab8bf27bb4e7b282d6597318dca10c1b
phon42 <owenbchanas@gmail.com>
2025-10-30 17:32:07 -0500

    Updated Loadout

--- packages/coreTypes/entityMechanics/entityTypes/pilot/Loadout.java
+++ packages/coreTypes/entityMechanics/entityTypes/pilot/Loadout.java
@@ -1,7 +1,9 @@
 package packages.coreTypes.entityMechanics.entityTypes.pilot;
 
-import main.Database;
 import main.HelperMethods;
+import packages.coreTypes.entityMechanics.entityTypes.pilot.loadout.pilotEquipment.PilotArmor;
+import packages.coreTypes.entityMechanics.entityTypes.pilot.loadout.pilotEquipment.PilotGear;
+import packages.coreTypes.entityMechanics.entityTypes.pilot.loadout.pilotEquipment.PilotWeapon;
 
 /**
  * See pgs. 108 - 114.
@@ -19,41 +21,44 @@ import main.HelperMethods;
  */
 public final class Loadout {
     /**
-     * The loadout's pilot armor (i.e. "Light Hardsuit").
-     * Can be any String. Cannot be null.
+     * The loadout's pilot armor (i.e. a PilotArmor representing the Light
+     *     Hardsuit).
+     * Can be any PilotArmor. Can be null.
      * 
      * See pgs. 108 - 109.
      */
-    private String pilotArmor;
+    private PilotArmor pilotArmor;
     /**
-     * The loadout's pilot weapons (i.e. {"Archaic Melee", "Archaic Ranged"}).
-     * Must be a String[] of length 2. Cannot be null or contain null elements.
+     * The loadout's pilot weapons (i.e. a PilotWeapon[] containing Archaic
+     *     Melee and Archaic Ranged).
+     * Must be a PilotWeapon[] of length 2. Cannot be null.
      * 
      * See pg. 110.
      */
-    private String[] pilotWeapons;
+    private PilotWeapon[] pilotWeapons;
     /**
-     * The loadout's pilot gear (i.e. {"Corrective", "Frag Grenades", "Patch"}).
-     * Must be a String[] of length 3. Cannot be null or contain null elements.
+     * The loadout's pilot gear (i.e. a PilotGear[] containing Corrective, Frag
+     *     Grenades, and Patch).
+     * Must be a PilotGear[] of length 3. Cannot be null.
      * 
      * See pgs. 112 - 114.
      */
-    private String[] pilotGear;
+    private PilotGear[] pilotGear;
 
     /**
      * Creates an empty Loadout.
      */
     public Loadout() {
-        setPilotArmor("");
-        setPilotWeapons(new String[] {"", ""});
-        setPilotGear(new String[] {"", "", ""});
+        setPilotArmor(null);
+        setPilotWeapons(new PilotWeapon[2]);
+        setPilotGear(new PilotGear[3]);
     }
     /**
      * Creates a non-empty Loadout using the provided pilotArmor,
      *     pilotWeapons, and pilotGear.
      */
-    public Loadout(String pilotArmor, String[] pilotWeapons,
-        String[] pilotGear) {
+    public Loadout(PilotArmor pilotArmor, PilotWeapon[] pilotWeapons,
+        PilotGear[] pilotGear) {
         setPilotArmor(pilotArmor);
         setPilotWeapons(pilotWeapons);
         setPilotGear(pilotGear);
@@ -68,24 +73,18 @@ public final class Loadout {
         this(loadout.pilotArmor, loadout.pilotWeapons, loadout.pilotGear);
     }
 
-    public String getPilotArmor() {
+    public PilotArmor getPilotArmor() {
         return pilotArmor;
     }
-    public String[] getPilotWeapons() {
+    public PilotWeapon[] getPilotWeapons() {
         return HelperMethods.copyOf(pilotWeapons);
     }
-    public String[] getPilotGear() {
+    public PilotGear[] getPilotGear() {
         return HelperMethods.copyOf(pilotGear);
     }
-    public void setPilotArmor(String pilotArmor) {
-        if (pilotArmor == null) {
-            throw new IllegalArgumentException("New pilot armor value is"
-                + " null");
-        }
-        if (! (pilotArmor.equals("")
-            || Database.isValidPilotArmor(pilotArmor))) {
-            throw new IllegalArgumentException("Pilot armor value: \""
-                + pilotArmor + "\" is an invalid value for Loadout.pilotArmor");
+    public void setPilotArmor(PilotArmor pilotArmor) {
+        if (pilotArmor != null) {
+            pilotArmor = new PilotArmor(pilotArmor);
         }
         this.pilotArmor = pilotArmor;
     }
@@ -96,7 +95,7 @@ public final class Loadout {
      * @throws IllegalArgumentException if pilotWeapons is null, of length other
      *     than 2, or contains a null element.
      */
-    public void setPilotWeapons(String[] pilotWeapons) {
+    public void setPilotWeapons(PilotWeapon[] pilotWeapons) {
         if (pilotWeapons == null) {
             throw new IllegalArgumentException("New pilot weapon value is"
                 + " null");
@@ -104,12 +103,6 @@ public final class Loadout {
         if (pilotWeapons.length != 2) {
             throw new IllegalArgumentException("New pilot weapon value is"
                 + " of invalid length: " + pilotWeapons.length);
-        }
-        for (String weapon : pilotWeapons) {
-            if (weapon == null) {
-                throw new IllegalArgumentException("New pilot weapon value"
-                    + " contains a null element");
-            }
         }
         pilotWeapons = HelperMethods.copyOf(pilotWeapons);
         this.pilotWeapons = pilotWeapons;
@@ -121,7 +114,7 @@ public final class Loadout {
      * @throws IllegalArgumentException if pilotGear is null, of length other
      *     than 3, or contains a null element.
      */
-    public void setPilotGear(String[] pilotGear) {
+    public void setPilotGear(PilotGear[] pilotGear) {
         if (pilotGear == null) {
             throw new IllegalArgumentException("New pilot gear value is"
                 + " null");
@@ -129,12 +122,6 @@ public final class Loadout {
         if (pilotGear.length != 3) {
             throw new IllegalArgumentException("New pilot gear value is"
                 + " of invalid length: " + pilotGear.length);
-        }
-        for (String gear : pilotGear) {
-            if (gear == null) {
-                throw new IllegalArgumentException("New pilot gear value"
-                    + " contains a null element");
-            }
         }
         pilotGear = HelperMethods.copyOf(pilotGear);
         this.pilotGear = pilotGear;
@@ -163,23 +150,36 @@ public final class Loadout {
      * @return a boolean representing whether the two Loadouts are the same.
      */
     public boolean equals(Loadout loadout) {
+        PilotWeapon weapon;
+        PilotWeapon[] weapons;
+        PilotGear gear;
+        PilotGear[] gearArray;
+
         if (loadout == null) {
             return false;
         }
         if (! loadout.getPilotArmor().equals(this.pilotArmor)) {
             return false;
         }
-        String weapon;
+        weapons = loadout.getPilotWeapons();
         for (int i = 0; i < this.pilotWeapons.length; i++) {
             weapon = this.pilotWeapons[i];
-            if (! weapon.equals(loadout.getPilotWeapons()[i])) {
+            if (weapon == null) {
+                if (weapons[i] != null) {
+                    return false;
+                }
+            } else if (! weapon.equals(weapons[i])) {
                 return false;
             }
         }
-        String gear;
+        gearArray = loadout.getPilotGear();
         for (int i = 0; i < pilotGear.length; i++) {
             gear = this.pilotGear[i];
-            if (! gear.equals(loadout.getPilotGear()[i])) {
+            if (gear == null) {
+                if (gearArray[i] != null) {
+                    return false;
+                }
+            } else if (! gear.equals(gearArray[i])) {
                 return false;
             }
         }
@@ -202,12 +202,10 @@ public final class Loadout {
         boolean test2;
         boolean test3;
 
-        test1 = (this.pilotArmor.equals(""));
-        test2 = (this.pilotWeapons[0].equals("")
-            && this.pilotGear[1].equals(""));
-        test3 = (this.pilotGear[0].equals("")
-            && this.pilotGear[1].equals("")
-            && this.pilotGear[2].equals(""));
+        test1 = this.pilotArmor == null;
+        test2 = this.pilotWeapons[0] == null && this.pilotGear[1] == null;
+        test3 = this.pilotGear[0] == null && this.pilotGear[1] == null
+            && this.pilotGear[2] == null;
         if (test1 && test2 && test3) {
             outputString += "  N/A\n";
             return outputString;
@@ -217,14 +215,14 @@ public final class Loadout {
         }
         if (! test2) {
             for (int i = 0; i < 2; i++) {
-                if (! this.pilotWeapons[i].equals("")) {
+                if (! (this.pilotWeapons[i] == null)) {
                     outputString += this.pilotWeapons[i] + ",";
                 }
             }
         }
         if (! test3) {
             for (int i = 0; i < 3; i++) {
-                if (! this.pilotGear[i].equals("")) {
+                if (! (this.pilotGear[i] == null)) {
                     outputString += this.pilotGear[i] + ",";
                 }
             }
@@ -232,7 +230,7 @@ public final class Loadout {
         outputArray = outputString.split(",");
         newOutputArray = new String[0];
         for (int i = 0; i < outputArray.length; i++) {
-            if (! outputArray[i].equals("")) {
+            if (! (outputArray[i] == null)) {
                 newOutputArray = HelperMethods.append(newOutputArray,
                     outputArray[i]);
             }
