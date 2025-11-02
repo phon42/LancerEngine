632099b73ffe599979e53218bc96f92e1511c158
phon42 <owenbchanas@gmail.com>
2025-11-02 13:55:16 -0600

    Updated Rule

--- packages/coreTypes/Rule.java
+++ packages/coreTypes/Rule.java
@@ -1,5 +1,7 @@
 package packages.coreTypes;
 
+import main.HelperMethods;
+
 /**
  * Represents a single "rule" - a piece of reference data from the rules.json
  *     file of a .lcp file. Contains the rule's name and data.
@@ -14,5 +16,32 @@ package packages.coreTypes;
 public class Rule {
     // TODO: fill out
     private String name;
-    private Object data; 
+    private Object data;
+
+    public Rule(String name, Object data) {
+        setName(name);
+        setData(data);
+    }
+    public Rule(Rule rule) {
+        this(rule.name, rule.data);
+    }
+
+    public String getName() {
+        return name;
+    }
+    public Object getData() {
+        // TODO: make a copy of data - kinda difficult because it has multiple
+        //     possible types
+        return data;
+    }
+    public void setName(String name) {
+        HelperMethods.checkString("name", name);
+        this.name = name;
+    }
+    public void setData(Object data) {
+        HelperMethods.checkObject("data", data);
+        // TODO: make a copy of data - kinda difficult because it has multiple
+        //     possible types
+        this.data = data;
+    }
 }
