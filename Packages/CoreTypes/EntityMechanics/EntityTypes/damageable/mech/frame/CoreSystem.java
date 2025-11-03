f932f2b740fcde7971a5d60315889eb4838bfea6
phon42 <owenbchanas@gmail.com>
2025-11-03 10:53:03 -0600

    Added CoreSystem

---
+++ Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/mech/frame/CoreSystem.java
@@ -0,0 +1,9 @@
+package Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.mech.frame;
+
+public class CoreSystem {
+    // TODO: fill out core system section - see pg. 33
+    // core system
+    // core system description
+    // core system passive
+    // core system active
+}
