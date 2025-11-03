0efa02cbf00493431e9cea6ec7bb9923b3c862e8
phon42 <owenbchanas@gmail.com>
2025-11-03 15:15:07 -0600

    Added TalentRank

---
+++ Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/pilot/talent/talentData/TalentRank.java
@@ -0,0 +1,21 @@
+package Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.pilot.talent.talentData;
+
+import Packages.CoreTypes.Counter;
+import Packages.CoreTypes.EntityMechanics.Action;
+import Packages.CoreTypes.EntityMechanics.Bonus;
+import Packages.CoreTypes.EntityMechanics.Synergy;
+
+public class TalentRank {
+    // Required properties
+    private String name;
+    private String description;
+
+    // Optional properties
+    private Synergy[] synergies;
+    private Action[] actions;
+    private Counter[] counters;
+    private Bonus[] bonuses;
+    private String[] integrated;
+    private boolean exclusive;
+    private String[] specialEquipment;
+}
