56bdd511bda77fb5048163f44dad3ab9307277fe
phon42 <owenbchanas@gmail.com>
2025-11-05 13:04:42 -0600

    Updated LCPManifest

--- Packages/CoreTypes/LCPManifest.java
+++ Packages/CoreTypes/LCPManifest.java
@@ -1,8 +1,14 @@
 package Packages.CoreTypes;
 
 public class LCPManifest {
+    // Required properties
     private String name;
     private String author;
     private HTMLString description;
     private Version version;
+    // Optional properties
+    private String itemPrefix;
+    private String imageURL;
+    private String website;
+    private LCPDependency[] dependencies;
 }
