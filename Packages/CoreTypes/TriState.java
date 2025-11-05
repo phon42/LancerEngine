2eab5f3e12da7aba42451e99352b4e0426bea6b1
phon42 <owenbchanas@gmail.com>
2025-11-05 15:41:04 -0600

    Updated TriState

--- Packages/CoreTypes/TriState.java
+++ Packages/CoreTypes/TriState.java
@@ -1,7 +1,28 @@
 package Packages.CoreTypes;
 
 public enum TriState {
-    TRUE,
-    FALSE,
-    UNSET
+    TRUE(1),
+    FALSE(0),
+    UNSET(-1);
+
+    private int value;
+
+    private TriState(int val) {
+        this.value = val;
+    }
+
+    public boolean toBoolean() {
+        if (this.value > -1) {
+            return this.value == 1;
+        }
+        throw new IllegalStateException("Cannot convert TriState.UNSET to a"
+            + " boolean");
+    }
+    public static TriState toTriState(boolean booleanValue) {
+        if (booleanValue) {
+            return TriState.TRUE;
+        }
+
+        return TriState.FALSE;
+    }
 }
