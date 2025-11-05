0a34e18bf1b1fd13df50b5cdc93024bbc9b6e4ed
phon42 <owenbchanas@gmail.com>
2025-11-05 14:34:21 -0600

    Added IActionData

---
+++ Packages/CoreTypes/EntityMechanics/Actions/actionBase/IActionData.java
@@ -0,0 +1,26 @@
+package Packages.CoreTypes.EntityMechanics.Actions.actionBase;
+
+import Packages.CoreTypes.Callable;
+import Packages.CoreTypes.HTMLString;
+import Packages.CoreTypes.EntityMechanics.Synergy;
+import Packages.CoreTypes.EntityMechanics.Actions.ActionBase;
+
+public class IActionData extends ActionBase {
+    // Optional properties
+    private int cost;
+    private boolean pilot;
+    private Synergy[] synergyLocations;
+    private boolean techAttack;
+    private String[] log;
+
+    public IActionData(String name, ActivationType activation,
+        HTMLString detailedDescription, Callable method, int cost,
+        boolean pilot, Synergy[] synergyLocations, boolean techAttack,
+        String[] log) {
+        super(name, activation, detailedDescription, method);
+    }
+    public IActionData(String name, ActivationType activation,
+        HTMLString detailedDescription) {
+        super(name, activation, detailedDescription);
+    }
+}
