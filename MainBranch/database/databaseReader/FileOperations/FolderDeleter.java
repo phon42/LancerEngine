a7903ad6c1c3a3696bf1ad84e7dfedc893132963
phon42 <owenbchanas@gmail.com>
2025-11-05 12:08:35 -0600

    Added FolderDeleter

---
+++ MainBranch/database/databaseReader/FileOperations/FolderDeleter.java
@@ -0,0 +1,8 @@
+package MainBranch.database.databaseReader.FileOperations;
+
+public class FolderDeleter {
+    // Prevent user from instantiating
+    private FolderDeleter() {}
+
+    public static void deleteFolder(String directoryPath) {}
+}
