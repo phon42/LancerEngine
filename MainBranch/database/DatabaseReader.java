786419d02a189857ff32b761976321a0c4c47bb5
phon42 <owenbchanas@gmail.com>
2025-11-08 17:27:35 -0600

    Moved most of DatabaseReader to FileParser

--- MainBranch/database/DatabaseReader.java
+++ MainBranch/database/DatabaseReader.java
@@ -1,13 +1,6 @@
 package MainBranch.database;
 
-import java.nio.file.Paths;
-
-import MainBranch.HelperMethods;
-import MainBranch.database.databaseReader.DataCaster;
-import MainBranch.database.databaseReader.FileOperations.FileReading;
-import MainBranch.database.databaseReader.FileOperations.fileReading.JSON;
-import MainBranch.database.databaseReader.FileOperations.fileReading.json.JSONArray;
-import MainBranch.database.databaseReader.FileOperations.fileReading.json.JSONObject;
+import MainBranch.database.databaseReader.FileOperations.FileReader;
 
 public class DatabaseReader {
     // 1. DatabaseReader's DatabaseReader.read() method is called by Database,
@@ -24,39 +17,6 @@ public class DatabaseReader {
     // 6. Feed that Object[] to DataCaster and thus eventually to DataCompiler
     // 7. Flush all the stored data from DatabaseReader
 
-    // All the data being held at the moment, as JSONObject[]s:
-    // ----very critical data:
-    private static JSONObject[] frameData;
-    private static JSONObject[] systemData;
-    private static JSONObject[] modificationData;
-    private static JSONObject[] weaponData;
-    // ----critical data:
-    private static JSONObject[] actionData;
-    private static JSONObject[] coreBonusData;
-    private static JSONObject[] dataTagData;
-    private static JSONObject[] manufacturerData;
-    private static JSONObject[] npcFeatureData;
-    private static JSONObject[] npcTemplateData;
-    private static JSONObject[] pilotEquipmentData;
-    private static JSONObject[] reserveData;
-    private static JSONObject[] skillData;
-    private static JSONObject[] stateData;
-    private static JSONObject[] talentData;
-    // ----less important data:
-    private static JSONObject[] environmentData;
-    private static JSONObject[] sitrepData;
-    // ----pretty unimportant data:
-    private static JSONObject[] backgroundData;
-    private static JSONObject[] bondData;
-    // ----just for reference:
-    private static JSONObject ruleData;
-    private static JSONObject[] termData;
-    private static JSONObject tableData;
-
-    static {
-        flushData();
-    }
-
     // Prevent user from instantiating this class
     private DatabaseReader() {}
 
@@ -66,168 +26,10 @@ public class DatabaseReader {
      *     .json files.
      * @param filePath a String which must contain a valid file path.
      */
-    public static void read(String filePath) {
-        // read the data
-        FileReading.readData(filePath);
-        // once you're done, send that data along to DataCaster, which sends it
-        //     along to DataCompiler
-        sendData();
+    public static void readLocal(String filePath) {
+        FileReader.read(filePath, false);
     }
-    /**
-     * Parse the provided .json data, then save its contents.
-     * @param jsonPath a String which contains the file path from which the JSON
-     *     data was extracted. Is assumed to be accurate. Cannot be null.
-     * @param fileData a String which contains the contents of a .json file. Is
-     *     assumed to be the contents of a .json file. Cannot be null.
-     */
-    public static void parseJSONFile(String jsonPath, String fileData) {
-        String fileName;
-        JSONObject[] data;
-        JSONArray jsonArray;
-
-        // Check whether jsonPath or fileData is null
-        HelperMethods.checkObject("jsonPath", jsonPath);
-        HelperMethods.checkObject("fileData", fileData);
-        // Extract the file name
-        fileName = Paths.get(jsonPath).toFile().getName();
-        // TODO: check to make sure this actually works as expected
-        if (fileName.indexOf(".") != -1) {
-            fileName = fileName.split("\\.")[0];
-        }
-        // Convert the file data to an object and then to a JSONObject[]
-        if (fileName.equals("rules")
-            || fileName.equals("tables")) {
-            // going to be receiving a JSONObject
-            data = new JSONObject[] {JSON.toJSONObject(fileData)};
-        } else {
-            // going to be receiving a JSONArray
-            jsonArray = JSON.toJSONArray(fileData);
-            data = new JSONObject[jsonArray.length()];
-            for (int i = 0; i < data.length; i++) {
-                data[i] = jsonArray.getJSONObject(i);
-            }
-        }
-        // either way, at this point we have data set to a JSONObject[] of our
-        //     data
-        // now we actually put that data into our static properties
-        saveJSONData(fileName, data);
-    }
-    private static void saveJSONData(String fileName, JSONObject[] data) {
-        // ----some critical data types:
-        if (fileName.equals("frames")) {
-            DatabaseReader.frameData = data;
-        } else if (fileName.equals("systems")) {
-            DatabaseReader.systemData = data;
-        } else if (fileName.equals("mods")) {
-            DatabaseReader.modificationData = data;
-        } else if (fileName.equals("weapons")) {
-            DatabaseReader.weaponData = data;
-        }
-        // ----the rest of the critical data types:
-        else if (fileName.equals("actions")) {
-            DatabaseReader.actionData = data;
-        } else if (fileName.equals("core_bonuses")) {
-            DatabaseReader.coreBonusData = data;
-        } else if (fileName.equals("tags")) {
-            DatabaseReader.dataTagData = data;
-        } else if (fileName.equals("manufacturers")) {
-            DatabaseReader.manufacturerData = data;
-        } else if (fileName.equals("npc_feature")) {
-            DatabaseReader.npcFeatureData = data;
-        } else if (fileName.equals("npc_template")) {
-            DatabaseReader.npcTemplateData = data;
-        } else if (fileName.equals("pilot_gear")) {
-            DatabaseReader.pilotEquipmentData = data;
-        } else if (fileName.equals("reserves")) {
-            DatabaseReader.reserveData = data;
-        } else if (fileName.equals("skills")) {
-            DatabaseReader.skillData = data;
-        } else if (fileName.equals("statuses")) {
-            DatabaseReader.stateData = data;
-        } else if (fileName.equals("talents")) {
-            DatabaseReader.talentData = data;
-        }
-        // ----less important
-        else if (fileName.equals("environments")) {
-            DatabaseReader.environmentData = data;
-        } else if (fileName.equals("sitreps")) {
-            DatabaseReader.sitrepData = data;
-        }
-        // ----almost unimportant
-        else if (fileName.equals("backgrounds")) {
-            DatabaseReader.backgroundData = data;
-        } else if (fileName.equals("bonds")) {
-            DatabaseReader.bondData = data;
-        }
-        // ----just for reference
-        else if (fileName.equals("rules")) {
-            DatabaseReader.ruleData = data[0];
-        } else if (fileName.equals("glossary")) {
-            DatabaseReader.termData = data;
-        } else if (fileName.equals("tables")) {
-            DatabaseReader.tableData = data[0];
-        }
-    }
-    private static void sendData() {
-        Object[] data;
-
-        data = new Object[] {
-            DatabaseReader.frameData,
-            DatabaseReader.systemData,
-            DatabaseReader.modificationData,
-            DatabaseReader.weaponData,
-            DatabaseReader.actionData,
-            DatabaseReader.coreBonusData,
-            DatabaseReader.dataTagData,
-            DatabaseReader.manufacturerData,
-            DatabaseReader.npcFeatureData,
-            DatabaseReader.npcTemplateData,
-            DatabaseReader.pilotEquipmentData,
-            DatabaseReader.reserveData,
-            DatabaseReader.skillData,
-            DatabaseReader.stateData,
-            DatabaseReader.talentData,
-            // ----less important
-            DatabaseReader.environmentData,
-            DatabaseReader.sitrepData,
-            // ----almost unimportant
-            DatabaseReader.backgroundData,
-            DatabaseReader.bondData,
-            // ----just for reference
-            DatabaseReader.ruleData,
-            DatabaseReader.termData,
-            DatabaseReader.tableData
-        };
-        flushData();
-        DataCaster.receiveData(data);
-    }
-    private static void flushData() {
-        // ----some of the critical data types:
-        DatabaseReader.frameData = new JSONObject[0];
-        DatabaseReader.systemData = new JSONObject[0];
-        DatabaseReader.modificationData = new JSONObject[0];
-        DatabaseReader.weaponData = new JSONObject[0];
-        // ----the rest of the critical data types:
-        DatabaseReader.actionData = new JSONObject[0];
-        DatabaseReader.coreBonusData = new JSONObject[0];
-        DatabaseReader.dataTagData = new JSONObject[0];
-        DatabaseReader.manufacturerData = new JSONObject[0];
-        DatabaseReader.npcFeatureData = new JSONObject[0];
-        DatabaseReader.npcTemplateData = new JSONObject[0];
-        DatabaseReader.pilotEquipmentData = new JSONObject[0];
-        DatabaseReader.reserveData = new JSONObject[0];
-        DatabaseReader.skillData = new JSONObject[0];
-        DatabaseReader.stateData = new JSONObject[0];
-        DatabaseReader.talentData = new JSONObject[0];
-        // ----less important
-        DatabaseReader.environmentData = new JSONObject[0];
-        DatabaseReader.sitrepData = new JSONObject[0];
-        // ----almost unimportant
-        DatabaseReader.backgroundData = new JSONObject[0];
-        DatabaseReader.bondData = new JSONObject[0];
-        // ----just for reference
-        DatabaseReader.ruleData = null;
-        DatabaseReader.termData = new JSONObject[0];
-        DatabaseReader.tableData = null;
+    public static void readExternal(String url) {
+        FileReader.read(url, true);
     }
 }
