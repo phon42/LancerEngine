43387fb910393dff64fe14f0216486d8304078fa
phon42 <owenbchanas@gmail.com>
2025-11-09 00:20:51 -0600

    Moved FileOperations.parseJSONText's code to ResourceParser

--- MainBranch/database/FileOperations.java
+++ MainBranch/database/FileOperations.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import MainBranch.database.fileOperations.DirectoryDeleter;
 import MainBranch.database.fileOperations.JSON;
+import MainBranch.database.fileOperations.ResourceParser;
 import MainBranch.database.fileOperations.ResourceReader;
 import MainBranch.database.fileOperations.ZIPUnzipper;
 import MainBranch.database.fileOperations.json.JSONException;
@@ -17,31 +18,7 @@ public class FileOperations {
         return ResourceReader.read(resourceLocator, external);
     }
     public static Object parseJSONText(String jsonData) {
-        Object result;
-
-        if (jsonData.charAt(0) == '[') {
-            // it's a JSONArray (hopefully)
-            // if it throws a JSONException, it's fine, just let it go off, it's
-            //     a RuntimeException
-            try {
-                result = JSON.toJSONArray(jsonData);
-                return result;
-            } catch (JSONException exception) {
-                throw new IllegalArgumentException("jsonData contained \"[\""
-                    + " but was not a JSONArray, causing a JSONException with"
-                    + " the following message: \"" + exception.getMessage()
-                    + "\"");
-            }
-        }
-        try {
-            result = JSON.toJSONObject(jsonData);
-            return result;
-        } catch (JSONException exception) {
-            throw new IllegalArgumentException("jsonData did not contain \"[\""
-                + " but was not a JSONObject, causing a JSONException with the"
-                + " following message: \"" + exception.getMessage()
-                + "\"");
-        }
+        return ResourceParser.parseJSONText(jsonData);
     }
     public static void deleteDirectory(String directoryPath) {
         DirectoryDeleter.deleteDirectory(directoryPath);
--- MainBranch/database/fileOperations/ResourceParser.java
+++ MainBranch/database/fileOperations/ResourceParser.java
@@ -6,4 +6,31 @@ public class ResourceParser {
     // prevent user from instantiating this class
     private ResourceParser() {}
 
+    public static Object parseJSONText(String jsonData) {
+        Object result;
+
+        if (jsonData.charAt(0) == '[') {
+            // it's a JSONArray (hopefully)
+            // if it throws a JSONException, it's fine, just let it go off, it's
+            //     a RuntimeException
+            try {
+                result = JSON.toJSONArray(jsonData);
+                return result;
+            } catch (JSONException exception) {
+                throw new IllegalArgumentException("jsonData contained \"[\""
+                    + " but was not a JSONArray, causing a JSONException with"
+                    + " the following message: \"" + exception.getMessage()
+                    + "\"");
+            }
+        }
+        try {
+            result = JSON.toJSONObject(jsonData);
+            return result;
+        } catch (JSONException exception) {
+            throw new IllegalArgumentException("jsonData did not contain \"[\""
+                + " but was not a JSONObject, causing a JSONException with the"
+                + " following message: \"" + exception.getMessage()
+                + "\"");
+        }
+    }
 }
