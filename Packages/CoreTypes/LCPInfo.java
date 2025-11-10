7068a38bd0cc73d2bd883910cd521f4826d4a1aa
phon42 <owenbchanas@gmail.com>
2025-11-10 15:06:33 -0600

    Updated LCPInfo

--- Packages/CoreTypes/LCPInfo.java
+++ Packages/CoreTypes/LCPInfo.java
@@ -8,6 +8,11 @@ public class LCPInfo {
     private Version version;
     // Semi-required (optional but has a specific default value other than null
     //     when not provided) properties
+    /**
+     * Unknown purpose. Only present in the base Lancer lcp, where its value is
+     *     true.
+     * Default value: true.
+     */
     private boolean active;
     // Optional properties
     private String itemPrefix;
