3f03f51d8e4480418bc26b3d7caee45c003a53e1
phon42 <owenbchanas@gmail.com>
2025-11-10 10:51:19 -0600

    Updated LCPInfo

--- Packages/CoreTypes/LCPInfo.java
+++ Packages/CoreTypes/LCPInfo.java
@@ -1,5 +1,17 @@
 package Packages.CoreTypes;
 
 public class LCPInfo {
-    
+    // Required properties
+    private String name;
+    private String author;
+    private HTMLString description;
+    private Version version;
+    // Semi-required (optional but has a specific default value other than null
+    //     when not provided) properties
+    private boolean active;
+    // Optional properties
+    private String itemPrefix;
+    private String imageURL;
+    private String website;
+    private LCPDependency[] dependencies;
 }
