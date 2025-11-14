6059efe5bc57900dfe26c85b38df0fc2cac45bc5
phon42 <owenbchanas@gmail.com>
2025-11-14 17:40:27 -0600

    Updated Version

--- Packages/CoreTypes/lcpInfo/Version.java
+++ Packages/CoreTypes/lcpInfo/Version.java
@@ -1,6 +1,11 @@
 package Packages.CoreTypes.lcpInfo;
 
+/**
+ * From https://github.com/massif-press/lancer-data/blob/master/README.md#lcp-structure.
+ */
 public class Version {
+    // TODO: consider having SemverVersion extend this
+    // TODO: fill out
     public Version(String input) {}
     public Version(Version version) {}
 }
