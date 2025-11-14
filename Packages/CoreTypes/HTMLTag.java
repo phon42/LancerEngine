df35d922e1518c485de4feff4d4dbe820c8b7fbe
phon42 <owenbchanas@gmail.com>
2025-11-14 15:54:38 -0600

    Added HTMLTag

---
+++ Packages/CoreTypes/HTMLTag.java
@@ -0,0 +1,48 @@
+package Packages.CoreTypes;
+
+import MainBranch.HelperMethods;
+
+public class HTMLTag {
+    /**
+     * The tag name of this tag (i.e. "p").
+     * Can be any String except "". Cannot be null.
+     * Case-insensitive and stored in lowercase.
+     * Cannot contain any whitespace.
+     */
+    private String name;
+    /**
+     * Whether this tag is a closing tag (in other words, if it's of the form
+     *     "</abc>" instead of "<abc>").
+     * Can be any boolean.
+     */
+    private boolean closingTag;
+
+    public HTMLTag(String tagName, boolean isClosingTag) {
+        setName(tagName);
+        setClosingTag(isClosingTag);
+    }
+    public HTMLTag(HTMLTag htmlTag) {
+        setName(htmlTag.name);
+        setClosingTag(htmlTag.closingTag);
+    }
+
+    public String getName() {
+        return name;
+    }
+    public boolean isClosingTag() {
+        return closingTag;
+    }
+    private void setName(String name) {
+        HelperMethods.checkString("name", name);
+        name = name.toLowerCase();
+        this.name = name;
+    }
+    private void setClosingTag(boolean closingTag) {
+        this.closingTag = closingTag;
+    }
+
+    @Override
+    public String toString() {
+        return "<" + (closingTag ? "/" : "") + name + ">";
+    }
+}
