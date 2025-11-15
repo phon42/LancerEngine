7abd936569083b7a7beb6f31b75afda8a73ac91d
phon42 <owenbchanas@gmail.com>
2025-11-14 21:56:22 -0600

    Added Cache

---
+++ MainBranch/database/DatabaseReadingPipeline/CacheSystem/Cache.java
@@ -0,0 +1,15 @@
+package MainBranch.database.DatabaseReadingPipeline.CacheSystem;
+
+import MainBranch.HelperMethods;
+
+public class Cache {
+    // Prevent user from instantiating
+    private Cache() {}
+
+    public static String getLCP(String lcpName) {
+        // TODO: fill out
+        HelperMethods.checkString("lcpName", lcpName);
+
+        return null;
+    }
+}
