ab918138c3394e7fef33316f6214cfe553c4b071
phon42 <owenbchanas@gmail.com>
2025-11-15 03:59:35 -0600

    Updated DirectoryExplorer

--- MainBranch/database/fileOperations/DirectoryExplorer.java
+++ MainBranch/database/fileOperations/DirectoryExplorer.java
@@ -1,6 +1,35 @@
 package MainBranch.database.fileOperations;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
 public class DirectoryExplorer {
     // Prevent user from instantiating
     private DirectoryExplorer() {}
+
+    public static Stream<Path> listContents(String path) throws
+        SecurityException {
+        // Created using
+        //     https://docs.oracle.com/javase/8/docs/api/java/nio/file/Files.html#walk-java.nio.file.Path-int-java.nio.file.FileVisitOption...-
+        Path directoryPath;
+        Stream<Path> stream;
+
+        directoryPath = FileSystems.getDefault().getPath(path);
+        directoryPath = directoryPath.normalize().toAbsolutePath();
+        try {
+            stream = Files.walk(directoryPath, 1);
+        } catch (IllegalArgumentException exception) {
+            throw new IllegalArgumentException("maxDepth argument of"
+                + " Files.walk(Path, int) is negative");
+        } catch (IOException exception) {
+            throw new IllegalStateException("Attempting to walk the directory"
+                + " threw an IOException with the following message: \""
+                + exception.getMessage() + "\"");
+        }
+
+        return stream;
+    }
 }
