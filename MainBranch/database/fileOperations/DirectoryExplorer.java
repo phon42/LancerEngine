f1d4b757a7a0416965f41193c208bdd84b6c4ab5
phon42 <owenbchanas@gmail.com>
2025-11-15 03:59:48 -0600

    Updated DirectoryExplorer

--- MainBranch/database/fileOperations/DirectoryExplorer.java
+++ MainBranch/database/fileOperations/DirectoryExplorer.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.stream.Stream;
 
 public class DirectoryExplorer {
@@ -31,5 +33,38 @@ public class DirectoryExplorer {
         }
 
         return stream;
+    }
+    public static Iterator<Path> listContentsAsIterator(String path) throws
+        SecurityException {
+        Stream<Path> stream;
+
+        stream = listContents(path);
+        return stream.iterator();
+    }
+    public static Path[] listContentsAsArray(String path) throws
+        SecurityException {
+        ArrayList<Path> arrayList;
+        Path[] array;
+
+        arrayList = listContentsAsArrayList(path);
+        array = new Path[arrayList.size()];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = arrayList.get(i);
+        }
+
+        return array;
+    }
+    public static ArrayList<Path> listContentsAsArrayList(String path) throws
+        SecurityException {
+        Iterator<Path> iterator;
+        ArrayList<Path> arrayList;
+
+        iterator = listContentsAsIterator(path);
+        arrayList = new ArrayList<>();
+        while (iterator.hasNext()) {
+            arrayList.add(iterator.next());
+        }
+
+        return arrayList;
     }
 }
