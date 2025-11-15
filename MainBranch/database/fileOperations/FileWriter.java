906b47fd38b8578bd34230e5b5ddf8120acf6a14
phon42 <owenbchanas@gmail.com>
2025-11-15 01:26:59 -0600

    Updated FileWriter

--- MainBranch/database/fileOperations/FileWriter.java
+++ MainBranch/database/fileOperations/FileWriter.java
@@ -1,6 +1,32 @@
 package MainBranch.database.fileOperations;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FileWriter {
     // Prevent user from instantiating
     private FileWriter() {}
+
+    public static String writeToFile(String filePath, String data) {
+        // Created using
+        //     https://www.baeldung.com/java-write-to-file#write-with-files-class
+        Path path;
+        byte[] dataBytes;
+
+        path = Paths.get(filePath);
+        path = path.toAbsolutePath();
+        filePath = path.toString();
+        dataBytes = data.getBytes();
+        try {
+            Files.write(path, dataBytes);
+        } catch (IOException exception) {
+            throw new IllegalStateException("Attempting to write to file at"
+                + " path: \"" + filePath + "\" threw an IOException with the"
+                + " following message: \"" + exception.getMessage() + "\"");
+        }
+
+        return filePath;
+    }
 }
