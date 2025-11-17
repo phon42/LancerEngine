6ddedb2b5ef81cde8516a5104299b1b5bb212fab
phon42 <owenbchanas@gmail.com>
2025-11-17 00:56:14 -0600

    Added SynergyLocation

---
+++ Packages/CoreTypes/EntityMechanics/SynergyLocation.java
@@ -0,0 +1,9 @@
+package Packages.CoreTypes.EntityMechanics;
+
+public class SynergyLocation {
+    // TODO: fill out
+    String value;
+
+    public SynergyLocation(String value) {}
+    public SynergyLocation(SynergyLocation synergyLocation) {}
+}
