bfcfe43ad891fa53b9b680920ea5f8343c02d8ad
phon42 <owenbchanas@gmail.com>
2025-11-16 22:13:15 -0600

    Updated Cache

--- MainBranch/database/DatabaseReadingPipeline/CacheSystem/Cache.java
+++ MainBranch/database/DatabaseReadingPipeline/CacheSystem/Cache.java
@@ -2,6 +2,7 @@ package MainBranch.database.DatabaseReadingPipeline.CacheSystem;
 
 import java.nio.file.Path;
 import MainBranch.HelperMethods;
+import MainBranch.UserPreferences;
 import MainBranch.database.FileOperations;
 import MainBranch.database.fileOperations.json.JSONException;
 import MainBranch.database.fileOperations.json.JSONObject;
@@ -15,9 +16,87 @@ public class Cache {
      *     provided name. If found, returns the LCP's local directory path in
      *     the cache directory. Otherwise, returns null.
      */
-    public static String getLCP(String lcpName) {
-        // TODO: fill out
+    public static Path getLCP(String lcpName) {
+        Path cachePath;
+        Path[] directoryContents;
+        Path lcpInfoPath;
+
         HelperMethods.checkString("lcpName", lcpName);
+        // The starting point - the cache directory
+        cachePath = UserPreferences.getCacheDir();
+        cachePath = cachePath.normalize().toAbsolutePath();
+        // Now list all files under that directory
+        directoryContents = FileOperations.listDirectoryContentsAsArray(
+            cachePath, true);
+        for (Path currElement : directoryContents) {
+            if (! currElement.toFile().isDirectory()) {
+                continue;
+            }
+            lcpInfoPath = findAndCheck(lcpName,
+                "info.json", currElement);
+            if (lcpInfoPath != null) {
+                return lcpInfoPath;
+            }
+            lcpInfoPath = findAndCheck(lcpName,
+                "lcp_manifest.json", currElement);
+            if (lcpInfoPath != null) {
+                return lcpInfoPath;
+            }
+        }
+
+        return null;
+    }
+    private static Path findAndCheck(String lcpName,
+        String fileNameToSearchFor, Path searchDirectory) {
+        Path lcpInfoPath;
+        Object[] lcpInfoJSON;
+        String name;
+
+        lcpInfoPath = findFileWithName(fileNameToSearchFor, searchDirectory);
+        if (lcpInfoPath != null) {
+            lcpInfoJSON = FileOperations.readAndParseResource(
+                lcpInfoPath.toString(), false, false);
+            try {
+                HelperMethods.checkObjectArray("lcpInfoJSON",
+                    lcpInfoJSON);
+            } catch (IllegalArgumentException exception) {
+                throw new IllegalStateException("Attempting to read one of the"
+                    + " files in the cache returned null values of some kind");
+            }
+            if (lcpInfoJSON.length != 1) {
+                throw new IllegalStateException("Attempting to read one"
+                    + " of the files in the cache returned either no"
+                    + " content or the contents of more than 1 file");
+            }
+            if (lcpInfoJSON[0] instanceof JSONObject) {
+                try {
+                    name =
+                        ((JSONObject) lcpInfoJSON[0])
+                        .getString("name");
+                    if (lcpName.equals(name)) {
+                        // we've found it!
+                        return searchDirectory;
+                    }
+                } catch (JSONException exception) {
+                    // found a file with the right name but it didn't have a
+                    //     "name" property
+                    // throw an exception if you want but I'm not gonna
+                }
+            }
+        }
+
+        return null;
+    }
+    private static Path findFileWithName(String fileString,
+        Path directoryToSearch) {
+        Path[] files;
+
+        files = FileOperations.getAllFilenamesInDirectory(directoryToSearch);
+        for (Path file : files) {
+            if (fileString.equals(file.getFileName().toString())) {
+                return file;
+            }
+        }
 
         return null;
     }
