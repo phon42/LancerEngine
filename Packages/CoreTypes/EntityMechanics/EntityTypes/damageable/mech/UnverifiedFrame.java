7fb6b05e134282a32ae357ef501e26ab4a9928d2
phon42 <owenbchanas@gmail.com>
2025-11-20 10:49:00 -0600

    Added UnverifiedFrame

---
+++ Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/mech/UnverifiedFrame.java
@@ -0,0 +1,6 @@
+package Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.mech;
+
+import Packages.CoreTypes.EntityMechanics.LicenseSystem.frameLicense.LicenseContent;
+
+public final class UnverifiedFrame extends LicenseContent {
+}
