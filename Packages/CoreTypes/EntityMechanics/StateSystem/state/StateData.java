41a1e533516a42b554be14c91829757811aeb9eb
phon42 <owenbchanas@gmail.com>
2025-11-22 19:57:26 -0600

    Updated StateData

--- Packages/CoreTypes/EntityMechanics/StateSystem/state/StateData.java
+++ Packages/CoreTypes/EntityMechanics/StateSystem/state/StateData.java
@@ -63,4 +63,65 @@ public class StateData {
         }
         this.stateEffects = stateEffects;
     }
+    /**
+     * Adds a provided StateData effect to this.stateEffects.
+     * @param stateEffects a StateData which cannot be null.
+     * @throws IllegalArgumentException if effect is null.
+     */
+    public void addEffect(StateData stateEffects) {
+        HelperMethods.checkObject("stateEffects", stateEffects);
+        setEffects(HelperMethods.append(this.stateEffects, stateEffects));
+    }
+    public StateData removeEffect(int index) {
+        StateData removedEffect;
+        StateData[] effects;
+
+        if (this.effects.length == 0) {
+            throw new IllegalArgumentException("Attempted to call"
+                + " State.removeEffect() when this.effects.length is 0");
+        }
+        if (index < 0 || index > this.effects.length) {
+            throw new IllegalArgumentException("index value: " + index + " is"
+                + " out of bounds for a State[] of length: "
+                + this.effects.length);
+        }
+        removedEffect = this.effects[index];
+        effects = new State[this.effects.length - 1];
+        for (int i = 0; i < effects.length; i++) {
+            if (i < index) {
+                effects[i] = this.effects[i];
+                continue;
+            }
+            if (i > index) {
+                effects[i] = this.effects[i - 1];
+            }
+        }
+        setEffects(effects);
+
+        return removedEffect;
+    }
+    /**
+     * Recursively checks whether any of the States this State has caused, or
+     *     any of the States they have caused, and so on, are of State.name
+     *     stateName.
+     * @param stateName a String containing a State.name value to search for.
+     *     Cannot be "" or null.
+     * @return a boolean containing the result of the check.
+     */
+    public boolean hasState(String stateName) {
+        boolean isPresent = false;
+
+        HelperMethods.checkObject("stateName", stateName);
+        // TODO: fill out once Database has an array of States set up
+        // stateName is a valid State
+        for (State state : this.effects) {
+            if (state.getName().equals(stateName)) {
+                isPresent = true;
+                break;
+            }
+            isPresent = isPresent || state.hasState(stateName);
+        }
+
+        return isPresent;
+    }
 }
