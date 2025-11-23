463994ccbc0be074d3742d20d2dd400dcb0a5def
phon42 <owenbchanas@gmail.com>
2025-11-22 19:50:36 -0600

    Updated State

--- Packages/CoreTypes/EntityMechanics/StateSystem/State.java
+++ Packages/CoreTypes/EntityMechanics/StateSystem/State.java
@@ -2,6 +2,7 @@ package Packages.CoreTypes.EntityMechanics.StateSystem;
 
 import MainBranch.HelperMethods;
 import Packages.CoreTypes.EntityMechanics.StateSystem.state.Condition;
+import Packages.CoreTypes.EntityMechanics.StateSystem.state.StateData;
 import Packages.CoreTypes.EntityMechanics.StateSystem.state.Status;
 
 /**
@@ -21,80 +22,45 @@ import Packages.CoreTypes.EntityMechanics.StateSystem.state.Status;
  */
 public class State {
     /**
-     * This State's name (i.e. "Immobilized" or "Danger Zone").
-     * Can be any String except "". Cannot be null.
-     * Case-sensitive.
+     * TODO: Add documentation
      */
-    protected String name;
-
-    /**
-     * The source of this state (i.e. "Taro's mech").
-     * Can be any String except "". Cannot be null.
-     */
-    protected String source;
-
+    protected StateData data;
     /**
      * The State (if there is one) that caused this State to exist.
      * Can be any State. Can be null.
      */
     protected State causedBy;
     /**
-     * Any States which this State causes to exist.
-     * Can be any State[] that does not contain null elements. Cannot be null.
+     * The source of this state (i.e. "Taro's mech").
+     * Can be any String except "". Can be null if this.causedBy is null.
      */
-    protected State[] effects;
-
+    protected String source;
     /**
-     * The duration of this state (i.e. "1 round").
+     * The duration of this State (i.e. "1 round").
      * Must be a valid String as defined by State.allowedDurations.
-     * Case-insensitive and stored in lowercase.
+     * Case-insensitive and stored in lowercase. Cannot be null.
      */
     protected String duration;
-    /**
-     * Contains an array of possible values for this.duration.
-     * - "source" - The life time of the source 
-     * Case-insensitive and stored in lowercase.
-     */
-    protected static final String[] allowedDurations = new String[] {"1 round",
-        "1 turn", "permanent", "until removed", "source"};
 
     public State(String name, String source, String duration) {
-        setName(name);
-        setSource(source);
         setCausedBy(null);
-        setEffects(new State[0]);
+        setSource(source);
         setDuration(duration);
     }
     public State(State state) {
-        setName(state.name);
-        setSource(state.source);
         setCausedBy(state.causedBy);
-        setEffects(state.effects);
+        setSource(state.source);
         setDuration(state.duration);
     }
 
-    public String getName() {
-        return name;
+    public State getCausedBy() {
+        return causedBy;
     }
     public String getSource() {
         return source;
     }
-    public State getCausedBy() {
-        return causedBy;
-    }
-    public State[] getEffects() {
-        return effects;
-    }
     public String getDuration() {
         return duration;
-    }
-    protected void setName(String name) {
-        HelperMethods.checkString("New name", name);
-        this.name = name;
-    }
-    protected void setSource(String source) {
-        HelperMethods.checkString("source", source);
-        this.source = source;
     }
     protected void setCausedBy(State causedBy) {
         if (causedBy != null) {
@@ -102,21 +68,9 @@ public class State {
         }
         this.causedBy = causedBy;
     }
-    /**
-     * Sets this.effects to the provided value.
-     * @param effects a State[] which cannot be null, be of length 0, or contain
-     *     null elements.
-     * @throws IllegalArgumentException if effects is null, of length 0, or
-     *     contains null elements.
-     */
-    protected void setEffects(State[] effects) {
-        HelperMethods.checkObjectArray("New effects", effects);
-        if (effects.length > 0) {
-            effects = HelperMethods.copyOf(effects);
-        } else {
-            effects = new State[0];
-        }
-        this.effects = effects;
+    protected void setSource(String source) {
+        HelperMethods.checkString("source", source);
+        this.source = source;
     }
     /**
      * Sets this.duration to the provided value.
@@ -135,7 +89,7 @@ public class State {
             }
         }
         throw new IllegalArgumentException("New duration value is an invalid"
-            + " value: \"" + name + "\"");
+            + " value: \"" + duration + "\"");
     }
 
     /**
@@ -161,97 +115,8 @@ public class State {
      * @return a boolean representing whether the two States are the same.
      */
     public boolean equals(State state) {
-        if (state == null) {
-            return false;
-        }
-        if (! this.name.equals(state.getName())) {
-            return false;
-        }
-        if (! this.source.equals(state.getSource())) {
-            return false;
-        }
-        if (this.causedBy == null) {
-            if (state.getCausedBy() != null) {
-                return false;
-            }
-        } else if (! this.causedBy.equals(state.getCausedBy())) {
-            return false;
-        }
-        if (this.effects.length != state.getEffects().length) {
-            return false;
-        } else {
-            for (int i = 0; i < this.effects.length; i++) {
-                if (! this.effects[i].equals(state.getEffects()[i])) {
-                    return false;
-                }
-            }
-        }
-        if (! this.duration.equals(state.getDuration())) {
-            return false;
-        }
-        
+        // TODO: fill out
         return true;
-    }
-    /**
-     * Adds a provided State effect to this.effects.
-     * @param effect a State which cannot be null.
-     * @throws IllegalArgumentException if effect is null.
-     */
-    public void addEffect(State effect) {
-        HelperMethods.checkObject("effect", effect);
-        setEffects(HelperMethods.append(this.effects, effect));
-    }
-    public State removeEffect(int index) {
-        State removedEffect;
-        State[] effects;
-
-        if (this.effects.length == 0) {
-            throw new IllegalArgumentException("Attempted to call"
-                + " State.removeEffect() when this.effects.length is 0");
-        }
-        if (index < 0 || index > this.effects.length) {
-            throw new IllegalArgumentException("index value: " + index + " is"
-                + " out of bounds for a State[] of length: "
-                + this.effects.length);
-        }
-        removedEffect = this.effects[index];
-        effects = new State[this.effects.length - 1];
-        for (int i = 0; i < effects.length; i++) {
-            if (i < index) {
-                effects[i] = this.effects[i];
-                continue;
-            }
-            if (i > index) {
-                effects[i] = this.effects[i - 1];
-            }
-        }
-        setEffects(effects);
-
-        return removedEffect;
-    }
-    /**
-     * Recursively checks whether any of the States this State has caused, or
-     *     any of the States they have caused, and so on, are of State.name
-     *     stateName.
-     * @param stateName a String containing a State.name value to search for.
-     *     Cannot be "" or null.
-     * @return a boolean containing the result of the check.
-     */
-    public boolean hasState(String stateName) {
-        boolean isPresent = false;
-
-        HelperMethods.checkObject("stateName", stateName);
-        // TODO: fill out once Database has an array of States set up
-        // stateName is a valid State
-        for (State state : this.effects) {
-            if (state.getName().equals(stateName)) {
-                isPresent = true;
-                break;
-            }
-            isPresent = isPresent || state.hasState(stateName);
-        }
-
-        return isPresent;
     }
     public Condition toCondition() {
         // TODO: fill out, throw an Exception if it can't be converted
