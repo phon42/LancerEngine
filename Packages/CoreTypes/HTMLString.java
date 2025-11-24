c197671dfe3cbb7e309a1a05260deb59038f5c9d
phon42 <owenbchanas@gmail.com>
2025-11-24 14:42:30 -0600

    Updated HTMLString

--- Packages/CoreTypes/HTMLString.java
+++ Packages/CoreTypes/HTMLString.java
@@ -1,8 +1,8 @@
 package Packages.CoreTypes;
 
 import java.util.ArrayList;
-
 import MainBranch.HelperMethods;
+import Packages.CoreTypes.htmlString.HTMLTag;
 
 public class HTMLString implements Comparable<HTMLString>, CharSequence {
     /**
