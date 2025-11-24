877e4cdec987138ccb4c8300d70c657d613b409a
phon42 <owenbchanas@gmail.com>
2025-11-24 15:05:59 -0600

    Updated VueHTMLString

--- Packages/CoreTypes/VueHTMLString.java
+++ Packages/CoreTypes/VueHTMLString.java
@@ -75,11 +75,155 @@ public class VueHTMLString implements Comparable<VueHTMLString>, CharSequence {
     }
     private Object[] calculateDataSequence() {
         // TODO: fill out
-        return new Object[0];
+        ArrayList<Object> sequence;
+        String rawValue;
+        String workingString;
+        boolean isClosingTag = false;
+        HTMLTag workingTag;
+        Object[] result;
+
+        sequence = new ArrayList<>();
+        // We will represent the possible value of rawValue through the method
+        //     as follows:
+        // "   " will represent some amount of whitespace of any kind, including
+        //     ""
+        // "abc" will represent some amount of non-whitespace characters that
+        //     are not '<', '/', or '>'
+        // "<" will represent itself
+        // "/" will represent itself
+        // ">" will represent itself
+        // "..." will represent some amount of content that could contain any of
+        //     the above
+        rawValue = this.rawValue;
+        // At this point, rawValue can be any amount of "   abc   ..." possibly
+        //     including a tag or not
+        sequence.add("");
+        while (! rawValue.isEmpty()) {
+            if (rawValue.indexOf("<") == -1) {
+                // rawValue is "   abc   " (does not contain a valid tag but
+                //     might contain '/' or '>')
+                addElement(sequence, rawValue);
+                rawValue = "";
+            } else if (rawValue.indexOf("<") > 0) {
+                // rawValue is "   abc   <..."
+                // remove "   abc   " off the front, leaving "<..."
+                // now we can add "   abc   " exactly as in the above case
+                workingString = rawValue.substring(0,
+                    rawValue.indexOf("<"));
+                rawValue = rawValue.substring(rawValue.indexOf("<"));
+                addElement(sequence, workingString);
+            } else {
+                // rawValue.indexOf("<") == 0
+                // rawValue is "<..."
+                if (rawValue.indexOf(">") == -1) {
+                    // rawValue does not contain a valid tag but does contain
+                    //     '<' and possibly '/'
+                    // it's essentially the same as "   abc   " because there's
+                    //     no valid tag here
+                    // add rawValue as in the rawValue = "   abc   " case
+                    addElement(sequence, rawValue);
+                    rawValue = "";
+                } else {
+                    // rawValue is "<   abc   >..."
+                    // we clip the "<   abc   >" off of rawValue and give that
+                    //     to workingString
+                    workingString = rawValue.substring(0,
+                        rawValue.indexOf(">") + 1);
+                    rawValue = rawValue.substring(rawValue.indexOf(">")
+                        + 1);
+                    // rawValue is now "..."
+                    // workingString is now "<   abc   >"
+                    // therefore workingString should in theory contain the raw
+                    //     material required for an HTMLTag
+                    workingString = workingString.substring(1,
+                        workingString.length() - 1);
+                    // workingString is now "   abc   "
+                    workingString = workingString.stripLeading();
+                    // workingString is now "abc   "
+                    if (workingString.indexOf("/") == 0) {
+                        // workingString is "/   abc   "
+                        workingString = workingString.substring(1);
+                        // workingString is "   abc   "
+                        if (workingString.indexOf("/") != -1
+                            || workingString.indexOf("<") != -1) {
+                            // workingString was originally of the form
+                            //     "<//>" or "</<>" which are both invalid
+                            // TODO: add exception message
+                            throw new IllegalStateException();
+                        } else {
+                            // workingString currently contains only "   " and
+                            //     "abc"
+                            // in other words, the original was of the form
+                            //     "</   abc   >"
+                            // which is valid
+                        }
+                        isClosingTag = true;
+                    } else if (workingString.indexOf("/") > 0) {
+                        // workingString is "abc/   abc   " which is invalid
+                        // TODO: add exception message
+                        throw new IllegalStateException();
+                    } else {
+                        // workingString.indexOf("/") == -1
+                        // workingString contains only "   " and "abc"
+                        // the original was of the form "<   abc   >" which is
+                        //     valid
+                        // it's not a closing tag but isClosingTag has a default
+                        //     value of false so nothing needs to be done
+                    }
+                    workingTag = new HTMLTag(workingString, isClosingTag);
+                    addElement(sequence, workingTag);
+                }
+            }
+        }
+        result = new Object[sequence.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = sequence.get(i);
+        }
+
+        return result;
     }
-    private Object[] copyDataSequence(Object[] dataSequence) {
+    private void addElement(ArrayList<Object> arrayList, Object newElement) {
+        Object lastElement;
+        boolean isLastElementString = false;
+        boolean isNewElementString = false;
+        int lastElementIndex;
+        String modifiedString;
+
+        HelperMethods.checkObject("arrayList", arrayList);
+        HelperMethods.checkObject("newElement", newElement);
+        lastElement = arrayList.get(arrayList.size() - 1);
+        HelperMethods.checkObject("lastElement", lastElement);
+        if (lastElement instanceof String) {
+            isLastElementString = true;
+        }
+        if (newElement instanceof String) {
+            isNewElementString = true;
+        }
+        lastElementIndex = arrayList.size() - 1;
+        if (isLastElementString && isNewElementString) {
+            modifiedString = (String) lastElement;
+            modifiedString += (String) newElement;
+            arrayList.set(lastElementIndex, modifiedString);
+        } else {
+            arrayList.add(newElement);
+        }
+    }
+    private Object[] copyDataSequence(Object[] original) {
         // TODO: fill out
-        return dataSequence;
+        Object[] copy;
+
+        HelperMethods.checkObjectArray("original", original);
+        copy = new Object[original.length];
+        for (int i = 0; i < original.length; i++) {
+            if (original[i] instanceof String) {
+                copy[i] = original[i];
+            } else {
+                // original is an HTMLTag
+                copy[i] = new HTMLTag((HTMLTag) original[i]);
+            }
+        }
+
+        return copy;
     }
     public static VueHTMLString format(VueHTMLString vueHTMLString,
         Object... args) {
