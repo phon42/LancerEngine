c9abedcffb4ce460164437dae66ee7813408df34
phon42 <owenbchanas@gmail.com>
2025-11-25 13:33:31 -0600

    Updated JSONType

--- Packages/CoreTypes/jsonTypeTree/JSONType.java
+++ Packages/CoreTypes/jsonTypeTree/JSONType.java
@@ -1,10 +1,47 @@
 package Packages.CoreTypes.jsonTypeTree;
 
+import java.math.BigDecimal;
+import MainBranch.database.fileOperations.json.JSONArray;
+import MainBranch.database.fileOperations.json.JSONObject;
+
 public enum JSONType {
+    NULL,
     BOOLEAN,
     INTEGER,
     BIGDECIMAL,
     STRING,
     JSONOBJECT,
-    JSONARRAY
+    JSONARRAY;
+
+    public static JSONType determineType(Object value) {
+        if (value == null) {
+            return JSONType.NULL;
+        }
+        if (value instanceof Boolean) {
+            return JSONType.BOOLEAN;
+        // TODO: find out how to do this check properly
+        } else if (value instanceof boolean) {
+            return JSONType.BOOLEAN;
+        }
+        if (value instanceof Integer) {
+            return JSONType.INTEGER;
+        // TODO: find out how to do this check properly
+        } else if (value instanceof int) {
+            return JSONType.INTEGER;
+        }
+        if (value instanceof BigDecimal) {
+            return JSONType.BIGDECIMAL;
+        }
+        if (value instanceof String) {
+            return JSONType.STRING;
+        }
+        if (value instanceof JSONObject) {
+            return JSONType.JSONOBJECT;
+        }
+        if (value instanceof JSONArray) {
+            return JSONType.JSONARRAY;
+        }
+        throw new IllegalArgumentException("value is of a type that is not"
+            + " one of the accepted JSONType values");
+    }
 }
