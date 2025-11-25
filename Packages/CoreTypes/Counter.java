830a9411e9132b86e300e6ea9351728e6fd59c68
phon42 <owenbchanas@gmail.com>
2025-11-25 12:53:28 -0600

    Updated Counter

--- Packages/CoreTypes/Counter.java
+++ Packages/CoreTypes/Counter.java
@@ -3,6 +3,7 @@ package Packages.CoreTypes;
 import MainBranch.HelperMethods;
 
 public class Counter {
+    // Main properties
     /**
      * The id of this counter (i.e. "ctr_brawler").
      * Can be any String except "". Cannot be null.
@@ -15,53 +16,201 @@ public class Counter {
      * Case-sensitive.
      */
     private String name;
-    /**
-     * The default value of this counter (i.e. 6).
-     * Can be any int.
-     */
-    private int defaultValue;
+
+    // Semi-required (optional but has a specific default value other than null
+    //     when not provided) properties
     /**
      * The minimum value of this counter (i.e. 1).
      * Can be any int.
      */
     private int min;
+    private static final int minDefault = Integer.MIN_VALUE;
     /**
      * The maximum value of this counter (i.e. 6).
      * Can be any int.
      */
     private int max;
+    private static final int maxDefault = Integer.MAX_VALUE;
 
-    public Counter(String id, String name, int defaultValue, int minimumValue,
-        int maximumValue) {
-        setID(id);
-        setName(name);
-        setDefaultValue(defaultValue);
+    // Technically optional property but since it's an int we're gonna have to
+    //     do something different
+    /**
+     * The default value of this counter (i.e. 6).
+     * Can be any int.
+     * 
+     * Is only used when this.defaultValuePresent is true.
+     */
+    private int defaultValue;
+
+    // Helper property
+    /**
+     * Whether this.defaultValue should be used.
+     */
+    private boolean defaultValuePresent;
+
+    /**
+     * Counter.min:              PROVIDED
+     * Counter.max:              PROVIDED
+     * Counter.defaultValue:     PROVIDED
+     */
+    public Counter(String id, String name, int minimumValue, int maximumValue,
+        int defaultValue) {
+        // Main properties
+        this(id, name);
+        // Semi-required properties
         setMin(minimumValue);
         setMax(maximumValue);
+        // Technically optional property
+        setDefaultValue(defaultValue);
+        // Helper property
+        setDefaultValuePresent(true);
+    }
+    /**
+     * Counter.min:              PROVIDED
+     * Counter.max:              PROVIDED
+     * Counter.defaultValue: NOT PROVIDED
+     */
+    public Counter(String id, String name, int minimumValue, int maximumValue) {
+        // Main properties
+        this(id, name);
+        // Semi-required properties
+        setMin(minimumValue);
+        setMax(maximumValue);
+        // Technically optional property
+        setDefaultValue(0);
+        // Helper property
+        setDefaultValuePresent(false);
+    }
+    /**
+     * Counter.min:              PROVIDED
+     * Counter.max:          NOT PROVIDED
+     * Counter.defaultValue:     PROVIDED
+     */
+    public Counter(String id, String name, int minimumValue,
+        Object maximumValue, int defaultValue) {
+        // Main properties
+        this(id, name);
+        // Semi-required properties
+        setMin(minimumValue);
+        setMax(Counter.maxDefault);
+        // Technically optional property
+        setDefaultValue(defaultValue);
+        // Helper property
+        setDefaultValuePresent(true);
+    }
+    /**
+     * Counter.min:              PROVIDED
+     * Counter.max:          NOT PROVIDED
+     * Counter.defaultValue: NOT PROVIDED
+     */
+    public Counter(String id, String name, int minimumValue) {
+        // Main properties
+        this(id, name);
+        // Semi-required properties
+        setMin(minimumValue);
+        setMax(Counter.maxDefault);
+        // Technically optional property
+        setDefaultValue(0);
+        // Helper property
+        setDefaultValuePresent(false);
+    }
+    /**
+     * Counter.min:          NOT PROVIDED
+     * Counter.max:              PROVIDED
+     * Counter.defaultValue:     PROVIDED
+     */
+    public Counter(String id, String name, Object minimumValue,
+        int maximumValue, int defaultValue) {
+        // Main properties
+        this(id, name);
+        // Semi-required properties
+        setMin(Counter.minDefault);
+        setMax(maximumValue);
+        // Technically optional property
+        setDefaultValue(defaultValue);
+        // Helper property
+        setDefaultValuePresent(true);
+    }
+    /**
+     * Counter.min:          NOT PROVIDED
+     * Counter.max:              PROVIDED
+     * Counter.defaultValue: NOT PROVIDED
+     */
+    public Counter(String id, String name, Object minimumValue,
+        int maximumValue) {
+        // Main properties
+        this(id, name);
+        // Semi-required properties
+        setMin(Counter.minDefault);
+        setMax(maximumValue);
+        // Technically optional property
+        setDefaultValue(0);
+        // Helper property
+        setDefaultValuePresent(false);
+    }
+    /**
+     * Counter.min:          NOT PROVIDED
+     * Counter.max:          NOT PROVIDED
+     * Counter.defaultValue:     PROVIDED
+     */
+    public Counter(String id, String name, Object minimumValue,
+        Object maximumValue, int defaultValue) {
+        // Main properties
+        this(id, name);
+        // Semi-required properties
+        setMin(Counter.minDefault);
+        setMax(Counter.maxDefault);
+        // Technically optional property
+        setDefaultValue(defaultValue);
+        // Helper property
+        setDefaultValuePresent(true);
+    }
+    /**
+     * Counter.min:          NOT PROVIDED
+     * Counter.max:          NOT PROVIDED
+     * Counter.defaultValue: NOT PROVIDED
+     */
+    public Counter(String id, String name) {
+        // Main properties
+        setID(id);
+        setName(name);
+        // Semi-required properties
+        setMin(Counter.minDefault);
+        setMax(Counter.maxDefault);
+        // Technically optional property
+        setDefaultValue(0);
+        // Helper property
+        setDefaultValuePresent(false);
     }
     public Counter(Counter counter) {
-        setID(counter.id);
-        setName(counter.name);
-        setDefaultValue(counter.defaultValue);
-        setMin(counter.min);
-        setMax(counter.max);
+        this(counter.id, counter.name, counter.min, counter.max,
+            counter.defaultValue);
+        setDefaultValuePresent(counter.defaultValuePresent);
     }
 
+    // Main properties
     public String getID() {
         return id;
     }
     public String getName() {
         return name;
     }
-    public int getDefaultValue() {
-        return defaultValue;
-    }
+    // Semi-required properties
     public int getMin() {
         return min;
     }
     public int getMax() {
         return max;
     }
+    // Technically optional property
+    public int getDefaultValue() {
+        return defaultValue;
+    }
+    // Helper property
+    public boolean isDefaultValuePresent() {
+        return defaultValuePresent;
+    }
+    // Main properties
     public void setID(String id) {
         HelperMethods.checkString("id", id);
         id = id.toLowerCase();
@@ -71,13 +220,19 @@ public class Counter {
         HelperMethods.checkString("name", name);
         this.name = name;
     }
-    public void setDefaultValue(int defaultValue) {
-        this.defaultValue = defaultValue;
-    }
+    // Semi-required properties
     public void setMin(int min) {
         this.min = min;
     }
     public void setMax(int max) {
         this.max = max;
+    }
+    // Technically optional property
+    public void setDefaultValue(int defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+    // Helper property
+    private void setDefaultValuePresent(boolean defaultValuePresent) {
+        this.defaultValuePresent = defaultValuePresent;
     }
 }
