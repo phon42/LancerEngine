28e7063062b0781f3e5dbdb13ac932840d5f458a
phon42 <owenbchanas@gmail.com>
2025-11-25 12:16:04 -0600

    Added WeaponType

---
+++ Packages/CoreTypes/EntityMechanics/WeaponType.java
@@ -0,0 +1,55 @@
+package Packages.CoreTypes.EntityMechanics;
+
+import MainBranch.HelperMethods;
+
+public class WeaponType {
+    /**
+     * The weapon type's int id (i.e. 0 for a CQB weapon).
+     * Can be any int.
+     * 
+     * Currently used values:
+     *     0 - CQB
+     *     1 - Cannon
+     *     2 - Launcher
+     *     3 - Melee
+     *     4 - Nexus
+     *     5 - Rifle
+     *     6 - Drone Weapon (Bligh integrated weapon only)
+     *     7 - Spool Weapon (Barbarossa integrated weapon only)
+     *     8 - ??? (Mimic Gun only)
+     *     9 - Special (Prototype Weapon only)
+     * 
+     * See pg. 33.
+     */
+    private int id;
+    /**
+     * The weapon type's name (i.e. "CQB").
+     * Can be any String except "". Cannot be null.
+     * Case-sensitive.
+     * 
+     * Currently used values:
+     *     "CQB", "Cannon", "Launcher", "Melee", "Nexus", "Rifle",
+     *     "Drone Weapon", "Spool Weapon", "???", "Special"
+     */
+    private String name;
+
+    public WeaponType(int id, String name) {
+        HelperMethods.verifyConstructor();
+        setID(id);
+        setName(name);
+    }
+
+    public int getID() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    private void setID(int id) {
+        this.id = id;
+    }
+    private void setName(String name) {
+        HelperMethods.checkString("name", name);
+        this.name = name;
+    }
+}
