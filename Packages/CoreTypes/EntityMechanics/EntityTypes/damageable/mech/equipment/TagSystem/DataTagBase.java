d368c6ec0691a38ca0abc58f4705894276ccad1b
phon42 <owenbchanas@gmail.com>
2025-11-24 20:52:10 -0600

    Added DataTagBase

---
+++ Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/mech/equipment/TagSystem/DataTagBase.java
@@ -0,0 +1,10 @@
+package Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.mech.equipment.TagSystem;
+
+public class DataTagBase {
+    /**
+     * The id for this data tag (i.e. "tg_ai").
+     * Must be a valid value for this.id (as defined by
+     *     DataTag.allowedDataTags). Cannot be null.
+     */
+    protected String id;
+}
