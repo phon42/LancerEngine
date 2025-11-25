868bf0b005701b47fb3aeefd39c58fe545b6d3d9
phon42 <owenbchanas@gmail.com>
2025-11-25 01:03:55 -0600

    Moved most of CoreBonus' code to UnverifiedCoreBonus

--- Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/pilot/UnverifiedCoreBonus.java
+++ Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/pilot/UnverifiedCoreBonus.java
@@ -1,5 +1,67 @@
 package Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.pilot;
 
+import MainBranch.HelperMethods;
+import Packages.CoreTypes.Counter;
+import Packages.CoreTypes.VueHTMLString;
+import Packages.CoreTypes.EntityMechanics.Bonus;
+import Packages.CoreTypes.EntityMechanics.Manufacturer;
+import Packages.CoreTypes.EntityMechanics.ISynergyData;
+import Packages.CoreTypes.EntityMechanics.Actions.actionBase.IActionData;
+
 public class UnverifiedCoreBonus {
-    
+    // TODO: fill out
+    // Required properties
+    /**
+     * The ID of this core bonus (i.e. "").
+     * Can be any String except "". Cannot be null.
+     * Case-insensitive and stored in lowercase.
+     */
+    private String id;
+    private String name;
+    private Manufacturer source;
+    private VueHTMLString effect;
+    private VueHTMLString description;
+
+    // Optional properties
+    private String mountedEffect;
+    private IActionData[] actions;
+    private IBonusData[] bonuses;
+    private ISynergyData[] synergies;
+    private IDeployableData[] deployables;
+    private Counter[] counters;
+    private String[] integrated;
+    private String[] specialEquipment;
+
+    public UnverifiedCoreBonus(
+        // Required properties
+        String id, String name, Manufacturer source, String effect,
+        String description,
+        // Optional properties
+        String mountedEffect, IActionData[] actions, IBonusData[] bonuses,
+        ISynergyData[] synergies, IDeployableData[] deployables,
+        Counter[] counters, String[] integrated, String[] specialEquipment
+    ) {
+        HelperMethods.verifyConstructor();
+        // Required properties
+        // Optional properties
+    }
+    public UnverifiedCoreBonus(String id, String name, Manufacturer source,
+        String effect, String description) {
+        this(id, name, source, effect, description, null,
+            null, null, null, null,
+            null, null, null);
+    }
+
+    // Required properties
+    public String getID() {
+        return id;
+    }
+    // Optional properties
+    // Required properties
+    private void setID(String id) {
+        HelperMethods.checkString("id", id);
+        id = id.toLowerCase();
+        this.id = id;
+    }
+    // Optional properties
 }
