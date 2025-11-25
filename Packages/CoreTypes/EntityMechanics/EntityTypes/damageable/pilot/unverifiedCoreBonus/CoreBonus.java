c74a984a4d0fb37b9ea95b856945d5dea5eb5816
phon42 <owenbchanas@gmail.com>
2025-11-25 01:12:36 -0600

    Updated CoreBonus

--- Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/pilot/unverifiedCoreBonus/CoreBonus.java
+++ Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/pilot/unverifiedCoreBonus/CoreBonus.java
@@ -1,4 +1,50 @@
 package Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.pilot.unverifiedCoreBonus;
 
-public class CoreBonus {
+import MainBranch.HelperMethods;
+import Packages.CoreTypes.Counter;
+import Packages.CoreTypes.EntityMechanics.ISynergyData;
+import Packages.CoreTypes.EntityMechanics.Manufacturer;
+import Packages.CoreTypes.EntityMechanics.Actions.actionBase.IActionData;
+import Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.pilot.UnverifiedCoreBonus;
+
+public class CoreBonus extends UnverifiedCoreBonus {
+    // Required property
+    /**
+     * The manufacturer of this core bonus (i.e. a Manufacturer representing
+     *     GMS).
+     * Can be any Manufacturer. Cannot be null.
+     */
+    private Manufacturer source;
+
+    public CoreBonus(
+        // Required properties
+        String id, String name, Manufacturer source, String effect,
+        String description,
+        // Optional properties
+        String mountedEffect, IActionData[] actions, IBonusData[] bonuses,
+        ISynergyData[] synergies, IDeployableData[] deployables,
+        Counter[] counters, String[] integrated, String[] specialEquipment
+    ) {
+        super(id, name, "", effect, description, mountedEffect, actions,
+            null, synergies, null, counters, integrated,
+            specialEquipment);
+        setSource(source);
+    }
+    public CoreBonus(String id, String name, Manufacturer source, String effect,
+        String description) {
+        this(id, name, source, effect, description, null,
+            null, null, null, null,
+            null, null, null);
+        setSource(source);
+    }
+
+    // Required property
+    public Manufacturer getSource() {
+        return source;
+    }
+    // Required property
+    private void setSource(Manufacturer source) {
+        HelperMethods.checkObject("source", source);
+        this.source = source;
+    }
 }
