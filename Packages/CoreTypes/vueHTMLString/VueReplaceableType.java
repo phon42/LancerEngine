9dcd95deb2404c56920901172cfb95cc911dee29
phon42 <owenbchanas@gmail.com>
2025-11-24 20:40:11 -0600

    Added VueReplaceableType

---
+++ Packages/CoreTypes/vueHTMLString/VueReplaceableType.java
@@ -0,0 +1,8 @@
+package Packages.CoreTypes.vueHTMLString;
+
+public class VueReplaceableType {
+    public VueReplaceableType replace(String replacement) {
+        // TODO: fill out
+        return this;
+    }
+}
