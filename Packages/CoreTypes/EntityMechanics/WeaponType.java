b69ed3832b1e3bbe23c35418786aff2d4905557e
phon42 <owenbchanas@gmail.com>
2025-11-26 11:59:28 -0600

    Updated WeaponType

--- Packages/CoreTypes/EntityMechanics/WeaponType.java
+++ Packages/CoreTypes/EntityMechanics/WeaponType.java
@@ -3,6 +3,7 @@ package Packages.CoreTypes.EntityMechanics;
 import MainBranch.HelperMethods;
 
 public class WeaponType {
+    private static int maxID = 0;
     /**
      * The weapon type's int id (i.e. 0 for a CQB weapon).
      * Can be any int.
@@ -39,14 +40,21 @@ public class WeaponType {
         setName(name);
     }
 
+    public static int getMaxID() {
+        return maxID;
+    }
     public int getID() {
         return id;
     }
     public String getName() {
         return name;
     }
+    private static void setMaxID(int maxID) {
+        WeaponType.maxID = maxID;
+    }
     private void setID(int id) {
         this.id = id;
+        setMaxID(Math.max(WeaponType.maxID, this.id));
     }
     private void setName(String name) {
         HelperMethods.checkString("name", name);
