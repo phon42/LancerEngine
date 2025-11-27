a41e0f91c5f56b97fbf2919aaebf5cb6f8adad68
phon42 <owenbchanas@gmail.com>
2025-11-26 22:40:44 -0600

    Updated Weapon

--- Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/mech/equipment/Weapon.java
+++ Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/mech/equipment/Weapon.java
@@ -1,13 +1,19 @@
 package Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.mech.equipment;
 
 import MainBranch.HelperMethods;
+import Packages.CoreTypes.Counter;
+import Packages.CoreTypes.VueHTMLString;
+import Packages.CoreTypes.EntityMechanics.Bonus;
+import Packages.CoreTypes.EntityMechanics.ISynergyData;
 import Packages.CoreTypes.EntityMechanics.License;
 import Packages.CoreTypes.EntityMechanics.RangeTag;
 import Packages.CoreTypes.EntityMechanics.WeaponSize;
 import Packages.CoreTypes.EntityMechanics.WeaponType;
+import Packages.CoreTypes.EntityMechanics.Actions.actionBase.IActionData;
+import Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.deployable.IDeployableData;
 import Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.mech.Equipment;
-import Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.mech.equipment.TagSystem.dataTagUnverified.dataTag.DataTag;
-import Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.mech.equipment.TagSystem.dataTagUnverified.dataTag.dataTag.Tag;
+import Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.mech.equipment.TagSystem.dataTagUnverified.DataTag;
+import Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.mech.equipment.weapon.IWeaponProfile;
 import Packages.CoreTypes.EntityMechanics.HarmSystem.Harm;
 
 /**
@@ -27,6 +33,62 @@ import Packages.CoreTypes.EntityMechanics.HarmSystem.Harm;
 public final class Weapon extends Equipment {
     // TODO: fill out with some kind of way to attack
     // TODO: fucking deal with Ushabti Omnigun somehow
+    // Required properties
+    /**
+     * The weapon's size (i.e. a WeaponSize representing a main weapon).
+     * Can be any WeaponSize. Cannot be null.
+     */
+    private WeaponSize size;
+    /**
+     * The weapon's type (i.e. a WeaponType representing a CQB weapon).
+     * Can be any WeaponType. Cannot be null.
+     */
+    private WeaponType type;
+
+    // Semi-required properties
+    private boolean barrage;
+    private boolean skirmish;
+    private boolean noAttack;
+    private boolean noMods;
+    private boolean noCoreBonus;
+    private boolean noBonus;
+    private boolean noSynergy;
+
+    // Optional properties
+    /**
+     * Must be a minimum of 0.
+     */
+    private int cost;
+    /**
+     * The weapon's damage, including heat and burn (i.e. a Harm[] containing
+     *     a Harm representing 10 Kinetic, 2 Heat, or 3 Burn).
+     * Can be any Harm[] that is not of length 0 or contains null elements. Can
+     *     be null.
+     */
+    private Harm[] harm;
+    // TODO: make sure to account for multiple possible ranges
+    /**
+     * The weapon's range (i.e. a RangeTag[] containing a RangeTag representing
+     *     Range 10).
+     * Can be any RangeTag[] that is not of length 0 or contains null elements.
+     *     Can be null.
+     */
+    private RangeTag[] range;
+    private int systemPoints;
+    private VueHTMLString effect;
+    private VueHTMLString onAttack;
+    private VueHTMLString onHit;
+    private VueHTMLString onCrit;
+    private IActionData[] actions;
+    private Bonus[] bonuses;
+    private ISynergyData[] synergies;
+    private IDeployableData[] deployables;
+    private Counter[] counters;
+    private String[] integrated;
+    private String[] specialEquipment;
+    private IWeaponProfile[] profiles;
+
+    // Helper properties
     /**
      * Contains an array of allowed values for Weapon.dataTags' DataTag.name
      *     values.
@@ -38,31 +100,6 @@ public final class Weapon extends Equipment {
         "Heat X (Self)", "Knockback X", "Limited X", "Seeking", "Unique",
         "Reaction", "Drone", "Protocol", "Full Action", "Deployable",
         "Quick Action", "X/Round", "Danger Zone", "AI", "Free Action"};
-    /**
-     * The weapon's size (i.e. a WeaponSize representing a main weapon).
-     * Can be any WeaponSize. Cannot be null.
-     */
-    private WeaponSize size;
-    /**
-     * The weapon's type (i.e. a WeaponType representing a CQB weapon).
-     * Can be any WeaponType. Cannot be null.
-     */
-    private WeaponType type;
-    /**
-     * The weapon's damage, including heat and burn (i.e. a Harm[] containing
-     *     a Harm representing 10 Kinetic, 2 Heat, or 3 Burn).
-     * Can be any Harm[] that does not contain null or null elements. Cannot be
-     *     null.
-     */
-    private Harm[] harm;
-    // TODO: make sure to account for multiple possible ranges
-    /**
-     * The weapon's range (i.e. a RangeTag[] containing a RangeTag representing
-     *     Range 10).
-     * Can be any RangeTag[] that does not contain null or null elements. Cannot
-     *     be null.
-     */
-    private RangeTag[] range;
 
     /**
      * Creates a new Weapon given a weapon name, manufacturer, the license the
@@ -176,12 +213,14 @@ public final class Weapon extends Equipment {
     @Override
     protected void setDataTags(DataTag[] dataTags) {
         boolean isValid;
+        String dataTagName;
 
         HelperMethods.checkObjectArray("dataTags", dataTags);
         for (DataTag tag : dataTags) {
             isValid = false;
+            dataTagName = tag.getData().getName();
             for (String allowedTag : Weapon.allowedNames) {
-                if (tag.getName().equals(allowedTag)) {
+                if (dataTagName.equals(allowedTag)) {
                     isValid = true;
                     break;
                 }
@@ -189,7 +228,7 @@ public final class Weapon extends Equipment {
             if (! isValid) {
                 throw new IllegalArgumentException("New tags array includes a"
                     + " Tag with an invalid tag name for a Weapon: \""
-                    + tag.getName() + "\"");
+                    + dataTagName + "\"");
             }
         }
         dataTags = HelperMethods.copyOf(dataTags);
