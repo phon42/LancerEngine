72ae711b4f9eb32b37689eda91ee0792f5988ceb
phon42 <owenbchanas@gmail.com>
2025-11-26 18:28:53 -0600

    Updated Table

--- Packages/CoreTypes/Table.java
+++ Packages/CoreTypes/Table.java
@@ -26,21 +26,22 @@ public class Table {
     private String name;
     /**
      * The table's data (too large to provide an example of).
-     * Can be any Object[] that does not contain null elements. Cannot be null.
+     * Can be any Object. Cannot be null.
      */
-    private Object[] data;
+    private Object data;
     /**
-     * The data type of this.data (i.e. and usually JSONType.STRING).
-     * Can be any JSONType. Cannot be null.
+     * A JSONTypeTree storing the data type of this.data (usually a JSONTypeTree
+     *     representing JSONType.STRING).
+     * Can be any JSONTypeTree. Cannot be null.
      */
-    private JSONType dataType;
+    private JSONTypeTree type;
 
-    public Table(String name, Object[] data) {
+    public Table(String name, Object data) {
         setName(name);
-        setDataType(calculateDataType(data));
+        setType(JSONTypeTree.constructTree(data));
         setData(data);
-        // setDataType call moved upwards so that setData knows what data type
-        //     this Table contains
+        // setType call moved upwards so that setData knows what data type this
+        //     Table contains
     }
     public Table(Table table) {
         setName(table.name);
@@ -50,181 +51,155 @@ public class Table {
     public String getName() {
         return name;
     }
-    public Object[] getData() {
-        return copyData(data);
+    public Object getData() {
+        return JSONTypeTree.copy(data, type);
     }
-    public JSONType getDataType() {
-        return dataType;
+    public JSONTypeTree getType() {
+        return type;
     }
     private void setName(String name) {
         HelperMethods.checkString("name", name);
         name = name.toLowerCase();
         this.name = name;
     }
-    private void setData(Object[] data) {
-        HelperMethods.checkObjectArray("data", data);
-        data = copyData(data);
+    private void setData(Object data) {
+        HelperMethods.checkObject("data", data);
+        if (type.getType() != JSONType.JSONARRAY) {
+            throw new IllegalArgumentException("data must be a JSONArray or"
+                + " Object[]");
+        }
+        data = JSONTypeTree.copy(data, type);
         this.data = data;
     }
-    private void setDataType(JSONType dataType) {
-        HelperMethods.checkObject("dataType", dataType);
-        this.dataType = dataType;
+    private void setType(JSONTypeTree type) {
+        HelperMethods.checkObject("type", type);
+        this.type = type;
     }
 
-    private JSONType calculateDataType(Object[] data) {
-        Object element;
+    public JSONType getDataElementType() {
+        JSONTypeTree[] children;
 
-        HelperMethods.checkObjectArray("data", data);
-        if (data.length == 0) {
-            return JSONType.STRING;
-        }
-        element = data[0];
-        if (element instanceof Boolean) {
-            return JSONType.BOOLEAN;
-        } else if (element instanceof Number) {
-            if (element instanceof Integer) {
-                return JSONType.INTEGER;
-            } else if (element instanceof BigDecimal) {
-                return JSONType.BIGDECIMAL;
-            } else {
-                throw new IllegalStateException("data is an array of some type"
-                    + " which extends Number, " + element.getClass() + ", which"
-                    + " is neither Integer nor BigDecimal");
-            }
-        } else if (element instanceof String) {
-            return JSONType.STRING;
-        } else if (element instanceof JSONObject) {
-            return JSONType.JSONOBJECT;
-        } else if (element instanceof JSONArray) {
-            return JSONType.JSONARRAY;
-        } else {
-            throw new IllegalStateException("data is of type: "
-                + element.getClass() + "[] which is none of the following"
-                + " allowed types: Boolean[], Integer[], BigDecimal[],"
-                + " String[], JSONObject[], or JSONArray[]");
-        }
-    }
-    private Object[] copyData(Object[] data) {
-        Object[] copy;
-        Object element;
+        if (this.type.getType() == JSONType.JSONARRAY) {
+            children = this.type.getChildrenDescriptors();
 
-        HelperMethods.checkObjectArray("data", data);
-        if (data.length == 0) {
-            return new Object[0];
+            return children[0].getType();
         }
-        if (this.dataType == null) {
-            throw new IllegalStateException("Cannot copy this.data while"
-                + " this.dataType is null");
-        }
-        copy = new Object[data.length];
-        for (int i = 0; i < data.length; i++) {
-            element = data[i];
-            if (this.dataType == JSONType.BOOLEAN) {
-                element = HelperMethods.copyOf((Boolean) element);
-            } else if (this.dataType == JSONType.INTEGER) {
-                element = HelperMethods.copyOf((Integer) element);
-            } else if (this.dataType == JSONType.BIGDECIMAL) {
-                element = HelperMethods.copyOf((BigDecimal) element);
-            } else if (this.dataType == JSONType.STRING) {
-                // nothing needs to be done here, there is no wrapper class for
-                //     String and Strings are immutable anyway
-            } else if (this.dataType == JSONType.JSONOBJECT) {
-                element = HelperMethods.copyOf((JSONObject) element);
-            } else {
-                // this.dataType is JSONType.JSONARRAY
-                element = HelperMethods.copyOf((JSONArray) element);
-            }
-            data[i] = element;
-        }
-
-        return copy;
+        throw new IllegalStateException("Table.data must be a JSONArray or"
+            + " Object[]");
     }
     public boolean[] getBooleanData() {
+        Object[] dataArray;
         boolean[] booleanData;
+        Boolean booleanElement;
 
-        if (this.dataType != JSONType.BOOLEAN) {
+        if (getDataElementType() != JSONType.BOOLEAN) {
             throw new IllegalStateException("Cannot create a boolean[] from"
                 + " a this.data property whose elements are of type other than"
                 + " Boolean");
         }
-        booleanData = new boolean[this.data.length];
-        for (int i = 0; i < this.data.length; i++) {
-            booleanData[i] = (Boolean) this.data[i];
+        dataArray = (Object[]) this.data;
+        booleanData = new boolean[dataArray.length];
+        for (int i = 0; i < dataArray.length; i++) {
+            booleanElement = (Boolean) dataArray[i];
+            booleanData[i] = booleanElement.booleanValue();
         }
 
         return booleanData;
     }
     public int[] getIntData() {
+        Object[] dataArray;
         int[] intData;
+        Integer intElement;
 
-        if (this.dataType != JSONType.INTEGER) {
+        if (getDataElementType() != JSONType.INTEGER) {
             throw new IllegalStateException("Cannot create an int[] from a"
                 + " this.data property whose elements are of type other than"
                 + " Integer");
         }
-        intData = new int[this.data.length];
-        for (int i = 0; i < this.data.length; i++) {
-            intData[i] = (Integer) this.data[i];
+        dataArray = (Object[]) this.data;
+        intData = new int[dataArray.length];
+        for (int i = 0; i < dataArray.length; i++) {
+            intElement = (Integer) dataArray[i];
+            intData[i] = intElement.intValue();
         }
 
         return intData;
     }
     public BigDecimal[] getBigDecimalData() {
+        Object[] dataArray;
         BigDecimal[] bigDecimalData;
+        BigDecimal bigDecimalElement;
 
-        if (this.dataType != JSONType.BIGDECIMAL) {
+        if (getDataElementType() != JSONType.BIGDECIMAL) {
             throw new IllegalStateException("Cannot create a BigDecimal[] from"
                 + " a this.data property whose elements are of type other than"
                 + " BigDecimal");
         }
-        bigDecimalData = new BigDecimal[this.data.length];
-        for (int i = 0; i < this.data.length; i++) {
-            bigDecimalData[i] = HelperMethods.copyOf((BigDecimal) this.data[i]);
+        dataArray = (Object[]) this.data;
+        bigDecimalData = new BigDecimal[dataArray.length];
+        for (int i = 0; i < dataArray.length; i++) {
+            bigDecimalElement = (BigDecimal) dataArray[i];
+            bigDecimalElement = HelperMethods.copyOf(bigDecimalElement);
+            bigDecimalData[i] = bigDecimalElement;
         }
 
         return bigDecimalData;
     }
     public String[] getStringData() {
+        Object[] dataArray;
         String[] stringData;
+        String stringElement;
 
-        if (this.dataType != JSONType.INTEGER) {
+        if (getDataElementType() != JSONType.INTEGER) {
             throw new IllegalStateException("Cannot create a String[] from a"
                 + " this.data property whose elements are of type other than"
                 + " String");
         }
-        stringData = new String[this.data.length];
-        for (int i = 0; i < this.data.length; i++) {
-            stringData[i] = (String) this.data[i];
+        dataArray = (Object[]) this.data;
+        stringData = new String[dataArray.length];
+        for (int i = 0; i < dataArray.length; i++) {
+            stringElement = (String) dataArray[i];
+            stringData[i] = stringElement;
         }
 
         return stringData;
     }
     public JSONObject[] getJSONObjectData() {
+        Object[] dataArray;
         JSONObject[] jsonObjectData;
+        JSONObject jsonObjectElement;
 
-        if (this.dataType != JSONType.INTEGER) {
+        if (getDataElementType() != JSONType.INTEGER) {
             throw new IllegalStateException("Cannot create a JSONObject[] from"
                 + " a this.data property whose elements are of type other than"
                 + " JSONObject");
         }
-        jsonObjectData = new JSONObject[this.data.length];
-        for (int i = 0; i < this.data.length; i++) {
-            jsonObjectData[i] = HelperMethods.copyOf((JSONObject) this.data[i]);
+        dataArray = (Object[]) this.data;
+        jsonObjectData = new JSONObject[dataArray.length];
+        for (int i = 0; i < dataArray.length; i++) {
+            jsonObjectElement = (JSONObject) dataArray[i];
+            jsonObjectElement = HelperMethods.copyOf(jsonObjectElement);
+            jsonObjectData[i] = jsonObjectElement;
         }
 
         return jsonObjectData;
     }
     public JSONArray[] getJSONArrayData() {
+        Object[] dataArray;
         JSONArray[] jsonArrayData;
+        JSONArray jsonArrayElement;
 
-        if (this.dataType != JSONType.INTEGER) {
+        if (getDataElementType() != JSONType.INTEGER) {
             throw new IllegalStateException("Cannot create a JSONArray[] from a"
                 + " this.data property whose elements are of type other than"
                 + " JSONArray");
         }
-        jsonArrayData = new JSONArray[this.data.length];
-        for (int i = 0; i < this.data.length; i++) {
-            jsonArrayData[i] = HelperMethods.copyOf((JSONArray) this.data[i]);
+        dataArray = (Object[]) this.data;
+        jsonArrayData = new JSONArray[dataArray.length];
+        for (int i = 0; i < dataArray.length; i++) {
+            jsonArrayElement = (JSONArray) dataArray[i];
+            jsonArrayElement = HelperMethods.copyOf(jsonArrayElement);
+            jsonArrayData[i] = jsonArrayElement;
         }
 
         return jsonArrayData;
