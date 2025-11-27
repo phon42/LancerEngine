c8cd368b6f0702e4134f35bf0048ab931b898106
phon42 <owenbchanas@gmail.com>
2025-11-27 02:57:02 -0600

    Updated MixedExpression

--- MainBranch/roll/MixedExpression.java
+++ MainBranch/roll/MixedExpression.java
@@ -39,6 +39,26 @@ public class MixedExpression {
         this.constantComponent = constantComponent;
     }
 
+    @Override
+    public String toString() {
+        String output;
+        String constantComponent;
+
+        if (this.randomComponent == null) {
+            output = this.constantComponent.toString();
+        } else {
+            output = this.randomComponent.toString();
+            constantComponent = this.constantComponent.toString();
+            if (this.constantComponent.getValue() < 0) {
+                constantComponent = constantComponent.substring(1);
+                output += " - " + constantComponent;
+            } else if (this.constantComponent.getValue() > 0) {
+                output += " + " + constantComponent;
+            }
+        }
+
+        return output;
+    }
     public int roll() {
         int randomResult = 0;
 
