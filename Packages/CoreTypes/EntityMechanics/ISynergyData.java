a34e5016a226bb2d3ccbd7d1f9aa139e4d0afd28
phon42 <owenbchanas@gmail.com>
2025-11-28 02:06:49 -0600

    Updated ISynergyData

--- Packages/CoreTypes/EntityMechanics/ISynergyData.java
+++ Packages/CoreTypes/EntityMechanics/ISynergyData.java
@@ -1,9 +1,81 @@
 package Packages.CoreTypes.EntityMechanics;
 
+import MainBranch.HelperMethods;
+import Packages.CoreTypes.VueHTMLString;
+import Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.mech.equipment.mechSystem.SystemType;
+
 public class ISynergyData {
     // TODO: fill out
-    private SynergyLocation synergyLocations;
-    private String[] locations;
+    // Required properties
+    private SynergyLocation[] locations;
+    private VueHTMLString detail;
 
-    public ISynergyData(String synergy) {}
+    // Optional properties
+    private WeaponType[] weaponTypes;
+    private SystemType[] systemTypes;
+    private WeaponSize[] weaponSizes;
+
+    public ISynergyData(SynergyLocation[] locations, String detail,
+        WeaponType[] weaponTypes, SystemType[] systemTypes,
+        WeaponSize[] weaponSizes) {
+        // Required properties
+        setLocations(locations);
+        setDetail(detail);
+        // Optional properties
+        setWeaponTypes(weaponTypes);
+        setSystemTypes(systemTypes);
+        setWeaponSizes(weaponSizes);
+    }
+    public ISynergyData(SynergyLocation[] locations, String detail) {
+        this(locations, detail, null, null,
+            null);
+    }
+
+    // Required properties
+    public SynergyLocation[] getLocations() {
+        return locations;
+    }
+    public VueHTMLString getDetail() {
+        return detail;
+    }
+    // Optional properties
+    public WeaponType[] getWeaponTypes() {
+        return weaponTypes;
+    }
+    public SystemType[] getSystemTypes() {
+        return systemTypes;
+    }
+    public WeaponSize[] getWeaponSizes() {
+        return weaponSizes;
+    }
+    // Required properties
+    private void setLocations(SynergyLocation[] locations) {
+        HelperMethods.checkObjectArray("locations", locations);
+        this.locations = locations;
+    }
+    private void setDetail(VueHTMLString detail) {
+        HelperMethods.checkVueHTMLString("detail", detail);
+        this.detail = detail;
+    }
+    // Optional properties
+    private void setWeaponTypes(WeaponType[] weaponTypes) {
+        HelperMethods.checkObjectArrayAlt("weaponTypes",
+            weaponTypes);
+        this.weaponTypes = weaponTypes;
+    }
+    private void setSystemTypes(SystemType[] systemTypes) {
+        HelperMethods.checkObjectArrayAlt("systemTypes",
+            systemTypes);
+        this.systemTypes = systemTypes;
+    }
+    public void setWeaponSizes(WeaponSize[] weaponSizes) {
+        HelperMethods.checkObjectArrayAlt("weaponSizes",
+            weaponSizes);
+        this.weaponSizes = weaponSizes;
+    }
+
+    private void setDetail(String detail) {
+        HelperMethods.checkObject("detail", detail);
+        setDetail(new VueHTMLString(detail));
+    }
 }
