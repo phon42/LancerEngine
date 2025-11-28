a461f48554613a28b5a40676049cf25b28534959
phon42 <owenbchanas@gmail.com>
2025-11-28 02:28:35 -0600

    Updated SynergyLocation

--- Packages/CoreTypes/EntityMechanics/SynergyLocation.java
+++ Packages/CoreTypes/EntityMechanics/SynergyLocation.java
@@ -1,8 +1,42 @@
 package Packages.CoreTypes.EntityMechanics;
 
+import MainBranch.HelperMethods;
+
 public class SynergyLocation {
     // TODO: fill out
-    String value;
+    // Required property
+    private String value;
 
-    public SynergyLocation(String value) {}
+    // Optional property
+    private String description;
+
+    public SynergyLocation(String value, String description) {
+        HelperMethods.verifyConstructor();
+        setValue(value);
+        setDescription(description);
+    }
+    public SynergyLocation(String value) {
+        this(value, null);
+    }
+
+    // Required property
+    public String getValue() {
+        return value;
+    }
+    // Optional property
+    public String getDescription() {
+        return description;
+    }
+    // Required property
+    private void setValue(String value) {
+        HelperMethods.checkString("value", value);
+        this.value = value;
+    }
+    // Optional property
+    public void setDescription(String description) {
+        if (description != null) {
+            HelperMethods.checkString("description", description);
+        }
+        this.description = description;
+    }
 }
