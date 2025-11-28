8c542bff022d2e316759c94b3fccaeef8394b2d3
phon42 <owenbchanas@gmail.com>
2025-11-28 01:58:27 -0600

    Added SystemType

---
+++ Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/mech/equipment/mechSystem/SystemType.java
@@ -0,0 +1,29 @@
+package Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.mech.equipment.mechSystem;
+
+import MainBranch.HelperMethods;
+
+public class SystemType {
+    /**
+     * This system type's value (i.e. "AI").
+     * Can be any String except "". Cannot be null.
+     * Case-insensitive.
+     * 
+     * Currently used values:
+     *     "AI", "Deployable", "Drone", "Flight System", "Shield", "System",
+     *         "Tech"
+     */
+    private String value;
+
+    public SystemType(String value) {
+        HelperMethods.verifyConstructor();
+        setValue(value);
+    }
+
+    public String getValue() {
+        return value;
+    }
+    private void setValue(String value) {
+        HelperMethods.checkString("value", value);
+        this.value = value;
+    }
+}
