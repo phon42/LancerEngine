4c044efb4f6a031a8cb71036ba98fcc26392819a
phon42 <owenbchanas@gmail.com>
2025-11-29 14:35:04 -0600

    Added NPCSystemType

---
+++ Packages/CoreTypes/EntityMechanics/NPCs/npcBase/npcFeature/NPCSystemType.java
@@ -0,0 +1,26 @@
+package Packages.CoreTypes.EntityMechanics.NPCs.npcBase.npcFeature;
+
+import MainBranch.HelperMethods;
+
+public class NPCSystemType {
+    // Required property
+    /**
+     * Can be any String except "". Cannot be null.
+     * Case-sensitive.
+     */
+    private String value;
+
+    public NPCSystemType(String value) {
+        setValue(value);
+    }
+
+    // Required property
+    public String getValue() {
+        return value;
+    }
+    // Required property
+    private void setValue(String value) {
+        HelperMethods.checkString("value", value);
+        this.value = value;
+    }
+}
