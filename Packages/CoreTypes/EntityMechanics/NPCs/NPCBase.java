d5ae9c82ba94cbf256e43f0c426c99bb0eb1737f
phon42 <owenbchanas@gmail.com>
2025-11-29 14:33:09 -0600

    Updated NPCBase and its children

--- Packages/CoreTypes/EntityMechanics/NPCs/NPCBase.java
+++ Packages/CoreTypes/EntityMechanics/NPCs/NPCBase.java
@@ -1,8 +1,42 @@
 package Packages.CoreTypes.EntityMechanics.NPCs;
 
+import MainBranch.HelperMethods;
+
 public class NPCBase {
     // TODO: fill out
     // Required properties
-    private String id;
-    private String name;
+    /**
+     * Can be any String except "". Cannot be null.
+     * Case-insensitive and stored in lowercase.
+     */
+    protected String id;
+    /**
+     * Can be any String except "". Cannot be null.
+     * Case-sensitive.
+     */
+    protected String name;
+
+    protected NPCBase(String id, String name) {
+        HelperMethods.verifyConstructor();
+        setID(id);
+        setName(name);
+    }
+
+    // Required properties
+    public String getID() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    // Required properties
+    protected void setID(String id) {
+        HelperMethods.checkString("id", id);
+        id = id.toLowerCase();
+        this.id = id;
+    }
+    protected void setName(String name) {
+        HelperMethods.checkString("name", name);
+        this.name = name;
+    }
 }
--- Packages/CoreTypes/EntityMechanics/NPCs/npcBase/NPCTemplate.java
+++ Packages/CoreTypes/EntityMechanics/NPCs/npcBase/NPCTemplate.java
@@ -1,5 +1,6 @@
 package Packages.CoreTypes.EntityMechanics.NPCs.npcBase;
 
+import MainBranch.HelperMethods;
 import Packages.CoreTypes.EntityMechanics.NPCs.NPCBase;
 
 /**
@@ -16,10 +17,109 @@ import Packages.CoreTypes.EntityMechanics.NPCs.NPCBase;
 public class NPCTemplate extends NPCBase {
     // TODO: fill out
     // Required properties
+    /**
+     * Can be any String except "". Cannot be null.
+     * Case-sensitive.
+     */
     private String description;
-    private String[] baseFeatures;
-    private String[] optionalFeatures;
+    /**
+     * Can be any int.
+     */
     private int power;
 
-    public NPCTemplate() {}
+    // Optional properties
+    /**
+     * Can be any String[] that is not of length 0 and does not contain "" or
+     *     null elements. Can be null.
+     * Elements are case-insensitive and stored in lowercase.
+     */
+    private String[] baseFeatures;
+    /**
+     * Can be any String[] that is not of length 0 and does not contain "" or
+     *     null elements. Cannot be null.
+     * Elements are case-insensitive and stored in lowercase.
+     */
+    private String[] optionalFeatures;
+
+    public NPCTemplate(
+        // NCPBase properties
+        String id, String name,
+        // Required properties
+        String description, int power,
+        // Optional properties
+        String[] baseFeatures, String[] optionalFeatures
+    ) {
+        HelperMethods.verifyConstructor();
+        // NCPBase properties
+        super(id, name);
+        // Required properties
+        setDescription(description);
+        setPower(power);
+        // Optional properties
+        setBaseFeatures(baseFeatures);
+        setOptionalFeatures(optionalFeatures);
+    }
+    public NPCTemplate(
+        // NCPBase properties
+        String id, String name,
+        // Required properties
+        String description, int power
+    ) {
+        this(id, name, description, power, null,
+            null);
+    }
+
+    // Required properties
+    public String getDescription() {
+        return description;
+    }
+    public int getPower() {
+        return power;
+    }
+    // Optional properties
+    public String[] getBaseFeatures() {
+        if (baseFeatures != null) {
+            return HelperMethods.copyOf(baseFeatures);
+        }
+
+        return baseFeatures;
+    }
+    public String[] getOptionalFeatures() {
+        if (optionalFeatures != null) {
+            return HelperMethods.copyOf(optionalFeatures);
+        }
+
+        return optionalFeatures;
+    }
+    // Required properties
+    private void setDescription(String description) {
+        HelperMethods.checkString("description", description);
+        this.description = description;
+    }
+    private void setPower(int power) {
+        this.power = power;
+    }
+    // Optional properties
+    private void setBaseFeatures(String[] baseFeatures) {
+        HelperMethods.checkStringArrayAlt("baseFeatures",
+            baseFeatures);
+        if (baseFeatures != null) {
+            baseFeatures = HelperMethods.copyOf(baseFeatures);
+            for (int i = 0; i < baseFeatures.length; i++) {
+                baseFeatures[i] = baseFeatures[i].toLowerCase();
+            }
+        }
+        this.baseFeatures = baseFeatures;
+    }
+    private void setOptionalFeatures(String[] optionalFeatures) {
+        HelperMethods.checkStringArrayAlt("optionalFeatures",
+            optionalFeatures);
+        if (optionalFeatures != null) {
+            optionalFeatures = HelperMethods.copyOf(optionalFeatures);
+            for (int i = 0; i < optionalFeatures.length; i++) {
+                optionalFeatures[i] = optionalFeatures[i].toLowerCase();
+            }
+        }
+        this.optionalFeatures = optionalFeatures;
+    }
 }
