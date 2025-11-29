68a910992cc8c1d849c16730601405cf734903a4
phon42 <owenbchanas@gmail.com>
2025-11-29 17:05:46 -0600

    Updated SystemType

--- Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/mech/equipment/systemBase/SystemType.java
+++ Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/mech/equipment/systemBase/SystemType.java
@@ -3,6 +3,10 @@ package Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.mech.equipment
 import MainBranch.HelperMethods;
 
 public class SystemType {
+    // Static property
+    protected static int maxID = 0;
+
+    // Required property
     /**
      * This system type's value (i.e. "Mech System").
      * Can be any String except "". Cannot be null.
@@ -13,16 +17,42 @@ public class SystemType {
      */
     protected String value;
 
-    public SystemType(String value) {
+    // Semi-required (optional but has a specific default value other than null
+    //     when not provided) property
+    /**
+     * The system type's int id (i.e. 0 for a mech system).
+     * Can be any int.
+     * 
+     * Currently used values:
+     *     0 - Mech System
+     *     1 - Weapon
+     */
+    protected int id;
+
+    public SystemType(String value, int id) {
         HelperMethods.verifyConstructor();
         setValue(value);
+        setID(id);
+    }
+    public SystemType(String value) {
+        this(value, SystemType.maxID + 1);
     }
 
+    // Required property
     public String getValue() {
         return value;
     }
+    // Semi-required property
+    public int getID() {
+        return id;
+    }
+    // Required property
     protected void setValue(String value) {
         HelperMethods.checkString("value", value);
         this.value = value;
+    }
+    // Semi-required property
+    protected void setID(int id) {
+        this.id = id;
     }
 }
