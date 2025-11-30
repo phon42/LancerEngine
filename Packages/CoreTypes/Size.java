5a9afb40fde35157474248a81d4bbc633c669f6d
phon42 <owenbchanas@gmail.com>
2025-11-29 23:05:57 -0600

    Updated Size

--- Packages/CoreTypes/Size.java
+++ Packages/CoreTypes/Size.java
@@ -1,6 +1,11 @@
 package Packages.CoreTypes;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import MainBranch.HelperMethods;
+
 public class Size {
+    // Required property
     /**
      * The size value stored by this object.
      * Size is stored as 2 * its value (i.e. Size 1/2 would be stored as int 1).
@@ -9,8 +14,14 @@ public class Size {
      */
     private int value;
 
+    // Reference property
+    private static final BigDecimal TWO = new BigDecimal(2);
+
     public Size(int value) {
         setValue(value);
+    }
+    public Size(BigDecimal value) {
+        calculateValue(value);
     }
 
     public int getValue() {
@@ -33,6 +44,15 @@ public class Size {
      */
     public String toString() {
         return "SIZE " + output();
+    }
+    private void calculateValue(BigDecimal value) {
+        BigInteger integer;
+
+        HelperMethods.checkObject("value", value);
+        value = value.multiply(Size.TWO);
+        // performing this intermediate step for now
+        integer = value.toBigInteger();
+        setValue(integer.intValue());
     }
     /**
      * A helper method which outputs the stored size, formatted properly so that
