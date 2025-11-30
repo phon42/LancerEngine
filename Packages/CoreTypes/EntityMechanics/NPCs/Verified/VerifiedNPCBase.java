087daa671de547a84f040ad1a0277313ac12ec3b
phon42 <owenbchanas@gmail.com>
2025-11-30 01:36:31 -0600

    Updated VerifiedNPCBase

--- Packages/CoreTypes/EntityMechanics/NPCs/Verified/VerifiedNPCBase.java
+++ Packages/CoreTypes/EntityMechanics/NPCs/Verified/VerifiedNPCBase.java
@@ -1,42 +1,9 @@
 package Packages.CoreTypes.EntityMechanics.NPCs.Verified;
 
-import MainBranch.HelperMethods;
+import Packages.CoreTypes.EntityMechanics.NPCs.NPCBase;
 
-public class VerifiedNPCBase {
-    // TODO: fill out
-    // Required properties
-    /**
-     * Can be any String except "". Cannot be null.
-     * Case-insensitive and stored in lowercase.
-     */
-    protected String id;
-    /**
-     * Can be any String except "". Cannot be null.
-     * Case-sensitive.
-     */
-    protected String name;
-
+public class VerifiedNPCBase extends NPCBase {
     protected VerifiedNPCBase(String id, String name) {
-        HelperMethods.verifyConstructor();
-        setID(id);
-        setName(name);
-    }
-
-    // Required properties
-    public String getID() {
-        return id;
-    }
-    public String getName() {
-        return name;
-    }
-    // Required properties
-    protected void setID(String id) {
-        HelperMethods.checkString("id", id);
-        id = id.toLowerCase();
-        this.id = id;
-    }
-    protected void setName(String name) {
-        HelperMethods.checkString("name", name);
-        this.name = name;
+        super(id, name);
     }
 }
