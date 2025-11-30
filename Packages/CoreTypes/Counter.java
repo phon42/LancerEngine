b4663f80f08b38f64c40306ba09a9ee11fb83cb0
phon42 <owenbchanas@gmail.com>
2025-11-29 20:18:25 -0600

    Updated Counter

--- Packages/CoreTypes/Counter.java
+++ Packages/CoreTypes/Counter.java
@@ -1,3 +1,69 @@
 package Packages.CoreTypes;
 
-public class Counter extends CounterBase {}
+public class Counter extends CounterBase {
+    /**
+     * Counter.min:              PROVIDED
+     * Counter.max:              PROVIDED
+     * Counter.defaultValue:     PROVIDED
+     */
+    public Counter(int minimumValue, int maximumValue, int defaultValue) {
+        super(minimumValue, maximumValue, defaultValue);
+    }
+    /**
+     * Counter.min:              PROVIDED
+     * Counter.max:              PROVIDED
+     * Counter.defaultValue: NOT PROVIDED
+     */
+    public Counter(int minimumValue, int maximumValue) {
+        super(minimumValue, maximumValue);
+    }
+    /**
+     * Counter.min:              PROVIDED
+     * Counter.max:          NOT PROVIDED
+     * Counter.defaultValue:     PROVIDED
+     */
+    public Counter(int minimumValue, Object maximumValue, int defaultValue) {
+        super(minimumValue, null, defaultValue);
+    }
+    /**
+     * Counter.min:              PROVIDED
+     * Counter.max:          NOT PROVIDED
+     * Counter.defaultValue: NOT PROVIDED
+     */
+    public Counter(int minimumValue) {
+        super(minimumValue);
+    }
+    /**
+     * Counter.min:          NOT PROVIDED
+     * Counter.max:              PROVIDED
+     * Counter.defaultValue:     PROVIDED
+     */
+    public Counter(Object minimumValue, int maximumValue, int defaultValue) {
+        super(null, maximumValue, defaultValue);
+    }
+    /**
+     * Counter.min:          NOT PROVIDED
+     * Counter.max:              PROVIDED
+     * Counter.defaultValue: NOT PROVIDED
+     */
+    public Counter(Object minimumValue, int maximumValue) {
+        super(null, maximumValue);
+    }
+    /**
+     * Counter.min:          NOT PROVIDED
+     * Counter.max:          NOT PROVIDED
+     * Counter.defaultValue:     PROVIDED
+     */
+    public Counter(Object minimumValue, Object maximumValue, int defaultValue) {
+        super(null, null, defaultValue);
+    }
+    public Counter() {
+        super();
+    }
+    public Counter(Counter counter) {
+        setMin(counter.min);
+        setMax(counter.max);
+        setDefaultValue(counter.defaultValue);
+        setDefaultValuePresent(counter.defaultValuePresent);
+    }
+}
