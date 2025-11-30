8e3ea07a9fc1dd4420956ebe3cd1d764313b26d8
phon42 <owenbchanas@gmail.com>
2025-11-30 01:34:05 -0600

    Added UnverifiedNPCBase

---
+++ Packages/CoreTypes/EntityMechanics/NPCs/Unverified/UnverifiedNPCBase.java
@@ -0,0 +1,9 @@
+package Packages.CoreTypes.EntityMechanics.NPCs.Unverified;
+
+import Packages.CoreTypes.EntityMechanics.NPCs.NPCBase;
+
+public class UnverifiedNPCBase extends NPCBase {
+    protected UnverifiedNPCBase(String id, String name) {
+        super(id, name);
+    }
+}
