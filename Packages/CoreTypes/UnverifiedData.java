00432236090aa219a5dbf637a2d1292df87a53f9
phon42 <owenbchanas@gmail.com>
2025-11-30 02:47:09 -0600

    Updated UnverifiedData

--- Packages/CoreTypes/UnverifiedData.java
+++ Packages/CoreTypes/UnverifiedData.java
@@ -2,9 +2,14 @@ package Packages.CoreTypes;
 
 /**
  * Provides an interface for unverified data types.
+ * @param UnverifiedType a Class representing this class.
  * @param VerifiedType a Class representing the verified version of this class.
  */
-public interface UnverifiedData<VerifiedType> {
+public interface UnverifiedData<UnverifiedType, VerifiedType> {
+    /**
+     * A method to return the class type of the unverified type.
+     */
+    Class<UnverifiedType> getUnverifiedType();
     /**
      * A method to return the class type of the verified type.
      */
