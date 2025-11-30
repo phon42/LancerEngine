3acd1b3c0d2d13224091c85ed2ccd07bbd962bc0
phon42 <owenbchanas@gmail.com>
2025-11-30 01:39:55 -0600

    Added UnverifiedNPCTemplate

---
+++ Packages/CoreTypes/EntityMechanics/NPCs/Unverified/unverifiedNPCBase/UnverifiedNPCTemplate.java
@@ -0,0 +1,124 @@
+package Packages.CoreTypes.EntityMechanics.NPCs.Unverified.unverifiedNPCBase;
+
+import MainBranch.HelperMethods;
+import Packages.CoreTypes.EntityMechanics.NPCs.Unverified.UnverifiedNPCBase;
+
+/**
+ * Represents a single NPC template. Contains information about its features and
+ *     power.
+ * 
+ * Requires an npc template id, name, description, base features array, optional
+ *     features array, and power to be instantiated.
+ * 
+ * Unused at present.
+ * 
+ * Safety: At least one of this class' properties has an allowed value of null.
+ */
+public class UnverifiedNPCTemplate extends UnverifiedNPCBase {
+    // Required properties
+    /**
+     * Can be any String except "". Cannot be null.
+     * Case-sensitive.
+     */
+    private String description;
+    /**
+     * Can be any int.
+     */
+    private int power;
+
+    // Optional properties
+    /**
+     * Can be any String[] that is not of length 0 and does not contain "" or
+     *     null elements. Can be null.
+     * Elements are case-insensitive and stored in lowercase.
+     */
+    private String[] baseFeatures;
+    /**
+     * Can be any String[] that is not of length 0 and does not contain "" or
+     *     null elements. Cannot be null.
+     * Elements are case-insensitive and stored in lowercase.
+     */
+    private String[] optionalFeatures;
+
+    public UnverifiedNPCTemplate(
+        // NCPBase properties
+        String id, String name,
+        // Required properties
+        String description, int power,
+        // Optional properties
+        String[] baseFeatures, String[] optionalFeatures
+    ) {
+        HelperMethods.verifyConstructor();
+        // NCPBase properties
+        super(id, name);
+        // Required properties
+        setDescription(description);
+        setPower(power);
+        // Optional properties
+        setBaseFeatures(baseFeatures);
+        setOptionalFeatures(optionalFeatures);
+    }
+    public UnverifiedNPCTemplate(
+        // NCPBase properties
+        String id, String name,
+        // Required properties
+        String description, int power
+    ) {
+        this(id, name, description, power, null,
+            null);
+    }
+
+    // Required properties
+    public String getDescription() {
+        return description;
+    }
+    public int getPower() {
+        return power;
+    }
+    // Optional properties
+    public String[] getBaseFeatures() {
+        if (baseFeatures != null) {
+            return HelperMethods.copyOf(baseFeatures);
+        }
+
+        return baseFeatures;
+    }
+    public String[] getOptionalFeatures() {
+        if (optionalFeatures != null) {
+            return HelperMethods.copyOf(optionalFeatures);
+        }
+
+        return optionalFeatures;
+    }
+    // Required properties
+    private void setDescription(String description) {
+        HelperMethods.checkString("description", description);
+        this.description = description;
+    }
+    private void setPower(int power) {
+        this.power = power;
+    }
+    // Optional properties
+    private void setBaseFeatures(String[] baseFeatures) {
+        HelperMethods.checkStringArrayAlt("baseFeatures",
+            baseFeatures);
+        if (baseFeatures != null) {
+            baseFeatures = HelperMethods.copyOf(baseFeatures);
+            for (int i = 0; i < baseFeatures.length; i++) {
+                baseFeatures[i] = baseFeatures[i].toLowerCase();
+            }
+        }
+        this.baseFeatures = baseFeatures;
+    }
+    private void setOptionalFeatures(String[] optionalFeatures) {
+        HelperMethods.checkStringArrayAlt("optionalFeatures",
+            optionalFeatures);
+        if (optionalFeatures != null) {
+            optionalFeatures = HelperMethods.copyOf(optionalFeatures);
+            for (int i = 0; i < optionalFeatures.length; i++) {
+                optionalFeatures[i] = optionalFeatures[i].toLowerCase();
+            }
+        }
+        this.optionalFeatures = optionalFeatures;
+    }
+}
