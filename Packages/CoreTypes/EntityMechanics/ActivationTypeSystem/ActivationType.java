816523acb2fa39fb3fa6e59e2cd96648f730e508
phon42 <owenbchanas@gmail.com>
2025-12-05 16:12:25 -0600

    Moved most of ActivationType's content to ActivationTypeBase

--- Packages/CoreTypes/EntityMechanics/ActivationTypeSystem/ActivationType.java
+++ Packages/CoreTypes/EntityMechanics/ActivationTypeSystem/ActivationType.java
@@ -7,7 +7,7 @@ import MainBranch.HelperMethods;
  * Represents an activation type. Contains information about that activation
  *     type's type and a helper property related to it.
  * 
- * Requires an activation type type to be instantiated.
+ * Requires an activation type value to be instantiated.
  * 
  * Unused at present.
  * 
@@ -15,41 +15,7 @@ import MainBranch.HelperMethods;
  * 
  * This class is immutable (in other words, no copies of it need to be created).
  */
-public class ActivationType {
-    // Required property
-    /**
-     * The value of this activation type (i.e. "free").
-     * =========================================================================
-     * Meaning of each value:
-     * "STANDARD MOVE
-     * Movement up to a character’s maximum SPEED.
-     * QUICK ACTION
-     * Actions that take a few moments, such as quickly firing a weapon,
-     *     activating a system, or moving further.
-     * REACTION
-     * Reactions are special actions that can be made outside of the usual turn
-     *     order as responses to incoming attacks, enemy movement, and other
-     *     events.
-     * FULL ACTION
-     * Actions that require full attention (e.g. firing a barrage, performing
-     *     field repairs on a mech).
-     * FREE ACTION
-     * Free actions are special actions granted by character traits, like mech
-     *     systems and talents."
-     * - pg. 61
-     * -------------------------------------------------------------------------
-     * "Unlike the standard action types, there is no limit to how many free
-     *     actions and reactions a character can take per round. Characters can
-     *     even take reactions outside of their turn."
-     * - pg. 61
-     * =========================================================================
-     * Can be any String except "". Cannot be null.
-     * Case-insensitive and stored in lowercase. Cannot be null.
-     * Anything that is not one of the standard values is automatically counted
-     *     as a free action.
-     */
-    private String value;
-
+public class ActivationType extends ActivationTypeBase {
     // Helper property
     /**
      * Exactly the same as this.value most of the time; is "free" when
@@ -57,31 +23,23 @@ public class ActivationType {
      * Can be any String except "". Cannot be null.
      * Case-insensitive and stored in lowercase. Cannot be null.
      */
-    private String type;
+    protected String type;
 
     public ActivationType(String value) {
-        HelperMethods.verifyConstructor();
-        setValue(value);
+        super(value);
         calculateType();
     }
     public ActivationType(String value, String type) {
-        HelperMethods.verifyConstructor();
-        setValue(value);
+        super(value);
         setType(type);
     }
 
-    public String getValue() {
-        return value;
-    }
+    // Helper property
     public String getType() {
         return type;
     }
-    private void setValue(String value) {
-        HelperMethods.checkString("value", value);
-        value = value.toLowerCase();
-        this.value = value;
-    }
-    private void setType(String type) {
+    // Helper property
+    protected void setType(String type) {
         HelperMethods.checkString("type", type);
         type = type.toLowerCase();
         this.type = type;
--- Packages/CoreTypes/EntityMechanics/ActivationTypeSystem/ActivationTypeBase.java
+++ Packages/CoreTypes/EntityMechanics/ActivationTypeSystem/ActivationTypeBase.java
@@ -1,5 +1,68 @@
 package Packages.CoreTypes.EntityMechanics.ActivationTypeSystem;
 
+import MainBranch.HelperMethods;
+
+/**
+ * Represents an activation type, either verified or unverified. Contains
+ *     information about that activation type's type and a helper property
+ *     related to it.
+ * 
+ * Requires an activation type value to be instantiated.
+ * 
+ * Used in ActivationType and UnverifiedActivationType.
+ * 
+ * Safety: None of this class' properties have allowed values of null.
+ * 
+ * This class is immutable (in other words, no copies of it need to be created).
+ */
 public class ActivationTypeBase {
-    
+    // Required property
+    /**
+     * The value of this activation type (i.e. "free").
+     * =========================================================================
+     * Meaning of each value:
+     * "STANDARD MOVE
+     * Movement up to a character’s maximum SPEED.
+     * QUICK ACTION
+     * Actions that take a few moments, such as quickly firing a weapon,
+     *     activating a system, or moving further.
+     * REACTION
+     * Reactions are special actions that can be made outside of the usual turn
+     *     order as responses to incoming attacks, enemy movement, and other
+     *     events.
+     * FULL ACTION
+     * Actions that require full attention (e.g. firing a barrage, performing
+     *     field repairs on a mech).
+     * FREE ACTION
+     * Free actions are special actions granted by character traits, like mech
+     *     systems and talents."
+     * - pg. 61
+     * -------------------------------------------------------------------------
+     * "Unlike the standard action types, there is no limit to how many free
+     *     actions and reactions a character can take per round. Characters can
+     *     even take reactions outside of their turn."
+     * - pg. 61
+     * =========================================================================
+     * Can be any String except "". Cannot be null.
+     * Case-insensitive and stored in lowercase. Cannot be null.
+     * Anything that is not one of the standard values is automatically counted
+     *     as a free action.
+     */
+    protected String value;
+
+    public ActivationTypeBase(String value) {
+        HelperMethods.verifyConstructor();
+        setValue(value);
+    }
+
+    // Required property
+    public String getValue() {
+        return value;
+    }
+    // Required property
+    protected void setValue(String value) {
+        HelperMethods.checkString("value", value);
+        value = value.toLowerCase();
+        this.value = value;
+    }
 }
