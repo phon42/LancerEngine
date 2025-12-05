e1829507644d73fff50fc0eedbed9ee8c51f63bb
phon42 <owenbchanas@gmail.com>
2025-12-05 17:31:45 -0600

    Moved some of SynergyLocation's content to SynergyLocationBase

--- Packages/CoreTypes/EntityMechanics/SynergyLocationSystem/SynergyLocationBase.java
+++ Packages/CoreTypes/EntityMechanics/SynergyLocationSystem/SynergyLocationBase.java
@@ -1,5 +1,30 @@
 package Packages.CoreTypes.EntityMechanics.SynergyLocationSystem;
 
+import MainBranch.HelperMethods;
+
+// TODO: fill out
 public class SynergyLocationBase {
-    
+    // Required property
+    /**
+     * This synergy location's id (i.e. "active_effects").
+     * Can be any String except "". Cannot be null.
+     * Case-insensitive and stored in lowercase.
+     */
+    protected String id;
+
+    protected SynergyLocationBase(String id) {
+        HelperMethods.verifyConstructor();
+        setID(id);
+    }
+
+    // Required property
+    public String getID() {
+        return id;
+    }
+    // Required property
+    protected void setID(String id) {
+        HelperMethods.checkString("id", id);
+        id = id.toLowerCase();
+        this.id = id;
+    }
 }
--- Packages/CoreTypes/EntityMechanics/SynergyLocationSystem/Verified/SynergyLocation.java
+++ Packages/CoreTypes/EntityMechanics/SynergyLocationSystem/Verified/SynergyLocation.java
@@ -5,14 +5,6 @@ import Packages.CoreTypes.EntityMechanics.SynergyLocationSystem.SynergyLocationB
 
 // TODO: fill out
 public class SynergyLocation extends SynergyLocationBase {
-    // Required property
-    /**
-     * This synergy location's id (i.e. "active_effects").
-     * Can be any String except "". Cannot be null.
-     * Case-insensitive and stored in lowercase.
-     */
-    private String id;
-
     // Optional property
     /**
      * A short description for developers of what this synergy location actually
@@ -24,27 +16,16 @@ public class SynergyLocation extends SynergyLocationBase {
     private String description;
 
     public SynergyLocation(String id, String description) {
-        HelperMethods.verifyConstructor();
-        setID(id);
+        super(id);
         setDescription(description);
     }
     public SynergyLocation(String id) {
         this(id, null);
     }
 
-    // Required property
-    public String getID() {
-        return id;
-    }
     // Optional property
     public String getDescription() {
         return description;
-    }
-    // Required property
-    private void setID(String id) {
-        HelperMethods.checkString("id", id);
-        id = id.toLowerCase();
-        this.id = id;
     }
     // Optional property
     public void setDescription(String description) {
