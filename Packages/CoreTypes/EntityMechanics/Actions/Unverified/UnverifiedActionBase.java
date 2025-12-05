dc2d1aac5560e16be8796f9f2f1f18a2da8e4dd1
phon42 <owenbchanas@gmail.com>
2025-12-05 17:36:40 -0600

    Updated UnverifiedActionBase

--- Packages/CoreTypes/EntityMechanics/Actions/Unverified/UnverifiedActionBase.java
+++ Packages/CoreTypes/EntityMechanics/Actions/Unverified/UnverifiedActionBase.java
@@ -1,5 +1,157 @@
 package Packages.CoreTypes.EntityMechanics.Actions.Unverified;
 
-public class UnverifiedActionBase {
-    
+import MainBranch.HelperMethods;
+import Packages.CoreTypes.Callable;
+import Packages.CoreTypes.EntityMechanics.Actions.ActionBaseBase;
+import Packages.CoreTypes.EntityMechanics.ActivationTypeSystem.Unverified.UnverifiedActivationType;
+import Packages.CoreTypes.EntityMechanics.FrequencySystem.Unverified.UnverifiedFrequency;
+import Packages.CoreTypes.EntityMechanics.SynergyLocationSystem.Unverified.UnverifiedSynergyLocation;
+import Packages.CoreTypes.TriState;
+
+/**
+ * See pgs. 61 - 64, 68 - 76, and 107 for more information.
+ * See also https://github.com/massif-press/lancer-data/blob/master/README.md#actions-actionsjson
+ *     and https://github.com/massif-press/lancer-data/blob/master/README.md#item-actions.
+ */
+/**
+ * Represents a single unverified action of any type. Contains information about
+ *     the action's name, activation speed, and a detailed description of what
+ *     it does, among other properties.
+ * 
+ * Requires an action name, activation speed, and a detailed description to be
+ *     instantiated.
+ * 
+ * Used in UnverifiedAction and UnverifiedIActionData.
+ * 
+ * Safety: At least one of this class' properties has an allowed value of null.
+ */
+public class UnverifiedActionBase extends ActionBaseBase {
+    // Required property
+    /**
+     * The activation type of this action (i.e. an UnverifiedActivationType
+     *     representing a free action).
+     * Can be any UnverifiedActivationType. Cannot be null.
+     */
+    protected UnverifiedActivationType activation;
+
+    // Optional properties
+    /**
+     * The frequency with which this action can be used, if there is one (i.e.
+     *     an UnverifiedFrequency that represents "1/round").
+     * Can be any UnverifiedFrequency. Cannot be null.
+     */
+    protected UnverifiedFrequency frequency;
+    /**
+     * An array of locations that this action is connected to (i.e. an
+     *     UnverifiedSynergyLocation[] containing an UnverifiedSynergyLocation
+     *     representing "overcharge").
+     * Can be any UnverifiedSynergyLocation[] that is not of length 0 and does
+     *     not contain null elements. Can be null.
+     */
+    private UnverifiedSynergyLocation[] synergyLocations;
+
+    protected UnverifiedActionBase(
+        // ActionBaseBase properties
+        String name, String detailedDescription, TriState pilot, TriState mech,
+        String[] confirm, TriState hideActive, String trigger, Callable method,
+        String requiredInitialConditions,
+        // Required property
+        UnverifiedActivationType activation,
+        // Optional properties
+        UnverifiedFrequency frequency,
+        UnverifiedSynergyLocation[] synergyLocations
+    ) {
+        super(name, detailedDescription, pilot, mech, confirm, hideActive,
+            trigger, method, requiredInitialConditions);
+        // Required property
+        setActivation(activation);
+        // Optional properties
+        setFrequency(frequency);
+        setSynergyLocations(synergyLocations);
+        // Helper property
+        setType(calculateType());
+    }
+    protected UnverifiedActionBase(
+        // ActionBaseBase properties
+        String name, String detailedDescription, TriState pilot, TriState mech,
+        String[] confirm, TriState hideActive, String trigger,
+        // Required property
+        UnverifiedActivationType activation,
+        // Optional property
+        UnverifiedFrequency frequency
+    ) {
+        this(name, detailedDescription, pilot, mech, confirm, hideActive,
+            trigger, null, null, activation,
+            frequency, null);
+    }
+    protected UnverifiedActionBase(
+        // ActionBaseBase properties
+        String name, String detailedDescription, TriState pilot, TriState mech,
+        String[] confirm, TriState hideActive,
+        // Required property
+        UnverifiedActivationType activation
+    ) {
+        this(name, detailedDescription, pilot, mech, confirm, hideActive,
+            null, null, null,
+            activation, null, null);
+    }
+    protected UnverifiedActionBase(
+        // ActionBaseBase properties
+        String name, String detailedDescription, String trigger,
+        // Required property
+        UnverifiedActivationType activation,
+        // Optional property
+        UnverifiedFrequency frequency
+    ) {
+        this(name, detailedDescription, null, null, null,
+            null, trigger, null,
+            null, activation, frequency,
+            null);
+    }
+    protected UnverifiedActionBase(
+        // ActionBaseBase properties
+        String name, String detailedDescription,
+        // Required property
+        UnverifiedActivationType activation
+    ) {
+        this(name, detailedDescription, null, null, null,
+            null, null, null,
+            null, activation, null,
+            null);
+    }
+
+    // Required property
+    public UnverifiedActivationType getActivation() {
+        return activation;
+    }
+    // Optional properties
+    public UnverifiedFrequency getFrequency() {
+        return frequency;
+    }
+    public UnverifiedSynergyLocation[] getSynergyLocations() {
+        if (synergyLocations == null) {
+            return synergyLocations;
+        }
+
+        return HelperMethods.copyOf(synergyLocations);
+    }
+    // Required property
+    protected void setActivation(UnverifiedActivationType activation) {
+        HelperMethods.checkObject("activation", activation);
+        this.activation = activation;
+    }
+    // Optional property
+    protected void setFrequency(UnverifiedFrequency frequency) {
+        HelperMethods.checkObject("frequency", frequency);
+        this.frequency = frequency;
+    }
+    private void setSynergyLocations(
+        UnverifiedSynergyLocation[] synergyLocations) {
+        HelperMethods.checkObjectArrayAlt("synergyLocations",
+            synergyLocations);
+        if (synergyLocations != null) {
+            synergyLocations = HelperMethods.copyOf(synergyLocations);
+        }
+        this.synergyLocations = synergyLocations;
+    }
 }
