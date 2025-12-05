68dffa63e5b507400147e10d522666ff50f197d7
phon42 <owenbchanas@gmail.com>
2025-12-05 00:56:08 -0600

    Added UnverifiedMechSystemType

---
+++ Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/mech/Equipment/Unverified/unverifiedEquipment/unverifiedSystemBase/UnverifiedMechSystemType.java
@@ -0,0 +1,5 @@
+package Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.mech.Equipment.Unverified.unverifiedEquipment.unverifiedSystemBase;
+
+public class UnverifiedMechSystemType {
+    
+}
