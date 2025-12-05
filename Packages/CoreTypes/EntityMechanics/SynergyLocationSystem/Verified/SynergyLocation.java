71721600aee8a1c04c34ef4dbb11147539245f56
phon42 <owenbchanas@gmail.com>
2025-12-05 17:28:33 -0600

    Updated SynergyLocation

--- Packages/CoreTypes/EntityMechanics/SynergyLocationSystem/Verified/SynergyLocation.java
+++ Packages/CoreTypes/EntityMechanics/SynergyLocationSystem/Verified/SynergyLocation.java
@@ -2,41 +2,52 @@ package Packages.CoreTypes.EntityMechanics.SynergyLocationSystem.Verified;
 
 import MainBranch.HelperMethods;
 
+// TODO: fill out
 public class SynergyLocation {
-    // TODO: fill out
     // Required property
-    private String value;
+    /**
+     * This synergy location's id (i.e. "active_effects").
+     * Can be any String except "". Cannot be null.
+     * Case-insensitive and stored in lowercase.
+     */
+    private String id;
 
     // Optional property
+    /**
+     * A short description for developers of what this synergy location actually
+     *     means (i.e. "The Active Effects panel near the top of the Active"
+     *     " Mode view").
+     * Can be any String except "". Can be null.
+     * Case-sensitive.
+     */
     private String description;
 
-    public SynergyLocation(String value, String description) {
+    public SynergyLocation(String id, String description) {
         HelperMethods.verifyConstructor();
-        setValue(value);
+        setID(id);
         setDescription(description);
     }
-    public SynergyLocation(String value) {
-        this(value, null);
+    public SynergyLocation(String id) {
+        this(id, null);
     }
 
     // Required property
-    public String getValue() {
-        return value;
+    public String getID() {
+        return id;
     }
     // Optional property
     public String getDescription() {
         return description;
     }
     // Required property
-    private void setValue(String value) {
-        HelperMethods.checkString("value", value);
-        this.value = value;
+    private void setID(String id) {
+        HelperMethods.checkString("id", id);
+        id = id.toLowerCase();
+        this.id = id;
     }
     // Optional property
     public void setDescription(String description) {
-        if (description != null) {
-            HelperMethods.checkString("description", description);
-        }
+        HelperMethods.checkStringAlt("description", description);
         this.description = description;
     }
 }
