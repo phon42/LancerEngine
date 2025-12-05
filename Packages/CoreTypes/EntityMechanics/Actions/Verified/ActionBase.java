dd73fe5120c4748c4cd809295898adfedc5a5e10
phon42 <owenbchanas@gmail.com>
2025-12-05 15:53:59 -0600

    Updated ActionBase

--- Packages/CoreTypes/EntityMechanics/Actions/Verified/ActionBase.java
+++ Packages/CoreTypes/EntityMechanics/Actions/Verified/ActionBase.java
@@ -1,6 +1,5 @@
 package Packages.CoreTypes.EntityMechanics.Actions.Verified;
 
-import java.util.HashMap;
 import java.util.NoSuchElementException;
 import MainBranch.Database;
 import MainBranch.HelperMethods;
@@ -8,8 +7,8 @@ import Packages.CoreTypes.Callable;
 import Packages.CoreTypes.EntityMechanics.ActivationType;
 import Packages.CoreTypes.EntityMechanics.Frequency;
 import Packages.CoreTypes.EntityMechanics.SynergyLocation;
+import Packages.CoreTypes.EntityMechanics.Actions.ActionBaseBase;
 import Packages.CoreTypes.EntityMechanics.frequency.FrequencyType;
-import Packages.CoreTypes.VueHTMLString;
 import Packages.CoreTypes.TriState;
 
 /**
@@ -18,87 +17,27 @@ import Packages.CoreTypes.TriState;
  *     and https://github.com/massif-press/lancer-data/blob/master/README.md#item-actions.
  */
 /**
- * Represents a single action of any type. Contains information about the
- *     action's name, activation speed, and a detailed description of what it
- *     does, among other properties.
+ * Represents a single verified action of any type. Contains information about
+ *     the action's name, activation speed, and a detailed description of what
+ *     it does, among other properties.
  * 
  * Requires an action name, activation speed, and a detailed description to be
  *     instantiated.
  * 
- * Used in myriad other classes.
+ * Used in Action and IActionData.
  * 
  * Safety: At least one of this class' properties has an allowed value of null.
  */
-public class ActionBase {
-    // Required properties
-    /**
-     * The name of this action (i.e. "Move" or "Turret Attack").
-     * For IActionData, might be a regular action name or something like
-     *     "Activate Item Name".
-     * Can be any String except "". Cannot be null.
-     * Case-sensitive.
-     */
-    protected String name;
+public class ActionBase extends ActionBaseBase {
+    // Required property
     /**
      * The activation type of this action (i.e. an ActivationType representing a
      *     free action).
      * Can be any ActivationType. Cannot be null.
      */
     protected ActivationType activation;
-    /**
-     * A detailed description of this action (too long to provide an example).
-     * Can be any VueHTMLString. Cannot be null.
-     * Case-sensitive.
-     */
-    protected VueHTMLString detail;
 
-    // Semi-required (optional but has a specific default value other than null
-    //     when not provided) properties
-    /**
-     * Whether this action can be used while in pilot form.
-     * Default value: false.
-     */
-    protected boolean pilot;
-    /**
-     * The default value for Action.pilot.
-     */
-    private static final boolean pilotDefault = false;
-    /**
-     * Whether this action can be used while in a mech.
-     * Default value: true.
-     */
-    protected boolean mech;
-    /**
-     * The default value for ActionBase.mech.
-     */
-    private static final boolean mechDefault = true;
-    /**
-     * An array of lines of flavor text that can be printed out when this action
-     *     is executed (i.e. a String[] containing the String "REACTOR LIMITER"
-     *     " DISABLED. CORE TEMPERATURES RISING.").
-     * Can be any String[] that does not contain null elements. Cannot be null.
-     * Default value: new String[] {"ACTIVATION CONFIRMED."}.
-     * Elements are case-sensitive.
-     */
-    protected String[] confirm;
-    /**
-     * The default value for ActionBase.confirm.
-     */
-    private static final String[] confirmDefault = new String[] {"ACTIVATION"
-        + " CONFIRMED."};
-    /**
-     * Unknown purpose.
-     * Is set to true for the Move and Free Action Actions in the base Lancer
-     *     LCP. Unused in all other locations.
-     * Default value: false.
-     */
-    protected boolean hideActive;
-    /**
-     * The default value for ActionBase.hideActive.
-     */
-    private static final boolean hideActiveDefault = false;
-
-    // Conditionally required properties
+    // Conditionally required property
     /**
      * The frequency with which this action can be used, if there is one (i.e. a
      *     Frequency that represents "1/round").
@@ -114,27 +53,8 @@ public class ActionBase {
      *     null.
      */
     protected Frequency frequency;
-    /**
-     * The trigger for this action, if there is one (i.e. a VueHTMLString
-     *     representing the String "You are hit by an attack and damage has"
-     *     " been rolled.").
-     * Required when this.activation is a reaction.
-     * When required:
-     *     Can be any VueHTMLString except "". Cannot be null.
-     * When not required:
-     *     Must be null.
-     * Can be any VueHTMLString except "". Can be null.
-     * Case-sensitive.
-     */
-    protected VueHTMLString trigger;
 
-    // Optional properties
-    /**
-     * A method allowing the program to perform the action automatically (unable
-     *     to provide an example).
-     * Can be any Callable. Can be null.
-     */
-    protected Callable method;
+    // Optional property
     /**
      * An array of locations that this action is connected to (i.e. a
      *     SynergyLocation[] containing a SynergyLocation representing
@@ -143,148 +63,99 @@ public class ActionBase {
      *     null.
      */
     private SynergyLocation[] synergyLocations;
-    /**
-     * Any initial conditions required for this action, if there are any (i.e.
-     *     "Requires activation of the <b>Spin Up Thrusters</b> quick action on"
-     *     " your turn. If you end your turn flying, you may nominate a"
-     *     " character within a Range equal to your Speed and within line of"
-     *     " sight, you may use this reaction.").
-     * Presence on the "Unleash SCYLLA Reaction" from the "SCYLLA-Class NHP"
-     *     from Gorgon III might be a typo. Present on the Ace talent.
-     * When this.activation is a reaction, can be any VueHTMLString except "".
-     *     Can be null.
-     * When this.activation is not a reaction, must be null.
-     * Can be any VueHTMLString except "". Can be null.
-     */
-    protected VueHTMLString init;
-
-    // Helper property
-    /**
-     * The general type of the action (i.e. "free").
-     * Value is based on this.activation.type and, secondarily, this.name.
-     * Can be any String except "". Cannot be null.
-     * Case-insensitive and stored in lowercase.
-     */
-    protected String type;
-    /**
-     * Stores a HashMap helpful in calculating ActionBase.type.
-     */
-    private static final HashMap<String, String> typeMap = new HashMap<>();
-
-    static {
-        ActionBase.typeMap.put("free", "free");
-        ActionBase.typeMap.put("protocol", "free");
-        ActionBase.typeMap.put("quick", "quick");
-        ActionBase.typeMap.put("full", "full");
-        ActionBase.typeMap.put("invade", "quick");
-        ActionBase.typeMap.put("full tech", "full");
-        ActionBase.typeMap.put("quick tech", "quick");
-        ActionBase.typeMap.put("reaction", "reaction");
-        ActionBase.typeMap.put("downtime", "downtime");
-    }
 
     protected ActionBase(
-        // Required properties
-        String name, ActivationType activation, String detailedDescription,
-        // Semi-required properties
-        TriState pilot, TriState mech, String[] confirm, TriState hideActive,
-        // Conditionally required properties
-        Frequency frequency, String trigger,
-        // Optional properties
-        Callable method, SynergyLocation[] synergyLocations,
-        String requiredInitialConditions
+        // ActionBaseBase properties
+        String name, String detailedDescription, TriState pilot, TriState mech,
+        String[] confirm, TriState hideActive, String trigger, Callable method,
+        String requiredInitialConditions,
+        // Required property
+        ActivationType activation,
+        // Conditionally required property
+        Frequency frequency,
+        // Optional property
+        SynergyLocation[] synergyLocations
     ) {
-        HelperMethods.verifyConstructor();
-        // Required properties
-        setName(name);
+        super(name, detailedDescription, pilot, mech, confirm, hideActive,
+            trigger, method, requiredInitialConditions);
+        // Required property
         setActivation(activation);
-        setDetail(detailedDescription);
-        // Semi-required properties
-        setPilotAndMech(pilot, mech);
-        setConfirm(confirm);
-        setHideActive(hideActive);
-        // Conditionally required properties
+        // Conditionally required property
         setFrequency(frequency);
-        setTrigger(trigger);
-        // Optional properties
-        setMethod(method);
+        // Optional property
         setSynergyLocations(synergyLocations);
-        setInit(requiredInitialConditions);
         // Helper property
         setType(calculateType());
     }
-    protected ActionBase(String name, ActivationType activation,
-        String detailedDescription) {
-        this(name, activation, detailedDescription, TriState.UNSET,
-            TriState.UNSET, null, TriState.UNSET, null,
-            null, null, null,
-            null);
+    protected ActionBase(
+        // ActionBaseBase properties
+        String name, String detailedDescription, TriState pilot, TriState mech,
+        String[] confirm, TriState hideActive, String trigger,
+        // Required property
+        ActivationType activation,
+        // Conditionally required property
+        Frequency frequency
+    ) {
+        super(name, detailedDescription, pilot, mech, confirm, hideActive,
+            trigger);
+        // Required property
+        setActivation(activation);
+        // Conditionally required property
+        setFrequency(frequency);
+        // Helper property
+        setType(calculateType());
     }
-    protected ActionBase(ActionBase actionBase) {
-        HelperMethods.checkObject("actionBase", actionBase);
-        // Required properties
-        setName(actionBase.name);
-        setActivation(actionBase.activation);
-        setDetail(actionBase.detail);
-        // Semi-required properties
-        setMethod(actionBase.method);
-        setPilot(actionBase.pilot);
-        setMech(actionBase.mech);
-        setConfirm(actionBase.confirm);
-        setHideActive(actionBase.hideActive);
-        // Conditionally required properties
-        setFrequency(actionBase.frequency);
-        setTrigger(actionBase.trigger);
-        // Optional properties
-        setMethod(actionBase.method);
-        setSynergyLocations(actionBase.synergyLocations);
-        setInit(actionBase.init);
+    protected ActionBase(
+        // ActionBaseBase properties
+        String name, String detailedDescription, TriState pilot, TriState mech,
+        String[] confirm, TriState hideActive,
+        // Required property
+        ActivationType activation
+    ) {
+        super(name, detailedDescription, pilot, mech, confirm, hideActive);
+        // Required property
+        setActivation(activation);
+        // Helper property
+        setType(calculateType());
+    }
+    protected ActionBase(
+        // ActionBaseBase properties
+        String name, String detailedDescription, String trigger,
+        // Required property
+        ActivationType activation,
+        // Conditionally required property
+        Frequency frequency
+    ) {
+        super(name, detailedDescription, trigger);
+        // Required property
+        setActivation(activation);
+        // Conditionally required property
+        setFrequency(frequency);
+        // Helper property
+        setType(calculateType());
+    }
+    protected ActionBase(
+        // ActionBaseBase properties
+        String name, String detailedDescription,
+        // Required property
+        ActivationType activation
+    ) {
+        super(name, detailedDescription);
+        // Required property
+        setActivation(activation);
         // Helper property
         setType(calculateType());
     }
 
-    // Required properties
-    public String getName() {
-        return name;
-    }
+    // Required property
     public ActivationType getActivation() {
         return activation;
     }
-    public VueHTMLString getDetail() {
-        return detail;
-    }
-    // Semi-required properties
-    public boolean isPilot() {
-        return pilot;
-    }
-    public boolean isMech() {
-        return mech;
-    }
-    public String[] getConfirm() {
-        return HelperMethods.copyOf(confirm);
-    }
-    public boolean isHideActive() {
-        return hideActive;
-    }
-    // Conditionally required properties
+    // Conditionally required property
     public Frequency getFrequency() {
         return frequency;
     }
-    public VueHTMLString getTrigger() {
-        if (trigger == null) {
-            return trigger;
-        }
-
-        return trigger;
-    }
-    // Optional properties
-    public Callable getMethod() {
-        if (method != null) {
-            return HelperMethods.copyOf(method);
-        }
-
-        return method;
-    }
+    // Optional property
     public SynergyLocation[] getSynergyLocations() {
         if (synergyLocations == null) {
             return synergyLocations;
@@ -292,54 +163,12 @@ public class ActionBase {
 
         return HelperMethods.copyOf(synergyLocations);
     }
-    public VueHTMLString getInit() {
-        if (init == null) {
-            return init;
-        }
-
-        return init;
-    }
-    // Helper property
-    public String getType() {
-        return type;
-    }
-    // Required properties
-    protected void setName(String name) {
-        HelperMethods.checkString("name", name);
-        this.name = name;
-    }
+    // Required property
     protected void setActivation(ActivationType activation) {
         HelperMethods.checkObject("activation", activation);
         this.activation = activation;
     }
-    protected void setDetail(VueHTMLString detail) {
-        HelperMethods.checkObject("detail", detail);
-        this.detail = detail;
-    }
-    protected void setDetail(String detail) {
-        HelperMethods.checkString("detail", detail);
-        setDetail(new VueHTMLString(detail));
-    }
-    // Semi-optional properties
-    protected void setPilot(boolean pilot) {
-        this.pilot = pilot;
-    }
-    protected void setMech(boolean mech) {
-        this.mech = mech;
-    }
-    protected void setConfirm(String[] confirm) {
-        if (confirm == null) {
-            confirm = HelperMethods.copyOf(ActionBase.confirmDefault);
-        } else {
-            HelperMethods.checkObjectArray("confirm", confirm);
-            confirm = HelperMethods.copyOf(confirm);
-        }
-        this.confirm = confirm;
-    }
-    protected void setHideActive(boolean hideActive) {
-        this.hideActive = hideActive;
-    }
-    // Conditionally required properties
+    // Conditionally required property
     protected void setFrequency(Frequency frequency) {
         FrequencyType type;
 
@@ -352,16 +181,7 @@ public class ActionBase {
         HelperMethods.checkObject("frequency", frequency);
         this.frequency = frequency;
     }
-    protected void setTrigger(VueHTMLString trigger) {
-        if (trigger != null) {
-            HelperMethods.checkVueHTMLString("trigger", trigger);
-        }
-        this.trigger = trigger;
-    }
-    // Optional properties
-    protected void setMethod(Callable method) {
-        this.method = method;
-    }
+    // Optional property
     private void setSynergyLocations(SynergyLocation[] synergyLocations) {
         if (synergyLocations == null) {
             this.synergyLocations = synergyLocations;
@@ -372,54 +192,8 @@ public class ActionBase {
         synergyLocations = HelperMethods.copyOf(synergyLocations);
         this.synergyLocations = synergyLocations;
     }
-    protected void setInit(VueHTMLString init) {
-        if (init != null) {
-            HelperMethods.checkVueHTMLString("init", init);
-        }
-        this.init = init;
-    }
-    // Helper property
-    protected void setType(String type) {
-        HelperMethods.checkString("type", type);
-        type = type.toLowerCase();
-        this.type = type;
-    }
 
-    protected void setPilotAndMech(TriState pilot, TriState mech) {
-        HelperMethods.checkObject("pilot", pilot);
-        HelperMethods.checkObject("mech", mech);
-        if (pilot == TriState.UNSET) {
-            setPilot(ActionBase.pilotDefault);
-        } else {
-            setPilot(pilot.toBoolean());
-        }
-        if (mech == TriState.UNSET) {
-            setMech(ActionBase.mechDefault);
-        } else {
-            setMech(mech.toBoolean());
-        }
-    }
-    protected void setTrigger(String trigger) {
-        if (trigger == null) {
-            this.trigger = null;
-        } else {
-            setTrigger(new VueHTMLString(trigger));
-        }
-    }
-    protected void setHideActive(TriState hideActive) {
-        if (hideActive == TriState.UNSET) {
-            this.hideActive = ActionBase.hideActiveDefault;
-        } else {
-            this.hideActive = hideActive.toBoolean();
-        }
-    }
-    protected void setInit(String init) {
-        if (init == null) {
-            this.init = null;
-        } else {
-            setInit(new VueHTMLString(init));
-        }
-    }
+    @Override
     protected void verifyProperties() {
         boolean isValidFreq1;
         boolean isValidFreq2;
@@ -452,6 +226,7 @@ public class ActionBase {
      * Calculates the correct value for the helper property ActionBase.type.
      * @return a String containing the correct value.
      */
+    @Override
     protected String calculateType() {
         String activationType;
         String result;
