07b66f6ed6a72f7504a4ce3abdd97c142680efe9
phon42 <owenbchanas@gmail.com>
2025-12-06 00:56:15 -0600

    Updated CoreSystemPassive

--- Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/mech/Frame/Verified/frame/coreSystem/iCoreSystemData/CoreSystemPassive.java
+++ Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/mech/Frame/Verified/frame/coreSystem/iCoreSystemData/CoreSystemPassive.java
@@ -1,14 +1,87 @@
 package Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.mech.Frame.Verified.frame.coreSystem.iCoreSystemData;
 
+import MainBranch.HelperMethods;
 import Packages.CoreTypes.EntityMechanics.ISynergyData;
 import Packages.CoreTypes.EntityMechanics.Actions.Verified.actionBase.IActionData;
 import Packages.CoreTypes.VueHTMLString.VueHTMLString;
 
 public class CoreSystemPassive {
     // Optional properties
-    private String passiveName;
-    private VueHTMLString passiveEffect;
-    private IActionData[] passiveActions;
-    private IActionData[] passiveBonuses;
-    private ISynergyData[] passiveSynergies;
+    /**
+     * Can be any String except "". Can be null.
+     * Case-sensitive.
+     */
+    private String name;
+    /**
+     * Can be any VueHTMLString except "". Can be null.
+     * Case-sensitive.
+     */
+    private VueHTMLString effect;
+    private IActionData[] actions;
+    private IActionData[] bonuses;
+    private ISynergyData[] synergies;
+
+    public CoreSystemPassive(String name, String effect, IActionData[] actions,
+        IActionData[] bonuses, ISynergyData[] synergies) {
+        // Optional properties
+        setName(name);
+        setEffect(effect);
+        setActions(actions);
+        setBonuses(bonuses);
+        setSynergies(synergies);
+    }
+
+    // Optional properties
+    public String getName() {
+        return name;
+    }
+    public VueHTMLString getEffect() {
+        return effect;
+    }
+    public IActionData[] getActions() {
+        return HelperMethods.copyOf(actions);
+    }
+    public IActionData[] getBonuses() {
+        return HelperMethods.copyOf(bonuses);
+    }
+    public ISynergyData[] getSynergies() {
+        return HelperMethods.copyOf(synergies);
+    }
+    // Optional properties
+    private void setName(String name) {
+        HelperMethods.checkStringAlt("name", name);
+        this.name = name;
+    }
+    private void setEffect(VueHTMLString effect) {
+        this.effect = effect;
+    }
+    private void setActions(IActionData[] actions) {
+        HelperMethods.checkObjectArrayAlt("actions", actions);
+        if (actions != null) {
+            actions = HelperMethods.copyOf(actions);
+        }
+        this.actions = actions;
+    }
+    private void setBonuses(IActionData[] bonuses) {
+        HelperMethods.checkObjectArrayAlt("bonuses", bonuses);
+        if (bonuses != null) {
+            bonuses = HelperMethods.copyOf(bonuses);
+        }
+        this.bonuses = bonuses;
+    }
+    private void setSynergies(ISynergyData[] synergies) {
+        HelperMethods.checkObjectArrayAlt("synergies", synergies);
+        if (synergies != null) {
+            synergies = HelperMethods.copyOf(synergies);
+        }
+        this.synergies = synergies;
+    }
+
+    private void setEffect(String effect) {
+        if (effect == null) {
+            this.effect = null;
+        } else {
+            setEffect(new VueHTMLString(effect));
+        }
+    }
 }
