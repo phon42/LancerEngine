8129c535086c85ad98ea98cd522585621cbfc068
phon42 <owenbchanas@gmail.com>
2025-12-05 20:27:08 -0600

    Moved a good amount of Bond's code to BondBase

--- Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/pilot/Bond/BondBase.java
+++ Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/pilot/Bond/BondBase.java
@@ -1,5 +1,72 @@
 package Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.pilot.Bond;
 
+import MainBranch.HelperMethods;
+
 public class BondBase {
-    
+    // Required properties
+    /**
+     * The bond's name (i.e. "The Harlequin").
+     * Can be any String except "". Cannot be null.
+     * Case-sensitive.
+     */
+    protected String name;
+    /**
+     * An array of the bond's major ideals (i.e. a String[] containing "I"
+     *     " addressed challenges with cunning, subterfuge, or deceit.", "I"
+     *     " expressed my heritage, background, or beliefs through my actions.",
+     *     and "I struggled with issues from my burdens or background.").
+     * Can be any String[] that does not contain null elements or elements that
+     *     are "". Cannot be null.
+     * Elements are case-sensitive.
+     */
+    protected String[] majorIdeals;
+    /**
+     * An array of the bond's minor ideals (i.e. a String[] containing "I"
+     *     " struggled against or humiliated the powerful.", "I pretended to be"
+     *     " someone else for a time.", "I charmed my way out of a charged"
+     *     " situation.", "I obtained a guarded, hidden, or secret object or"
+     *     " piece of information.").
+     * Can be any String[] that does not contain null elements or elements that
+     *     are "". Cannot be null.
+     * Elements are case-sensitive.
+     */
+    protected String[] minorIdeals;
+
+    protected BondBase(
+        // Required properties
+        String name, String[] majorIdeals, String[] minorIdeals
+    ) {
+        // Required properties
+        setName(name);
+        setMajorIdeals(majorIdeals);
+        setMinorIdeals(minorIdeals);
+    }
+
+    // Required properties
+    public String getName() {
+        return name;
+    }
+    public String[] getMajorIdeals() {
+        return HelperMethods.copyOf(majorIdeals);
+    }
+    public String[] getMinorIdeals() {
+        return HelperMethods.copyOf(minorIdeals);
+    }
+    // Required properties
+    protected void setName(String name) {
+        HelperMethods.checkString("name", name);
+        this.name = name;
+    }
+    protected void setMajorIdeals(String[] majorIdeals) {
+        HelperMethods.checkStringArray("majorIdeals",
+            majorIdeals);
+        majorIdeals = HelperMethods.copyOf(majorIdeals);
+        this.majorIdeals = majorIdeals;
+    }
+    protected void setMinorIdeals(String[] minorIdeals) {
+        HelperMethods.checkStringArray("minorIdeals",
+            minorIdeals);
+        majorIdeals = HelperMethods.copyOf(minorIdeals);
+        this.minorIdeals = minorIdeals;
+    }
 }
--- Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/pilot/Bond/Verified/Bond.java
+++ Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/pilot/Bond/Verified/Bond.java
@@ -1,6 +1,7 @@
 package Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.pilot.Bond.Verified;
 
 import MainBranch.HelperMethods;
+import Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.pilot.Bond.BondBase;
 import Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.pilot.Bond.bondBase.BondPower;
 import Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.pilot.Bond.bondBase.BondQuestion;
 
@@ -17,7 +18,7 @@ import Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.pilot.Bond.bond
  * 
  * This class is immutable (in other words, no copies of it need to be created).
  */
-public class Bond {
+public class Bond extends BondBase {
     // Required properties
     /**
      * The bond's id (i.e. "ktb-bond-harlequin").
@@ -25,29 +26,6 @@ public class Bond {
      * Case-insensitive and stored in lowercase.
      */
     private String id;
-    /**
-     * The bond's name (i.e. "The Harlequin").
-     * Can be any String except "". Cannot be null.
-     * Case-sensitive.
-     */
-    private String name;
-    /**
-     * An array of the bond's major ideals (i.e. a String[] containing "I"
-     *     " addressed challenges with cunning, subterfuge, or deceit.", "I"
-     *     " expressed my heritage, background, or beliefs through my actions.",
-     *     and "I struggled with issues from my burdens or background.").
-     * Can be any String[] that does not null elements. Cannot be null.
-     */
-    private String[] majorIdeals;
-    /**
-     * An array of the bond's minor ideals (i.e. a String[] containing "I"
-     *     " struggled against or humiliated the powerful.", "I pretended to be"
-     *     " someone else for a time.", "I charmed my way out of a charged"
-     *     " situation.", "I obtained a guarded, hidden, or secret object or"
-     *     " piece of information.").
-     * Can be any String[] that does not null elements. Cannot be null.
-     */
-    private String[] minorIdeals;
     /**
      * This bond's question (i.e. a BondQuestion representing "What gives you"
      *     " your powers?").
@@ -62,27 +40,23 @@ public class Bond {
      */
     private BondPower[] powers;
 
-    public Bond(String id, String name, String[] majorIdeals,
-        String[] minorIdeals, BondQuestion[] questions, BondPower[] powers) {
+    public Bond(
+        // BondBase properties
+        String name, String[] majorIdeals, String[] minorIdeals,
+        // Required properties
+        String id, BondQuestion[] questions, BondPower[] powers
+    ) {
+        // BondBase properties
+        super(name, majorIdeals, minorIdeals);
+        // Required properties
         setID(id);
-        setName(name);
-        setMajorIdeals(majorIdeals);
-        setMinorIdeals(minorIdeals);
         setQuestions(questions);
         setPowers(powers);
     }
 
+    // Required properties
     public String getID() {
         return id;
-    }
-    public String getName() {
-        return name;
-    }
-    public String[] getMajorIdeals() {
-        return HelperMethods.copyOf(majorIdeals);
-    }
-    public String[] getMinorIdeals() {
-        return HelperMethods.copyOf(minorIdeals);
     }
     public BondQuestion[] getQuestions() {
         return HelperMethods.copyOf(questions);
@@ -90,26 +64,11 @@ public class Bond {
     public BondPower[] getPowers() {
         return HelperMethods.copyOf(powers);
     }
+    // Required properties
     private void setID(String id) {
         HelperMethods.checkString("id", id);
         id = id.toLowerCase();
         this.id = id;
-    }
-    private void setName(String name) {
-        HelperMethods.checkString("name", name);
-        this.name = name;
-    }
-    private void setMajorIdeals(String[] majorIdeals) {
-        HelperMethods.checkStringArray("majorIdeals",
-            majorIdeals);
-        majorIdeals = HelperMethods.copyOf(majorIdeals);
-        this.majorIdeals = majorIdeals;
-    }
-    private void setMinorIdeals(String[] minorIdeals) {
-        HelperMethods.checkStringArray("minorIdeals",
-            minorIdeals);
-        majorIdeals = HelperMethods.copyOf(minorIdeals);
-        this.minorIdeals = minorIdeals;
     }
     private void setQuestions(BondQuestion[] questions) {
         HelperMethods.checkObjectArray("questions", questions);
