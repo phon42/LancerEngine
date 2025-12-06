deed28c3ea97204fe1bfc67206f3b55a232abc2c
phon42 <owenbchanas@gmail.com>
2025-12-06 01:58:55 -0600

    Updated ICoreSystemData

--- Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/mech/Frame/Verified/frame/coreSystem/ICoreSystemData.java
+++ Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/mech/Frame/Verified/frame/coreSystem/ICoreSystemData.java
@@ -1,5 +1,7 @@
 package Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.mech.Frame.Verified.frame.coreSystem;
 
+import MainBranch.HelperMethods;
+import Packages.CoreTypes.EntityMechanics.ActivationType.Verified.ActivationType;
 import Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.mech.Frame.Verified.frame.coreSystem.iCoreSystemData.CoreSystemActive;
 import Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.mech.Frame.Verified.frame.coreSystem.iCoreSystemData.CoreSystemData;
 import Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.mech.Frame.Verified.frame.coreSystem.iCoreSystemData.CoreSystemExtras;
@@ -9,12 +11,81 @@ import Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.mech.Frame.Veri
  * See pg. 33.
  */
 public class ICoreSystemData {
-    // Required property
+    // Required properties
     private CoreSystemData basicData;
+    private CoreSystemActive active;
 
     // Semi-required (optional but has a specific default value other than null
     //     when not provided) properties
-    private CoreSystemActive active;
     private CoreSystemPassive passive;
     private CoreSystemExtras extraData;
+
+    public ICoreSystemData(
+        // Required properties
+        CoreSystemData basicData, CoreSystemActive active,
+        // Semi-required properties
+        CoreSystemPassive passive, CoreSystemExtras extraData
+    ) {
+        HelperMethods.verifyConstructor();
+        // Required properties
+        setBasicData(basicData);
+        setActive(active);
+        // Semi-required properties
+        setPassive(passive);
+        setExtraData(extraData);
+    }
+    public ICoreSystemData(
+        // Required properties
+        CoreSystemData basicData, CoreSystemActive active
+    ) {
+        this(basicData, active, null, null);
+    }
+    public ICoreSystemData(
+        // Required properties
+        String coreSystemName, String activeName, String activeEffect,
+        ActivationType activeActivationType
+    ) {
+        HelperMethods.verifyConstructor();
+        setBasicData(coreSystemName);
+        setActive(activeName, activeEffect, activeActivationType);
+    }
+
+    // Required properties
+    public CoreSystemData getBasicData() {
+        return basicData;
+    }
+    public CoreSystemActive getActive() {
+        return active;
+    }
+    // Semi-required properties
+    public CoreSystemPassive getPassive() {
+        return passive;
+    }
+    public CoreSystemExtras getExtraData() {
+        return extraData;
+    }
+    // Required properties
+    private void setBasicData(CoreSystemData basicData) {
+        HelperMethods.checkObject("basicData", basicData);
+        this.basicData = basicData;
+    }
+    private void setActive(CoreSystemActive active) {
+        HelperMethods.checkObject("basicData", basicData);
+        this.active = active;
+    }
+    // Semi-required properties
+    private void setPassive(CoreSystemPassive passive) {
+        this.passive = passive;
+    }
+    private void setExtraData(CoreSystemExtras extraData) {
+        this.extraData = extraData;
+    }
+
+    private void setBasicData(String name) {
+        setBasicData(new CoreSystemData(name));
+    }
+    private void setActive(String name, String effect,
+        ActivationType activation) {
+        setActive(new CoreSystemActive(name, effect, activation));
+    }
 }
