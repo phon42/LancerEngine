eadd114e490194d69ad326cb61e40263a9cc9fa9
phon42 <owenbchanas@gmail.com>
2025-12-05 20:36:53 -0600

    Added UnverifiedBondID

---
+++ Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/pilot/Bond/Verified/bond/Unverified/UnverifiedBondID.java
@@ -0,0 +1,9 @@
+package Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.pilot.Bond.Verified.bond.Unverified;
+
+import Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.pilot.Bond.Verified.bond.BondIDBase;
+
+public class UnverifiedBondID extends BondIDBase {
+    public UnverifiedBondID(String id) {
+        super(id);
+    }
+}
