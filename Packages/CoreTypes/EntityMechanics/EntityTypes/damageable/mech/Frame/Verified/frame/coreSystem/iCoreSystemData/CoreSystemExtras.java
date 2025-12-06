f922979539702230ee50d68386994b1664f184be
phon42 <owenbchanas@gmail.com>
2025-12-06 01:06:40 -0600

    Added CoreSystemExtras

---
+++ Packages/CoreTypes/EntityMechanics/EntityTypes/damageable/mech/Frame/Verified/frame/coreSystem/iCoreSystemData/CoreSystemExtras.java
@@ -0,0 +1,14 @@
+package Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.mech.Frame.Verified.frame.coreSystem.iCoreSystemData;
+
+import Packages.CoreTypes.Counter.counterBase.CounterData;
+import Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.Deployable.deployable.iDeployableDataBase.Verified.IDeployableData;
+import Packages.CoreTypes.EntityMechanics.EntityTypes.damageable.mech.Equipment.Verified.equipment.TagSystem.unverifiedDataTag.DataTag;
+
+public class CoreSystemExtras {
+    // Optional properties
+    private IDeployableData[] deployables;
+    private CounterData[] counters;
+    private String[] integrated;
+    private String[] specialEquipment;
+    private DataTag[] tags;
+}
